f6f303a383f41314e6a0701568b375e77943c6e0
jack ning <github@bytedesk.com>
2023-12-20 07:55:15 +0800

    update

--- frontend/apps/opengpts/src/utils/simplifySchema.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { JsonRefs } from "./json-refs";
-
-// jsonforms doesn't support schemas with root $ref
-// so we resolve root $ref and replace it with the actual schema
-export function simplifySchema(schema: any) {
-  return JsonRefs.resolveRefs(schema).then((r: any) => r.resolved);
-}
--- frontend/apps/opengpts/src/utils/str.ts
+++
@@ -1,5 +0,0 @@
-export function str(o: unknown): React.ReactNode {
-  return typeof o === "object"
-    ? JSON.stringify(o, null, 2)
-    : (o as React.ReactNode);
-}
