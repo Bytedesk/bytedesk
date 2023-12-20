65c975379709894326d286eb3cbb47e1b3e8dd0c
jack ning <github@bytedesk.com>
2023-12-20 08:28:11 +0800

    update

---
+++ backend/spring-boot/src/main/java/com/bytedesk/ai/AiApplication.java
@@ -0,0 +1,14 @@
+package com.bytedesk.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 
+@SpringBootApplication
+public class AiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiApplication.class, args);
+	}
+
+}
