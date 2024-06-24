b0e23e1c0948cbcbdede9e38f497434e519c54b9
jack ning <github@bytedesk.com>
2024-06-25 06:47:09 +0800

    Update IpService.java

--- modules/core/src/main/java/com/bytedesk/core/ip/IpService.java
+++ modules/core/src/main/java/com/bytedesk/core/ip/IpService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-03-16 13:28:03
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-06-20 14:18:12
+ * @LastEditTime: 2024-06-25 06:46:56
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -13,6 +13,8 @@
  * Copyright (c) 2024 by bytedesk.com, All Rights Reserved. 
  */
 package com.bytedesk.core.ip;
+
+import java.util.regex.Pattern;
 
 import org.lionsoul.ip2region.xdb.Searcher;
 import org.springframework.stereotype.Service;
@@ -35,6 +37,30 @@ public class IpService {
 
     private final UidUtils uidUtils;
 
+    // 正则表达式用于匹配IPv4地址
+    private static final String IPV4_PATTERN = "(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])";
+
+    // 正则表达式用于匹配IPv6地址
+    private static final String IPV6_PATTERN = "([0-9a-f]{1,4}:){7}([0-9a-f]){1,4}|([0-9a-f]{1,4}:){6}(:[0-9a-f]{1,4}){2}|([0-9a-f]{1,4}:){5}(:[0-9a-f]{1,4}){3}|([0-9a-f]{1,4}:){4}(:[0-9a-f]{1,4}){4}|([0-9a-f]{1,4}:){3}(:[0-9a-f]{1,4}){5}|([0-9a-f]{1,4}:){2}(:[0-9a-f]{1,4}){6}|([0-9a-f]{1,4}:)(:[0-9a-f]{1,4}){7}|(:(:[0-9a-f]{1,4}){7})|(::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))";
+
+    /**
+     * 验证IP地址格式是否正确
+     * 
+     * @param ip 要验证的IP地址
+     * @return 如果格式正确则返回true，否则返回false
+     */
+    public static boolean isValidIp(String ip) {
+        // 验证IPv4地址
+        if (Pattern.matches(IPV4_PATTERN, ip)) {
+            return true;
+        }
+        // 验证IPv6地址
+        if (Pattern.matches(IPV6_PATTERN, ip)) {
+            return true;
+        }
+        return false;
+    }
+
     /**
      * 获取客户端ip
      * 
@@ -53,6 +79,12 @@ public class IpService {
      * @return
      */
     public String getIpLocation(String ip) {
+        // java.lang.Exception: invalid ip address `[0:0:0:0:0:0:0:1]` // replace localhost with 127.0.0.1
+        // 首先验证IP格式是否正确
+        if (!isValidIp(ip)) {
+            log.error("Invalid IP address format: " + ip);
+            return "0|0|0|内网IP|内网IP";
+        }
         try {
             return searcher.search(ip);
         } catch (Exception e) {
