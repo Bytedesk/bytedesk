7dff4523a4a45f555d88f32b0d338241270a07fe
jack ning <github@bytedesk.com>
2024-09-23 23:24:37 +0800

    update

--- starter/src/main/java/com/bytedesk/starter/controller/HelloController.java
+++ starter/src/main/java/com/bytedesk/starter/controller/HelloController.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-23 07:53:01
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-05-29 16:26:56
+ * @LastEditTime: 2024-09-16 13:13:29
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -20,6 +20,8 @@ import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,7 +37,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * 
+ * for testing api
  * http://127.0.0.1:9003/swagger-ui/index.html
  */
 @Slf4j
@@ -122,6 +124,13 @@ public class HelloController {
 	@PreAuthorize("hasRole('SUPER')")
 	public String superAccess() {
 		return "Super Board.";
+	}
+
+	// http://127.0.0.1:9003/hello/callback
+	@PostMapping("/callback")
+	public ResponseEntity<?> getCallback(@RequestBody String requestBody) {
+		log.info("hello callback {}", requestBody);
+		return ResponseEntity.ok(JsonResult.success("callback", requestBody));
 	}
 
 	// https://spring.io/guides/gs/rest-service-cors
