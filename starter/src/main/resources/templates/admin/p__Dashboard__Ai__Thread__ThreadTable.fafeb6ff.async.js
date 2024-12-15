6c09e427aa3ddf0cc714845ee7621d69b5f3cc6d
jack ning <github@bytedesk.com>
2024-12-15 21:46:19 +0800

    Sync from bytedesk-private: update

---
+++ starter/src/main/resources/templates/admin/p__Dashboard__Ai__Thread__ThreadTable.fafeb6ff.async.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkadmin=self.webpackChunkadmin||[]).push([[7357],{48595:function(n,e,t){t.r(e);var r=t(73030),u=t(31549);e.default=function(){return(0,u.jsx)(u.Fragment,{children:(0,u.jsx)(r.Z,{type:"robot"})})}}}]);
