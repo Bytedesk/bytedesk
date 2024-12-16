23d4d4e9f52492f05992f6c4fc6d59fc9fef76b5
jack ning <github@bytedesk.com>
2024-12-16 15:23:42 +0800

    Sync from bytedesk-private: update

--- modules/service/src/main/java/com/bytedesk/service/visitor/VisitorRestControllerAnonymous.java
+++ modules/service/src/main/java/com/bytedesk/service/visitor/VisitorRestControllerAnonymous.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-11-22 18:07:44
+ * @LastEditTime: 2024-12-16 14:55:44
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -93,12 +93,6 @@ public class VisitorRestControllerAnonymous {
         return ResponseEntity.ok(JsonResult.success("get unread messages success", messages));
     }
 
-    /**
-     * 客户端长连接断开，调用此接口发送消息
-     * 
-     * @param map map
-     * @return json
-     */
     @VisitorAnnotation(title = "visitor", action = "sendRestMessage", description = "sendRestMessage")
     @PostMapping("/message/send")
     public ResponseEntity<?> sendRestMessage(@RequestBody Map<String, String> map) {
--- modules/service/src/main/java/com/bytedesk/service/visitor/VisitorRestService.java
+++ modules/service/src/main/java/com/bytedesk/service/visitor/VisitorRestService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-11-23 10:41:25
+ * @LastEditTime: 2024-12-16 14:56:53
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -77,12 +77,6 @@ public class VisitorRestService extends BaseRestService<VisitorEntity, VisitorRe
         return ConvertServiceUtils.convertToVisitorResponse(visitorOptional.get());
     }
 
-    /**
-     * create visitor record
-     * 
-     * @param visitorRequest
-     * @return
-     */
     public UserProtobuf create(VisitorRequest visitorRequest, HttpServletRequest request) {
         //
         String uid = visitorRequest.getUid();
