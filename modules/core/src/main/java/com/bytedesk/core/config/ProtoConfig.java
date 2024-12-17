e5d073860c1b6ef1e04ee2f9edf6ee1b2b3a8b00
jack ning <github@bytedesk.com>
2024-12-17 17:38:47 +0800

    Sync from bytedesk-private: update

--- modules/core/src/main/java/com/bytedesk/core/config/ProtoConfig.java
+++ modules/core/src/main/java/com/bytedesk/core/config/ProtoConfig.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:46
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-04-04 12:07:11
+ * @LastEditTime: 2024-12-17 17:37:28
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -12,7 +12,7 @@
  *  联系：270580156@qq.com
  * Copyright (c) 2024 by bytedesk.com, All Rights Reserved. 
  */
-package com.bytedesk.core.socket.protobuf;
+package com.bytedesk.core.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
