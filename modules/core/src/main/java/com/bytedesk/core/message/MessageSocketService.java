325ac9af81f19f060f7ff5de270a02be6a69b9e3
jack ning <github@bytedesk.com>
2024-12-17 16:02:26 +0800

    Sync from bytedesk-private: update

--- modules/core/src/main/java/com/bytedesk/core/message/MessageSocketService.java
+++ modules/core/src/main/java/com/bytedesk/core/message/MessageSocketService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-02-26 10:36:50
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-12-04 15:41:28
+ * @LastEditTime: 2024-12-17 15:58:34
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -155,3 +155,4 @@ public class MessageSocketService {
     }
 
 }
+
