57980a4156505532000315a7dcf6300d40785b32
jack ning <github@bytedesk.com>
2024-12-17 17:12:09 +0800

    Sync from bytedesk-private: Update MessageConvertUtils.java

--- modules/core/src/main/java/com/bytedesk/core/utils/MessageConvertUtils.java
+++ modules/core/src/main/java/com/bytedesk/core/utils/MessageConvertUtils.java
@@ -1,3 +1,17 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2024-02-28 12:06:13
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2024-12-17 17:00:04
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
+ *  仅支持企业内部员工自用，严禁私自用于销售、二次销售或者部署SaaS方式销售 
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ *  联系：270580156@qq.com
+ * Copyright (c) 2024 by bytedesk.com, All Rights Reserved. 
+ */
 package com.bytedesk.core.utils;
 
 import java.io.IOException;
@@ -19,15 +33,5 @@ public class MessageConvertUtils {
         JsonFormat.parser().merge(json, targetBuilder);
         return targetBuilder.build();
     }
-
-    // public static MessageProto.Message convertMessageJsonToProto(String
-    // messageJson) {
-    // return null;
-    // }
-
-    // public static String convertMessageProtoToJson(MessageProto.Message
-    // messageProto) {
-    // return "";
-    // }
 
 }
