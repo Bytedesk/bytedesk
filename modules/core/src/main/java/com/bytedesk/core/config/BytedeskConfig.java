4ae7fc0673160203b4d188ecb0d7735a74ac6ea0
jack ning <github@bytedesk.com>
2024-12-20 12:47:32 +0800

    Sync from bytedesk-private: update

--- modules/core/src/main/java/com/bytedesk/core/config/BytedeskConfig.java
+++ modules/core/src/main/java/com/bytedesk/core/config/BytedeskConfig.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-30 07:52:26
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-10-22 11:58:34
+ * @LastEditTime: 2024-12-20 12:40:00
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -19,6 +19,7 @@ import java.util.Arrays;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -52,6 +53,11 @@ public class BytedeskConfig {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ApplicationEventPublisher applicationEventPublisher(ApplicationEventPublisher publisher) {
+        return publisher;
     }
 
     @Bean
