75f5e10d3d476dd97f554693ca4edee4a2b2afcb
jack ning <github@bytedesk.com>
2024-12-22 23:23:15 +0800

    Sync from bytedesk-private: Update StarterApplication.java

--- starter/src/main/java/com/bytedesk/starter/StarterApplication.java
+++ starter/src/main/java/com/bytedesk/starter/StarterApplication.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 15:02:58
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-12-22 22:21:07
+ * @LastEditTime: 2024-12-22 23:22:28
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -18,6 +18,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 // import com.redis.om.spring.annotations.EnableRedisDocumentRepositories;
 
@@ -25,6 +26,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @ComponentScan("com.bytedesk.*")
 // @EnableRedisDocumentRepositories(basePackages = "com.bytedesk.*")
+@EnableRetry
 @SpringBootApplication
 public class StarterApplication {
 
