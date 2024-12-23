c9007bac261f0a02f7e6cec31b944d71cdc1b6be
jack ning <github@bytedesk.com>
2024-12-24 07:57:07 +0800

    Sync from bytedesk-private: up

--- modules/service/src/main/java/com/bytedesk/service/strategy/KbdocCsThreadCreationStrategy.java
+++ modules/service/src/main/java/com/bytedesk/service/strategy/KbdocCsThreadCreationStrategy.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-08-29 23:00:00
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-12-18 17:22:34
+ * @LastEditTime: 2024-12-24 07:53:08
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -122,7 +122,7 @@ public class KbdocCsThreadCreationStrategy implements CsThreadCreationStrategy {
         // userExtra.put("defaultReply", robot.getDefaultReply());
         // user.setExtra(JSON.toJSONString(userExtra));
         //
-        return ThreadMessageUtil.getThreadWelcomeMessage(user, thread);
+        return ThreadMessageUtil.getThreadRobotWelcomeMessage(user, thread);
     }
 
 }
--- modules/service/src/main/java/com/bytedesk/service/strategy/ThreadMessageUtil.java
+++ modules/service/src/main/java/com/bytedesk/service/strategy/ThreadMessageUtil.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-08-29 22:22:38
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-12-23 15:16:32
+ * @LastEditTime: 2024-12-24 07:52:27
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -24,21 +24,42 @@ import com.bytedesk.core.message.MessageStatusEnum;
 import com.bytedesk.core.message.MessageTypeEnum;
 import com.bytedesk.core.message.MessageUtils;
 import com.bytedesk.core.rbac.user.UserProtobuf;
-import com.bytedesk.core.rbac.user.UserTypeEnum;
 import com.bytedesk.core.thread.ThreadEntity;
 import com.bytedesk.core.uid.UidUtils;
 import com.bytedesk.service.agent.AgentEntity;
 import com.bytedesk.service.utils.ConvertServiceUtils;
 
 import java.time.LocalDateTime;
-// import java.time.LocalDateTime;
 
 // 可以根据需要选择是否使用 @Component 注解
 // 如果该方法不需要被Spring容器管理，则不需要此注解
 public class ThreadMessageUtil {
 
+    public static MessageProtobuf getThreadRobotWelcomeMessage(UserProtobuf user, ThreadEntity thread) {
+        // ... 方法的实现保持不变 ...
+        MessageEntity message = MessageEntity.builder()
+                .content(thread.getContent())
+                .type(MessageTypeEnum.WELCOME.name())
+                .status(MessageStatusEnum.READ.name())
+                .client(ClientEnum.SYSTEM.name())
+                .user(JSON.toJSONString(user))
+                .build();
+        message.setUid(UidUtils.getInstance().getUid());
+        message.setOrgUid(thread.getOrgUid());
+        message.setCreatedAt(LocalDateTime.now());
+        message.setUpdatedAt(LocalDateTime.now());
+        message.setThreadTopic(thread.getTopic());
+        //
+        MessageExtra extra = MessageUtils.getMessageExtra(thread.getOrgUid());
+        message.setExtra(JSON.toJSONString(extra));
+        //
+        return ConvertServiceUtils.convertToMessageProtobuf(message, thread);
+    }
+
+
     // 将此方法设为静态，以便在没有实例化类的情况下调用
-    public static MessageProtobuf getThreadWelcomeMessage(UserProtobuf user, ThreadEntity thread) {
+    public static MessageProtobuf getThreadWelcomeMessage(AgentEntity agent, ThreadEntity thread) {
+        UserProtobuf user = ConvertServiceUtils.convertToUserProtobuf(agent);
         // ... 方法的实现保持不变 ...
         MessageEntity message = MessageEntity.builder()
                 .content(thread.getContent())
@@ -52,14 +73,14 @@ public class ThreadMessageUtil {
         message.setCreatedAt(LocalDateTime.now());
         message.setUpdatedAt(LocalDateTime.now());
         // 
-        if (user.getType().equals(UserTypeEnum.ROBOT.name())) {
-            message.setType(MessageTypeEnum.WELCOME.name());
-            message.setContent(thread.getContent());
-        }
+        // if (user.getType().equals(UserTypeEnum.ROBOT.name())) {
+        //     message.setType(MessageTypeEnum.WELCOME.name());
+        //     message.setContent(thread.getContent());
+        // }
         //
-        if (thread.isOffline()) {
-            message.setType(MessageTypeEnum.LEAVE_MSG.name());
-        }
+        // if (thread.isOffline()) {
+        //     message.setType(MessageTypeEnum.LEAVE_MSG.name());
+        // }
         message.setThreadTopic(thread.getTopic());
         //
         MessageExtra extra = MessageUtils.getMessageExtra(thread.getOrgUid());
@@ -68,7 +89,8 @@ public class ThreadMessageUtil {
         return ConvertServiceUtils.convertToMessageProtobuf(message, thread);
     }
 
-    public static MessageProtobuf getThreadQueueMessage(UserProtobuf user, ThreadEntity thread) {
+    public static MessageProtobuf getThreadQueueMessage(AgentEntity agent, ThreadEntity thread) {
+        UserProtobuf user = ConvertServiceUtils.convertToUserProtobuf(agent);
         // ... 方法的实现保持不变 ...
         MessageEntity message = MessageEntity.builder()
                 .content(thread.getContent())
