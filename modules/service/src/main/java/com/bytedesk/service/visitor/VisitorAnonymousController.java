5135f16c9903f27591ead067701a0d9cacd323c3
jack ning <github@bytedesk.com>
2024-12-24 15:19:29 +0800

    Sync from bytedesk-private: update

--- modules/service/src/main/java/com/bytedesk/service/visitor/VisitorAnonymousController.java
+++ modules/service/src/main/java/com/bytedesk/service/visitor/VisitorAnonymousController.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-12-24 15:02:07
+ * @LastEditTime: 2024-12-24 15:18:22
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -61,7 +61,7 @@ public class VisitorAnonymousController {
     @VisitorAnnotation(title = "visitor", action = "init", description = "init visitor")
     @ApiRateLimiter(value = 10.0, timeout = 1)
     @PostMapping("/init")
-    public ResponseEntity<?> init(VisitorRequest visitorRequest, HttpServletRequest request) {
+    public ResponseEntity<?> init(@RequestBody VisitorRequest visitorRequest, HttpServletRequest request) {
         //
         String ip = IpUtils.getIp(request);
         if (ip != null) {
@@ -78,7 +78,7 @@ public class VisitorAnonymousController {
 
     @VisitorAnnotation(title = "visitor", action = "requestThread", description = "request thread")
     @PostMapping("/thread")
-    public ResponseEntity<?> requestThread(VisitorRequest visitorRequest) {
+    public ResponseEntity<?> requestThread(@RequestBody VisitorRequest visitorRequest) {
         //
         MessageProtobuf messageProtobuf = visitorService.requestThread(visitorRequest);
         //
