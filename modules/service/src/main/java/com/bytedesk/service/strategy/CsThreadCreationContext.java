241ffd5bc8c7f6c5bf939e149998bf9419ff6ec5
jack ning <github@bytedesk.com>
2024-12-24 11:47:54 +0800

    Sync from bytedesk-private: update

--- modules/service/src/main/java/com/bytedesk/service/strategy/CsThreadCreationContext.java
+++ modules/service/src/main/java/com/bytedesk/service/strategy/CsThreadCreationContext.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-08-29 22:07:52
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-11-01 14:25:41
+ * @LastEditTime: 2024-12-24 11:47:46
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
