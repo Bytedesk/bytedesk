b3adda2c45bf106877d08e2b6378ed1dcdf5093e
jack ning <github@bytedesk.com>
2024-12-24 10:40:48 +0800

    Sync from bytedesk-private: update

--- modules/core/src/main/java/com/bytedesk/core/message/MessageRequest.java
+++ modules/core/src/main/java/com/bytedesk/core/message/MessageRequest.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-02-21 10:00:32
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-07-04 14:56:27
+ * @LastEditTime: 2024-12-24 10:34:35
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -28,5 +28,5 @@ public class MessageRequest extends BaseRequest {
     
     private String userUid;
 
-    private String orgUid;
+    // private String orgUid;
 }
