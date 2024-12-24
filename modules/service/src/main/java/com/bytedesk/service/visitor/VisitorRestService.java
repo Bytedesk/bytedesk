065f37c35079a1a045ded1ad0538d0e37e81ce63
jack ning <github@bytedesk.com>
2024-12-24 10:00:47 +0800

    Sync from bytedesk-private: update

--- modules/service/src/main/java/com/bytedesk/service/visitor/VisitorRestService.java
+++ modules/service/src/main/java/com/bytedesk/service/visitor/VisitorRestService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-12-16 14:56:53
+ * @LastEditTime: 2024-12-24 10:00:21
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -119,7 +119,7 @@ public class VisitorRestService extends BaseRestService<VisitorEntity, VisitorRe
     }
 
     /** 策略模式 */
-    public MessageProtobuf createCsThread(VisitorRequest visitorRequest) {
+    public MessageProtobuf requestThread(VisitorRequest visitorRequest) {
         return csThreadCreationContext.createCsThread(visitorRequest);
     }
 
