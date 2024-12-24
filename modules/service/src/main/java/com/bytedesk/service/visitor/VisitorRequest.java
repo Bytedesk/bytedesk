d8df9971f6c334c862ae5c3c7820d1d013f4ac96
jack ning <github@bytedesk.com>
2024-12-24 14:01:02 +0800

    Sync from bytedesk-private: update

--- modules/service/src/main/java/com/bytedesk/service/visitor/VisitorRequest.java
+++ modules/service/src/main/java/com/bytedesk/service/visitor/VisitorRequest.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-04-04 17:05:48
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-12-24 09:46:10
+ * @LastEditTime: 2024-12-24 13:01:36
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -47,9 +47,11 @@ public class VisitorRequest extends BaseRequest {
 	private String lang = LanguageEnum.ZH_CN.name();
 
 	// location info
-	private String ip;
+	@Builder.Default
+	private String ip = "127.0.0.1";
 
-	private String ipLocation;
+	@Builder.Default
+	private String ipLocation = "localhost";
 
 	// device info
 	private String browser;
