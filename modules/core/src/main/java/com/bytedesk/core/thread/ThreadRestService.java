a99a5f7cc43234d10566047ff5174b7bbcc4141b
jack ning <github@bytedesk.com>
2024-12-25 15:34:12 +0800

    Sync from bytedesk-private: update

--- modules/core/src/main/java/com/bytedesk/core/thread/ThreadRestService.java
+++ modules/core/src/main/java/com/bytedesk/core/thread/ThreadRestService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-12-25 13:06:59
+ * @LastEditTime: 2024-12-25 15:31:55
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -378,13 +378,13 @@ public class ThreadRestService extends BaseRestService<ThreadEntity, ThreadReque
         return threadRepository.findByOwnerAndHideAndDeleted(user, false, false, pageable);
     }
 
-    // TODO: 更新缓存
     // @Cacheable(value = "threadOpen")
     public List<ThreadEntity> findStateOpen() {
         List<String> types = Arrays.asList(new String[] { ThreadTypeEnum.AGENT.name(), ThreadTypeEnum.WORKGROUP.name(),
                 ThreadTypeEnum.ROBOT.name() });
-        List<String> states = Arrays .asList(new String[] { ThreadStateEnum.CLOSED.name()});
-        return threadRepository.findByTypesInAndStatesNotInAndDeleted(types, states, false);
+        // List<String> states = Arrays .asList(new String[] { ThreadStateEnum.CLOSED.name()});
+        // return threadRepository.findByTypesInAndStatesNotInAndDeleted(types, states, false);
+        return threadRepository.findByTypesInAndStateAndDeletedFalse(types, ThreadStateEnum.STARTED.name());
     }
 
     @Caching(put = {
