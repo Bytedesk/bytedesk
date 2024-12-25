222740f978f1f3a05afb10a8778abffaa8c3c150
jack ning <github@bytedesk.com>
2024-12-25 10:43:36 +0800

    Sync from bytedesk-private: update

--- modules/service/src/main/java/com/bytedesk/service/routing/RouteService.java
+++ modules/service/src/main/java/com/bytedesk/service/routing/RouteService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-09-19 18:59:41
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-12-24 22:38:02
+ * @LastEditTime: 2024-12-25 10:40:47
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
