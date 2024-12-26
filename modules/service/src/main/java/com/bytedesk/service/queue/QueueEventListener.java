acf490905bacb92a6995cd2706f51ecfcce917f4
jack ning <github@bytedesk.com>
2024-12-26 13:33:50 +0800

    update

--- modules/service/src/main/java/com/bytedesk/service/queue/QueueEventListener.java
+++ modules/service/src/main/java/com/bytedesk/service/queue/QueueEventListener.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-05-29 22:19:11
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-12-26 11:43:23
+ * @LastEditTime: 2024-12-26 12:53:52
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -55,6 +55,18 @@ public class QueueEventListener {
     public void onThreadCloseEvent(ThreadCloseEvent event) {
         ThreadEntity thread = event.getThread();
         log.info("queue onThreadCloseEvent: {}", thread.getAgent());
+        String queueTopic = TopicUtils.getQueueTopicFromThreadTopic(thread.getTopic());
+        log.info("queue onThreadAcceptEvent: {}", queueTopic);
+        String queueDay = thread.getCreatedAt().format(DateTimeFormatter.ISO_DATE);
+        Optional<QueueEntity> queueOptional = queueRestService.findByTopicAndDay(queueTopic, queueDay);
+        if (queueOptional.isPresent()) {
+            QueueEntity queue = queueOptional.get();
+            // 增加已完成人数
+            queue.increaseServedNumber();
+            queueRestService.save(queue);
+        } else {
+            log.error("queue onThreadAcceptEvent: queue not found: {}", queueTopic);
+        }
     }
     
 
@@ -68,8 +80,9 @@ public class QueueEventListener {
         Optional<QueueEntity> queueOptional = queueRestService.findByTopicAndDay(queueTopic, queueDay);
         if (queueOptional.isPresent()) {
             QueueEntity queue = queueOptional.get();
+            // 减少排队人数，增加接入人数
             queue.acceptThread();
-            queueRestService.save(queueOptional.get());
+            queueRestService.save(queue);
         } else {
             log.error("queue onThreadAcceptEvent: queue not found: {}", queueTopic);
         }
