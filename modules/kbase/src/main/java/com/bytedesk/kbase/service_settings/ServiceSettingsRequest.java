c978462e1faf1057d37fa257e9dd77f4e6b6ff6e
jack ning <github@bytedesk.com>
2024-12-26 12:36:29 +0800

    update

--- modules/kbase/src/main/java/com/bytedesk/kbase/service_settings/ServiceSettingsRequest.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/service_settings/ServiceSettingsRequest.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-06-14 10:45:08
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-12-23 14:45:46
+ * @LastEditTime: 2024-12-26 12:31:43
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -29,7 +29,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 @Data
-I18N_QUEUE_MESSAGE_TEMPLATE
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class ServiceSettingsRequest  implements Serializable {
