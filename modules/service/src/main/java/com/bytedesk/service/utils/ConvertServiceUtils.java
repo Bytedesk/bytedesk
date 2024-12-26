08d2659baf8a2a19f308c7a8e33c5b460ed7188f
jack ning <github@bytedesk.com>
2024-12-26 10:49:05 +0800

    update

--- modules/service/src/main/java/com/bytedesk/service/utils/ConvertServiceUtils.java
+++ modules/service/src/main/java/com/bytedesk/service/utils/ConvertServiceUtils.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-06-04 11:25:45
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-12-26 10:11:28
+ * @LastEditTime: 2024-12-26 10:44:59
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -61,7 +61,8 @@ public class ConvertServiceUtils {
     }
 
     public static String convertToUserProtobufJSONString(VisitorRequest visitorRequest) {
-        return JSON.toJSONString(convertToUserProtobuf(visitorRequest));
+        UserProtobuf userProtobuf = convertToUserProtobuf(visitorRequest);
+        return JSON.toJSONString(userProtobuf);
     }
 
     public static UserProtobuf convertToUserResponseSimple(UserProtobuf visitorResponseSimple) {
