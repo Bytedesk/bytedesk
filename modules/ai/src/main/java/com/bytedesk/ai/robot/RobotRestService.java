8d7f9a34100ffd3a61347b3b985cf6924ef86407
jack ning <github@bytedesk.com>
2024-12-27 11:12:25 +0800

    update

--- modules/ai/src/main/java/com/bytedesk/ai/robot/RobotRestService.java
+++ modules/ai/src/main/java/com/bytedesk/ai/robot/RobotRestService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-03-22 16:44:41
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-12-23 12:52:38
+ * @LastEditTime: 2024-12-27 11:04:04
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
