1508f612d6897617a67b58feef83a97308996f9b
jack ning <github@bytedesk.com>
2024-12-28 12:34:59 +0800

    update

--- modules/core/src/main/java/com/bytedesk/core/token/TokenResponse.java
+++ modules/core/src/main/java/com/bytedesk/core/token/TokenResponse.java
@@ -12,7 +12,7 @@
  *  联系：270580156@qq.com
  * Copyright (c) 2024 by bytedesk.com, All Rights Reserved. 
  */
-package com.bytedesk.core.rbac.token;
+package com.bytedesk.core.token;
 
 public class TokenResponse {
 
