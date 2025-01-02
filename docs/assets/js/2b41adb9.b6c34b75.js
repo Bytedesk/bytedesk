fffbecbf17d1b30851250e619eae3f267f588fa2
jack ning <github@bytedesk.com>
2025-01-03 07:52:00 +0800

    update

---
+++ docs/assets/js/2b41adb9.b6c34b75.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[2090],{7466:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/bytedesk/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":5,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ docs/zh-CN/assets/js/acecf23e.15a70fd6.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/bytedesk/zh-CN/blog","blogTitle":"Blog","authorsListPath":"/bytedesk/zh-CN/blog/authors"}')}}]);
