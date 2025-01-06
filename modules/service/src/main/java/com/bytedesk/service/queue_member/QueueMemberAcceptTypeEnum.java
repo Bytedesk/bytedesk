0aa8433f58e3fbbbf43dde5c973b16f775ed9282
jack ning <github@bytedesk.com>
2025-01-06 18:03:40 +0800

    update

--- modules/service/src/main/java/com/bytedesk/service/queue_member/QueueMemberAcceptTypeEnum.java
+++ modules/service/src/main/java/com/bytedesk/service/queue_member/QueueMemberAcceptTypeEnum.java
@@ -5,8 +5,7 @@
  * @LastEditTime: 2024-12-20 10:20:16
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
- *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
- *  仅支持企业内部员工自用，严禁私自用于销售、二次销售或者部署SaaS方式销售 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
  *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
  *  contact: 270580156@qq.com 
  *  联系：270580156@qq.com
