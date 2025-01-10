683f36d4e7c8fa9ca93a0e7aa2243ae17e105cbf
jack ning <github@bytedesk.com>
2025-01-10 18:06:22 +0800

    update

--- starter/src/main/java/com/bytedesk/starter/test/prefix/AirportsRepository.java
+++ starter/src/main/java/com/bytedesk/starter/test/prefix/AirportsRepository.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-09-28 13:06:28
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-01-10 17:07:24
+ * @LastEditTime: 2025-01-10 17:59:50
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -13,17 +13,21 @@
  */
 package com.bytedesk.starter.test.prefix;
 
-import java.util.List;
+// import java.util.List;
 
-import com.redis.om.spring.autocomplete.Suggestion;
-import com.redis.om.spring.repository.RedisDocumentRepository;
-import com.redis.om.spring.repository.query.autocomplete.AutoCompleteOptions;
+// import org.springframework.stereotype.Repository;
 
-// https://github.com/redis/redis-om-spring
-// https://github.com/redis-developer/redis-om-autocomplete-demo
-public interface AirportsRepository extends RedisDocumentRepository<Airport, String> {
+// import com.redis.om.spring.autocomplete.Suggestion;
+// import com.redis.om.spring.repository.RedisDocumentRepository;
+// import com.redis.om.spring.repository.query.autocomplete.AutoCompleteOptions;
+
+// // https://github.com/redis/redis-om-spring
+// // https://github.com/redis-developer/redis-om-autocomplete-demo
+// @Repository
+// public interface AirportsRepository extends RedisDocumentRepository<Airport, String> {
     
-  List<Suggestion> autoCompleteName(String query);
+//   List<Suggestion> autoCompleteName(String query);
 
-  List<Suggestion> autoCompleteNameOptions(String query, AutoCompleteOptions options);
-}
+//   List<Suggestion> autoCompleteNameOptions(String query, AutoCompleteOptions options);
+// }
+  
