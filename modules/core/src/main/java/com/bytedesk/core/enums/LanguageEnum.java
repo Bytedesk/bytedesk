11b6c385b0368ae1b5b9412166bbd80fa98bed28
jack ning <github@bytedesk.com>
2025-01-13 15:04:40 +0800

    update

--- modules/core/src/main/java/com/bytedesk/core/enums/LanguageEnum.java
+++ modules/core/src/main/java/com/bytedesk/core/enums/LanguageEnum.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-06-07 14:19:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-12-24 16:12:00
+ * @LastEditTime: 2025-01-13 15:03:18
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
