73939623cde446c0c079c3954a84aa1d2e08a23b
jack ning <github@bytedesk.com>
2025-01-15 15:18:10 +0800

    update

--- modules/core/src/main/java/com/bytedesk/core/ip/IpTypeEnum.java
+++ modules/core/src/main/java/com/bytedesk/core/ip/IpTypeEnum.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-06-27 11:12:55
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-06-27 12:05:48
+ * @LastEditTime: 2025-01-15 15:15:58
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -15,9 +15,7 @@ package com.bytedesk.core.ip;
 
 public enum IpTypeEnum {
     WHITELIST,
-    WHITERANGE,
-    BLACKLIST,
-    BLACKRANGE;
+    BLACKLIST;
 
     // 根据字符串查找对应的枚举常量
     public static IpTypeEnum fromValue(String value) {
