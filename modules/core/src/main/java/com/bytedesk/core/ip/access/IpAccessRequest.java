564ec72a1f949dbef3a66488c71e4fc40c15556f
jack ning <github@bytedesk.com>
2025-01-17 16:33:00 +0800

    update

--- modules/core/src/main/java/com/bytedesk/core/ip/access/IpAccessRequest.java
+++ modules/core/src/main/java/com/bytedesk/core/ip/access/IpAccessRequest.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-01-17 15:27:36
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-01-17 15:27:38
+ * @LastEditTime: 2025-01-17 16:32:09
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -15,6 +15,20 @@ package com.bytedesk.core.ip.access;
 
 import com.bytedesk.core.base.BaseRequest;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class IpAccessRequest extends BaseRequest {
-    
+    // 
+    private String ip;
+    private String ipLocation;
+    // 
 }
