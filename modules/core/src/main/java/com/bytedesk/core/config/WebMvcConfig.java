99120e880cdbfa6845d37f8c86060bab71df1cf8
jack ning <github@bytedesk.com>
2025-01-17 20:47:33 +0800

    update

--- modules/core/src/main/java/com/bytedesk/core/config/WebMvcConfig.java
+++ modules/core/src/main/java/com/bytedesk/core/config/WebMvcConfig.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-26 15:28:57
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-01-17 15:58:29
+ * @LastEditTime: 2025-01-17 20:25:46
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
