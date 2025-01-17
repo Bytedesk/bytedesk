19dc8f8da51910f2e7ed1e7f962154d4dd6b9109
jack ning <github@bytedesk.com>
2025-01-17 16:57:57 +0800

    update

--- modules/core/src/main/java/com/bytedesk/core/ip/black/IpBlacklistSpecification.java
+++ modules/core/src/main/java/com/bytedesk/core/ip/black/IpBlacklistSpecification.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-01-17 15:21:47
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-01-17 15:41:11
+ * @LastEditTime: 2025-01-17 16:54:15
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.StringUtils;
 
 import com.bytedesk.core.base.BaseSpecification;
 
@@ -28,7 +29,18 @@ public class IpBlacklistSpecification extends BaseSpecification {
         return (root, query, criteriaBuilder) -> {
             List<Predicate> predicates = new ArrayList<>();
             predicates.addAll(getBasicPredicates(root, criteriaBuilder, request.getOrgUid()));
-            //
+            // ip
+            if (StringUtils.hasText(request.getIp())) {
+                predicates.add(criteriaBuilder.like(root.get("ip"), "%" + request.getIp() + "%"));
+            }
+            // reason
+            if (StringUtils.hasText(request.getReason())) {
+                predicates.add(criteriaBuilder.like(root.get("reason"), "%" + request.getReason() + "%"));
+            }
+            // userNickname
+            if (StringUtils.hasText(request.getUserNickname())) {
+                predicates.add(criteriaBuilder.like(root.get("userNickname"), "%" + request.getUserNickname() + "%"));
+            }
             //
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
         };
