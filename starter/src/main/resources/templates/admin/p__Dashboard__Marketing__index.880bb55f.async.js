4a8fdad54e950d4c7875725a510e15fbff7a14d4
jack ning <github@bytedesk.com>
2025-01-24 20:27:47 +0800

    update

---
+++ starter/src/main/resources/templates/admin/p__Dashboard__Marketing__index.880bb55f.async.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkadmin=self.webpackChunkadmin||[]).push([[8164],{46378:function(n,e,t){t.r(e);var u=t(74698),a=(t(44194),t(31549));e.default=function(){return(0,a.jsx)(a.Fragment,{children:(0,a.jsx)(u.Outlet,{})})}}}]);
