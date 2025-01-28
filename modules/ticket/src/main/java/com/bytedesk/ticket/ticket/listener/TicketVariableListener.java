4bdff8afb79c78dfbbcbf048e98ffcf5214d10e0
jack ning <github@bytedesk.com>
2025-01-28 10:58:30 +0800

    update

--- modules/ticket/src/main/java/com/bytedesk/ticket/ticket/listener/TicketVariableListener.java
+++ modules/ticket/src/main/java/com/bytedesk/ticket/ticket/listener/TicketVariableListener.java
@@ -1,37 +1,33 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2025-01-28 10:20:57
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-01-28 10:55:25
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ * 
+ * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
+ */
 package com.bytedesk.ticket.ticket.listener;
 
-import org.flowable.engine.delegate.VariableListener;
-import org.flowable.variable.service.delegate.VariableListenerDelegate;
+import org.flowable.engine.delegate.ExecutionListener;
+import org.flowable.engine.delegate.DelegateExecution;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
-public class TicketVariableListener implements VariableListener {
+public class TicketVariableListener implements ExecutionListener {
     
     @Override
-    public void onCreate(VariableListenerDelegate delegate) {
-        String variableName = delegate.getVariableName();
-        Object variableValue = delegate.getValue();
-        log.info("Variable created: {} = {}", variableName, variableValue);
-        // TODO: 处理变量创建事件
-    }
-
-    @Override
-    public void onUpdate(VariableListenerDelegate delegate) {
-        String variableName = delegate.getVariableName();
-        Object oldValue = delegate.getOldValue();
-        Object newValue = delegate.getValue();
-        log.info("Variable updated: {} from {} to {}", variableName, oldValue, newValue);
-        // TODO: 处理变量更新事件
-    }
-
-    @Override
-    public void onDelete(VariableListenerDelegate delegate) {
-        String variableName = delegate.getVariableName();
-        Object value = delegate.getValue();
-        log.info("Variable deleted: {} = {}", variableName, value);
-        // TODO: 处理变量删除事件
+    public void notify(DelegateExecution execution) {
+        String eventName = execution.getEventName();
+        String processInstanceId = execution.getProcessInstanceId();
+        
+        log.info("Variable event: {} in process: {}", eventName, processInstanceId);
     }
 } 
