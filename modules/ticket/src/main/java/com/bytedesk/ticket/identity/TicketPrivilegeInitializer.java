bae49a2709160d2cfc6140cf6d803891ec672382
jack ning <github@bytedesk.com>
2025-01-30 14:59:51 +0800

    update

--- modules/ticket/src/main/java/com/bytedesk/ticket/identity/TicketPrivilegeInitializer.java
+++ modules/ticket/src/main/java/com/bytedesk/ticket/identity/TicketPrivilegeInitializer.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-01-30 14:02:26
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-01-30 14:12:40
+ * @LastEditTime: 2025-01-30 14:58:53
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -11,12 +11,10 @@
  * 
  * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
  */
-package com.bytedesk.ticket.config;
+package com.bytedesk.ticket.identity;
 
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
-
-import com.bytedesk.ticket.identity.TicketIdentityService;
 
 import lombok.RequiredArgsConstructor;
 
