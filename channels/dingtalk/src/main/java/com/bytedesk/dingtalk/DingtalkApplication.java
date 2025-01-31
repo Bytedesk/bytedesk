a76bad0590692f38f4c774513989f09ae1ebc2bc
jack ning <github@bytedesk.com>
2025-01-31 19:06:35 +0800

    update

--- channels/dingtalk/src/main/java/com/bytedesk/dingtalk/DingtalkApplication.java
+++ channels/dingtalk/src/main/java/com/bytedesk/dingtalk/DingtalkApplication.java
@@ -1,13 +1,26 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2024-05-07 12:11:42
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-01-31 18:59:58
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ * 
+ * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
+ */
 package com.bytedesk.dingtalk;
 
-import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DingtalkApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DingtalkApplication.class, args);
-	}
+	// public static void main(String[] args) {
+	// 	SpringApplication.run(DingtalkApplication.class, args);
+	// }
 
 }
