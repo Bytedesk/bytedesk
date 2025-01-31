8098c8beaaa528a68061773225ad11f8da16b80b
jack ning <github@bytedesk.com>
2025-01-31 23:07:37 +0800

    update

--- starter/src/main/java/com/bytedesk/starter/StarterApplication.java
+++ starter/src/main/java/com/bytedesk/starter/StarterApplication.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 15:02:58
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-01-18 21:09:30
+ * @LastEditTime: 2025-01-31 23:05:57
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
--- starter/vite.config.ts
+++
@@ -1,4 +0,0 @@
-export default defineConfig({
-  base: '/notebase/',  // 确保这个配置存在
-  // ... 其他配置
-}) 
