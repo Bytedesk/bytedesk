4c46721f439fc76d8ce2a03010d059140eeded7c
jack ning <github@bytedesk.com>
2025-02-01 18:37:11 +0800

    update

--- modules/core/src/main/java/com/bytedesk/core/workflow/flow/model/block/model/BlockOptions.java
+++
@@ -1,8 +0,0 @@
-package com.bytedesk.core.workflow.flow.model.block.model;
-
-import lombok.Data;
-
-@Data
-public abstract class BlockOptions {
-    // 基础选项字段
-} 
--- modules/core/src/main/java/com/bytedesk/core/workflow/flow/model/block/model/options/RuntimeConfig.java
+++
@@ -1,9 +0,0 @@
-package com.bytedesk.core.workflow.flow.model.block.model.options;
-
-import lombok.Data;
-
-@Data
-public class RuntimeConfig {
-    private Integer timeout;  // 超时时间(ms)
-    private Integer memory;   // 内存限制(MB)
-} 
