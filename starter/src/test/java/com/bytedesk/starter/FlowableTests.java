67020127866a560aeca068be97425b1cabd4ce4f
jack ning <github@bytedesk.com>
2025-02-02 13:26:45 +0800

    Update FlowableTests.java

--- starter/src/test/java/com/bytedesk/starter/FlowableTests.java
+++ starter/src/test/java/com/bytedesk/starter/FlowableTests.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-02-02 11:21:45
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-02 12:00:21
+ * @LastEditTime: 2025-02-02 12:28:09
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
