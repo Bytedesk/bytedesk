2a941a485966d27a2e6e3b808042cc35934c7965
jack ning <github@bytedesk.com>
2025-02-03 08:35:00 +0800

    update

--- modules/ticket/src/main/java/com/bytedesk/ticket/config/FlowableConfig.java
+++ modules/ticket/src/main/java/com/bytedesk/ticket/config/FlowableConfig.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-01-15 21:33:51
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-01-30 15:12:39
+ * @LastEditTime: 2025-02-03 08:28:53
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -26,8 +26,18 @@ public class FlowableConfig {
         return engineConfiguration -> {
             engineConfiguration.setEnableProcessDefinitionInfoCache(true);
             engineConfiguration.setDatabaseSchemaUpdate("true");
-            // engineConfiguration.setEnableHistoryCleaning(false);
-            // engineConfiguration.setHistoryLevel(HistoryLevel.AUDIT);
         };
     }
+    
+    // 可选：实现多租户数据源
+    // private MultiTenantDataSource createMultiTenantDataSource() {
+    //     // 实现多租户数据源逻辑
+    //     return new MultiTenantDataSource() {
+    //         @Override
+    //         public DataSource getDataSource(String tenantId) {
+    //             // 根据tenantId返回对应的数据源
+    //             return null;
+    //         }
+    //     };
+    // }
 } 
--- modules/ticket/src/main/java/com/bytedesk/ticket/service/TicketDmnService.java
+++ modules/ticket/src/main/java/com/bytedesk/ticket/service/TicketDmnService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-01-21 12:45:10
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-02 23:54:17
+ * @LastEditTime: 2025-02-03 08:29:35
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -24,31 +24,40 @@ import lombok.RequiredArgsConstructor;
 import java.util.HashMap;
 import java.util.Map;
 
+
 @Service
 @RequiredArgsConstructor
 public class TicketDmnService {
     
     private final DmnEngine dmnEngine;
+    // private final IdentityService identityService;
     
     public void evaluateTicketPriority(TicketEntity ticket) {
-        Map<String, Object> variables = new HashMap<>();
-        // variables.put("customerLevel", ticket.getCustomerLevel());
-        // variables.put("issueType", ticket.getIssueType());
-        // variables.put("impactLevel", ticket.getImpactLevel());
+        // 设置当前租户
+        // identityService.setTenantId(ticket.getTenantId());
         
-        DmnDecisionService decisionService = dmnEngine.getDmnDecisionService();
-        Map<String, Object> result = decisionService.createExecuteDecisionBuilder()
-            .decisionKey("ticketPriorityRules")
-            .variables(variables)
-            .executeDecision()
-            .get(0);
-        
-        String priority = (String) result.get("priority");
-        // String slaTime = (String) result.get("slaTime");
-        
-        // 更新工单优先级和SLA时间
-        ticket.setPriority(priority);
-        // ticket.setSlaTime(slaTime);
+        try {
+            Map<String, Object> variables = new HashMap<>();
+            // variables.put("tenantId", ticket.getTenantId());
+            // variables.put("customerLevel", ticket.getCustomerLevel());
+            // variables.put("issueType", ticket.getIssueType());
+            // variables.put("impactLevel", ticket.getImpactLevel());
+            
+            DmnDecisionService decisionService = dmnEngine.getDmnDecisionService();
+            Map<String, Object> result = decisionService.createExecuteDecisionBuilder()
+                .decisionKey("ticketPriorityRules")
+                // .tenantId(ticket.getTenantId())  // 指定租户
+                .variables(variables)
+                .executeDecision()
+                .get(0);
+            
+            String priority = (String) result.get("priority");
+            ticket.setPriority(priority);
+            
+        } finally {
+            // 清除租户上下文
+            // identityService.clearAuthentication();
+        }
     }
     
     // public Map<String, Object> determineAssignment(String category, String priority) {
