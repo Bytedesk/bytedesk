829e7fba6d71ec5aeff4881ee47617948f45c1b8
jack ning <github@bytedesk.com>
2025-02-03 13:22:23 +0800

    update

--- modules/kbase/src/main/java/com/bytedesk/kbase/notebase/NotebasePermissions.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/notebase/NotebasePermissions.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-11-05 17:07:03
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-11-06 21:37:19
+ * @LastEditTime: 2025-02-03 13:20:53
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -15,11 +15,11 @@ package com.bytedesk.kbase.notebase;
 
 public class NotebasePermissions {
 
-    public static final String Notebase_PREFIX = "Notebase_";
+    public static final String NOTEBASE_PREFIX = "NOTEBASE_";
     // Notebase permissions
-    public static final String Notebase_CREATE = "hasAuthority('Notebase_CREATE')";
-    public static final String Notebase_READ = "hasAuthority('Notebase_READ')";
-    public static final String Notebase_UPDATE = "hasAuthority('Notebase_UPDATE')";
-    public static final String Notebase_DELETE = "hasAuthority('Notebase_DELETE')";
-    public static final String Notebase_EXPORT = "hasAuthority('Notebase_EXPORT')";
+    public static final String NOTEBASE_CREATE = "hasAuthority('NOTEBASE_CREATE')";
+    public static final String NOTEBASE_READ = "hasAuthority('NOTEBASE_READ')";
+    public static final String NOTEBASE_UPDATE = "hasAuthority('NOTEBASE_UPDATE')";
+    public static final String NOTEBASE_DELETE = "hasAuthority('NOTEBASE_DELETE')";
+    public static final String NOTEBASE_EXPORT = "hasAuthority('NOTEBASE_EXPORT')";
 }
