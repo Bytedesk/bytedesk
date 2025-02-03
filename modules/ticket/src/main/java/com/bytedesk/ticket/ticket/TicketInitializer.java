6faa2bf2756e2ef77f076dfaf868396db4b891ff
jack ning <github@bytedesk.com>
2025-02-03 13:40:27 +0800

    update

--- modules/ticket/src/main/java/com/bytedesk/ticket/ticket/TicketInitializer.java
+++ modules/ticket/src/main/java/com/bytedesk/ticket/ticket/TicketInitializer.java
@@ -1,17 +1,17 @@
 /*
  * @Author: jackning 270580156@qq.com
- * @Date: 2024-11-06 21:43:58
+ * @Date: 2025-02-03 13:34:21
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-03 13:36:36
+ * @LastEditTime: 2025-02-03 13:37:25
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
- *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
  *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
  *  contact: 270580156@qq.com 
- *  联系：270580156@qq.com
- * Copyright (c) 2024 by bytedesk.com, All Rights Reserved. 
+ * 
+ * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
  */
-package com.bytedesk.kbase.notebase;
+package com.bytedesk.ticket.ticket;
 
 import org.springframework.beans.factory.SmartInitializingSingleton;
 import org.springframework.stereotype.Component;
@@ -22,22 +22,27 @@ import com.bytedesk.core.rbac.authority.AuthorityRequest;
 import com.bytedesk.core.rbac.authority.AuthorityService;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 @AllArgsConstructor
-public class NotebaseInitializer implements SmartInitializingSingleton {
+public class TicketInitializer implements SmartInitializingSingleton {
 
     private final AuthorityService authorityService;
 
     @Override
     public void afterSingletonsInstantiated() {
         initAuthority();
+        initTicketCategory();
+        initTicket();
     }
 
     private void initAuthority() {
         for (PermissionEnum permission : PermissionEnum.values()) {
-            String permissionValue = NotebasePermissions.NOTEBASE_PREFIX + permission.name();
+            String permissionValue = TicketPermissions.TICKET_PREFIX + permission.name();
             if (authorityService.existsByValue(permissionValue)) {
+                log.info("Ticket authority {} already exists", permissionValue);
                 continue;
             }
             AuthorityRequest authRequest = AuthorityRequest.builder()
@@ -48,6 +53,14 @@ public class NotebaseInitializer implements SmartInitializingSingleton {
             authRequest.setUid(permissionValue.toLowerCase());
             authorityService.create(authRequest);
         }
+    }
+
+    private void initTicketCategory() {
+        log.info("initTicketCategory");
+    }
+
+    private void initTicket() {
+        log.info("initTicket");
     }
 
 }
