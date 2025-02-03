7b0a3c86e9ad2cec90c37a18c502211f761e3ac6
jack ning <github@bytedesk.com>
2025-02-03 09:05:02 +0800

    update

--- modules/ticket/src/main/java/com/bytedesk/ticket/listener/TicketEventListener.java
+++ modules/ticket/src/main/java/com/bytedesk/ticket/listener/TicketEventListener.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-01-23 14:52:45
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-03 08:51:17
+ * @LastEditTime: 2025-02-03 08:57:17
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -26,6 +26,7 @@ import com.bytedesk.ticket.event.TicketCreateEvent;
 import com.bytedesk.ticket.event.TicketUpdateEvent;
 import com.bytedesk.ticket.ticket.TicketEntity;
 import com.bytedesk.ticket.ticket.TicketRepository;
+import com.bytedesk.ticket.ticket.TicketTypeEnum;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -51,11 +52,12 @@ public class TicketEventListener {
 
         // 启动流程时指定租户   
         ProcessInstance processInstance = runtimeService.createProcessInstanceBuilder()
-        .processDefinitionKey(TicketConsts.TICKET_PROCESS_KEY_AGENT)
+        .processDefinitionKey(ticket.getType().equals(TicketTypeEnum.AGENT.name()) 
+            ? TicketConsts.TICKET_PROCESS_KEY_AGENT : TicketConsts.TICKET_PROCESS_KEY_GROUP)
         .tenantId(ticket.getOrgUid())
         .variables(variables)
         .start();
-        
+
         // 设置工单的流程实例ID
         ticket.setProcessInstanceId(processInstance.getId());
         ticketRepository.save(ticket);
