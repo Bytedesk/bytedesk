7ea844984822b8b6e748ae3dd00fea42292b30ca
jack ning <github@bytedesk.com>
2025-02-03 22:45:43 +0800

    update

--- modules/ticket/src/main/java/com/bytedesk/ticket/ticket/TicketCategories.java
+++ modules/ticket/src/main/java/com/bytedesk/ticket/ticket/TicketCategories.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-02-03 13:42:18
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-03 22:21:52
+ * @LastEditTime: 2025-02-03 22:30:55
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -23,11 +23,11 @@ public class TicketCategories {
      */
     public static final String[] TECHNICAL_SUPPORT = {
         I18Consts.I18N_PREFIX + "ticket.category.technical_support",              // parent
-        I18Consts.I18N_PREFIX + "ticket.category.technical.system_error",         // children
-        I18Consts.I18N_PREFIX + "ticket.category.technical.software_issue",
-        I18Consts.I18N_PREFIX + "ticket.category.technical.hardware_issue",
-        I18Consts.I18N_PREFIX + "ticket.category.technical.network_issue",
-        I18Consts.I18N_PREFIX + "ticket.category.technical.account_permission"
+        // I18Consts.I18N_PREFIX + "ticket.category.technical.system_error",         // children
+        // I18Consts.I18N_PREFIX + "ticket.category.technical.software_issue",
+        // I18Consts.I18N_PREFIX + "ticket.category.technical.hardware_issue",
+        // I18Consts.I18N_PREFIX + "ticket.category.technical.network_issue",
+        // I18Consts.I18N_PREFIX + "ticket.category.technical.account_permission"
     };
 
     /**
@@ -36,11 +36,11 @@ public class TicketCategories {
      */
     public static final String[] SERVICE_REQUEST = {
         I18Consts.I18N_PREFIX + "ticket.category.service_request",               // parent
-        I18Consts.I18N_PREFIX + "ticket.category.service.new_account",          // children
-        I18Consts.I18N_PREFIX + "ticket.category.service.permission_change",
-        I18Consts.I18N_PREFIX + "ticket.category.service.device_request",
-        I18Consts.I18N_PREFIX + "ticket.category.service.environment_setup",
-        I18Consts.I18N_PREFIX + "ticket.category.service.data_recovery"
+        // I18Consts.I18N_PREFIX + "ticket.category.service.new_account",          // children
+        // I18Consts.I18N_PREFIX + "ticket.category.service.permission_change",
+        // I18Consts.I18N_PREFIX + "ticket.category.service.device_request",
+        // I18Consts.I18N_PREFIX + "ticket.category.service.environment_setup",
+        // I18Consts.I18N_PREFIX + "ticket.category.service.data_recovery"
     };
 
     /**
@@ -49,9 +49,9 @@ public class TicketCategories {
      */
     public static final String[] CONSULTATION = {
         I18Consts.I18N_PREFIX + "ticket.category.consultation",                  // parent
-        I18Consts.I18N_PREFIX + "ticket.category.consult.product_usage",        // children
-        I18Consts.I18N_PREFIX + "ticket.category.consult.technical_solution",
-        I18Consts.I18N_PREFIX + "ticket.category.consult.business_process"
+        // I18Consts.I18N_PREFIX + "ticket.category.consult.product_usage",        // children
+        // I18Consts.I18N_PREFIX + "ticket.category.consult.technical_solution",
+        // I18Consts.I18N_PREFIX + "ticket.category.consult.business_process"
     };
 
     /**
@@ -60,9 +60,9 @@ public class TicketCategories {
      */
     public static final String[] COMPLAINT_SUGGESTION = {
         I18Consts.I18N_PREFIX + "ticket.category.complaint_suggestion",          // parent
-        I18Consts.I18N_PREFIX + "ticket.category.complaint.service_quality",     // children
-        I18Consts.I18N_PREFIX + "ticket.category.complaint.system_improvement",
-        I18Consts.I18N_PREFIX + "ticket.category.complaint.process_optimization"
+        // I18Consts.I18N_PREFIX + "ticket.category.complaint.service_quality",     // children
+        // I18Consts.I18N_PREFIX + "ticket.category.complaint.system_improvement",
+        // I18Consts.I18N_PREFIX + "ticket.category.complaint.process_optimization"
     };
 
     /**
@@ -71,11 +71,11 @@ public class TicketCategories {
      */
     public static final String[] OPERATION_MAINTENANCE = {
         I18Consts.I18N_PREFIX + "ticket.category.operation_maintenance",         // parent
-        I18Consts.I18N_PREFIX + "ticket.category.operation.system_maintenance",  // children
-        I18Consts.I18N_PREFIX + "ticket.category.operation.data_backup",
-        I18Consts.I18N_PREFIX + "ticket.category.operation.monitoring_alert",
-        I18Consts.I18N_PREFIX + "ticket.category.operation.security_issue",
-        I18Consts.I18N_PREFIX + "ticket.category.operation.performance_optimization"
+        // I18Consts.I18N_PREFIX + "ticket.category.operation.system_maintenance",  // children
+        // I18Consts.I18N_PREFIX + "ticket.category.operation.data_backup",
+        // I18Consts.I18N_PREFIX + "ticket.category.operation.monitoring_alert",
+        // I18Consts.I18N_PREFIX + "ticket.category.operation.security_issue",
+        // I18Consts.I18N_PREFIX + "ticket.category.operation.performance_optimization"
     };
 
     /**
