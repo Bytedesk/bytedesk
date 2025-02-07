2843c91435f0933d95f2e27950dd44b947f999e7
jack ning <github@bytedesk.com>
2025-02-07 21:04:41 +0800

    auto commit: 2025-02-07 21:04:40

---
+++ modules/core/src/main/java/com/bytedesk/core/config/BytedeskPropertiesResponse.java
@@ -0,0 +1,34 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2025-02-07 20:45:09
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-02-07 20:46:13
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ * 
+ * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
+ */
+package com.bytedesk.core.config;
+
+import java.io.Serializable;
+
+import lombok.Data;
+
+@Data
+public class BytedeskPropertiesResponse implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Boolean debug;
+    private String edition;
+    // 
+    private String name;
+    private String logo;
+    private String description;
+    private String version;
+    
+    
+}
