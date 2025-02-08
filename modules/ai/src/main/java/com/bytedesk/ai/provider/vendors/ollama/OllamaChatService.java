3c160803badb059189c4c18bb6f971351ba044a2
jack ning <github@bytedesk.com>
2025-02-08 20:18:39 +0800

    auto commit: 2025-02-08 20:18:38

--- modules/ai/src/main/java/com/bytedesk/ai/provider/vendors/ollama/OllamaChatService.java
+++ modules/ai/src/main/java/com/bytedesk/ai/provider/vendors/ollama/OllamaChatService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-09-27 14:58:12
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-11-11 16:34:17
+ * @LastEditTime: 2025-02-08 20:18:24
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -12,6 +12,8 @@
  * Copyright (c) 2024 by bytedesk.com, All Rights Reserved. 
  */
 package com.bytedesk.ai.provider.vendors.ollama;
+
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -23,7 +25,10 @@ import com.agentsflex.core.message.MessageStatus;
 import com.agentsflex.llm.ollama.OllamaLlm;
 import com.agentsflex.llm.ollama.OllamaLlmConfig;
 import com.alibaba.fastjson2.JSON;
+import com.bytedesk.ai.provider.LlmProviderEntity;
+import com.bytedesk.ai.provider.LlmProviderRestService;
 import com.bytedesk.ai.robot.RobotLlm;
+import com.bytedesk.core.enums.LevelEnum;
 import com.bytedesk.core.message.IMessageSendService;
 import com.bytedesk.core.message.MessageProtobuf;
 import com.bytedesk.core.message.MessageTypeEnum;
@@ -42,6 +47,9 @@ public class OllamaChatService {
 
     @Autowired
     private IMessageSendService messageSendService;
+
+    @Autowired
+    private LlmProviderRestService llmProviderRestService;
 
     public void sendSseMessage(String query, RobotLlm robotLlm, MessageProtobuf messageProtobuf) {
         //
@@ -62,23 +70,29 @@ public class OllamaChatService {
     }
 
     public void sendWsMessage(String query, RobotLlm robotLlm, MessageProtobuf messageProtobuf) {
+
+        String providerName = robotLlm.getProvider();
+        Optional<LlmProviderEntity> providerOptional = llmProviderRestService.findByName(providerName, LevelEnum.ORGANIZATION.name());
+        if (!providerOptional.isPresent()) {
+            log.error("ollama provider {} not exist", providerName);
+            return;
+        }
         //
         String prompt = robotLlm.getPrompt() + "\n" + query;
         // Llm llm = Ollama
         OllamaLlmConfig config = new OllamaLlmConfig();
-        config.setEndpoint(ollamaBaseUrl);
-        config.setModel(ollamaDefaultModel);
+        config.setEndpoint(providerOptional.get().getApiUrl());
+        config.setModel(robotLlm.getModel());
         config.setDebug(true);
 
         Llm llm = new OllamaLlm(config);
         llm.chatStream(prompt, (context, response) -> {
             // {"content":"有一","fullContent":"有一","messageContent":"有一","status":"MIDDLE"}
             // {"completionTokens":11,"content":"","fullContent":"有一只深海鱼，每天都自由地游来游去，但它却一点也不开心。\n为什么呢？\n因为它压力很大。","messageContent":"有一只深海鱼，每天都自由地游来游去，但它却一点也不开心。\n为什么呢？\n因为它压力很大。","status":"END","totalTokens":27}
-            log.info(JSON.toJSONString(response.getMessage()));
+            log.info("ollama response: {}", JSON.toJSONString(response.getMessage()));
             AiMessage aiMessage = response.getMessage();
             messageProtobuf.setType(MessageTypeEnum.STREAM);
             messageProtobuf.setContent(aiMessage.getContent());
-            // MessageUtils.notifyUser(messageProtobuf);
             messageSendService.sendProtobufMessage(messageProtobuf);
             // 
             if (aiMessage.getStatus().equals(MessageStatus.END)) {
