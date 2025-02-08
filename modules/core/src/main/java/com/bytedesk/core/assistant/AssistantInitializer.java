a49344b1d3d544aba0b387d96334f660a162ae36
jack ning <github@bytedesk.com>
2025-02-08 16:14:49 +0800

    auto commit: 2025-02-08 16:14:49

--- modules/core/src/main/java/com/bytedesk/core/assistant/AssistantInitializer.java
+++ modules/core/src/main/java/com/bytedesk/core/assistant/AssistantInitializer.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-11-05 13:43:02
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-08 15:43:57
+ * @LastEditTime: 2025-02-08 15:55:32
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -67,6 +67,18 @@ public class AssistantInitializer implements SmartInitializingSingleton {
         clipboardAssistantRequest.setLevel(LevelEnum.PLATFORM.name());
         assistantService.create(clipboardAssistantRequest);
 
+        // 
+        // TODO: query扩充，用户填写一个问题，系统根据问题生成多个类似不同问法，用LLM生成一批同义句
+        // AssistantRequest queryExpansionAssistantRequest = AssistantRequest.builder()
+        //         .topic(TopicUtils.TOPIC_QUERY_EXPANSION_ASSISTANT)
+        //         .nickname(I18Consts.I18N_QUERY_EXPANSION_ASSISTANT_NAME)
+        //         .avatar(AvatarConsts.getDefaultQueryExpansionAssistantAvatarUrl())
+        //         .description(I18Consts.I18N_QUERY_EXPANSION_ASSISTANT_DESCRIPTION)
+        //         .build();
+        // queryExpansionAssistantRequest.setUid(BytedeskConsts.DEFAULT_QUERY_EXPANSION_ASSISTANT_UID);
+        // queryExpansionAssistantRequest.setLevel(LevelEnum.PLATFORM.name());
+        // assistantService.create(queryExpansionAssistantRequest);
+
         // https://mp.weixin.qq.com/s/nu-2ji9NOszcZ_6SWd469A
         // TODO: 意图改写
         AssistantRequest intentRewriteAssistantRequest = AssistantRequest.builder()
@@ -79,6 +91,47 @@ public class AssistantInitializer implements SmartInitializingSingleton {
         intentRewriteAssistantRequest.setLevel(LevelEnum.PLATFORM.name());
         assistantService.create(intentRewriteAssistantRequest);
 
+        /**
+         * https://mp.weixin.qq.com/s/nu-2ji9NOszcZ_6SWd469A
+         prompt = """
+                # 角色
+                你是一位意图样本生成专家，擅长根据给定的模板生成意图及对应的槽位信息。你能够准确地解析用户输入，并将其转化为结构化的意图和槽位数据。
+
+                ## 技能
+                ### 技能1：解析用户指令
+                - **任务**：根据用户提供的自然语言指令，识别出用户的意图。
+
+                ### 技能2：生成结构化意图和槽位信息
+                - 意图分类：video_search,music_search,information_search
+                - 槽位分类：
+                -- information_search: classification,video_category,video_name,video_season
+                -- music_search: music_search,music_singer,music_tag,music_release_time
+                -- video_search: video_actor,video_name,video_episode
+                - **任务**：将解析出的用户意图转换为结构化的JSON格式。
+                - 确保每个意图都有相应的槽位信息，不要自行编造槽位。
+                - 槽位信息应包括所有必要的细节，如演员、剧名、集数、歌手、音乐标签、发布时间等。
+
+                ### 技能3：在线搜索
+                - 如果遇到关于电影情节的描述，可以调用搜索引擎获取到电影名、演员等信息称补充到actor,name等槽位中
+
+                ### 输出示例
+                - 以JSON格式输出，例如：
+                -"这周杭州的天气如何明天北京有雨吗"：{'infor_search':{'extra_info':['这周杭州的天气如何明天北京有雨吗']}}
+                -"我一直在追赵丽颖的楚乔传我看到第二十集了它已经更新了吗我可以看下一集吗"：{'video_search':{'video_actor':['赵丽颖'],'video_name':['楚乔传'],'video_episode':['第21集'],'extra_info':['我一直在追赵丽颖的楚乔传我看到第二十集了它已经更新了吗我可以看下一集吗']}}
+
+                ## 限制
+                - 只处理与意图生成相关的任务。
+                - 生成的意图和槽位信息必须准确且完整。
+                - 在解析过程中，确保理解用户的意图并正确映射到相应的服务类型
+                - 如果遇到未知的服务类型或槽位信息，可以通过调用搜索工具进行补充和确认。
+                - 直接输出Json，不要输出其他思考过程
+                生成后的用于知识库的数据集格式如下：
+
+                [{
+                "instruction":"播放一首七仔的歌曲我想听他的经典老歌最好是70年代的音乐风格",
+                "output":"{'music_search':{'singer':['张学友'],'music_tag':['经典老歌'],'release_time':['70年代']}}"
+                }]
+         */
         // TODO: 意图识别:意图槽位/意图置信度
         AssistantRequest intentClassificationAssistantRequest = AssistantRequest.builder()
                 .topic(TopicUtils.TOPIC_INTENT_CLASSIFICATION_ASSISTANT)
@@ -100,6 +153,10 @@ public class AssistantInitializer implements SmartInitializingSingleton {
         emotionAssistantRequest.setUid(BytedeskConsts.DEFAULT_EMOTION_ASSISTANT_UID);
         emotionAssistantRequest.setLevel(LevelEnum.PLATFORM.name());
         assistantService.create(emotionAssistantRequest);
+
+        // TODO: 评测
+        
+        // TODO: 质检
     }
     
     
