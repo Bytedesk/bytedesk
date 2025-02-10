45ebbc259cb7f2cb58926e7c8d35067b424ae86d
jack ning <github@bytedesk.com>
2025-02-10 20:37:45 +0800

    auto commit: 2025-02-10 20:37:44

--- modules/core/src/main/java/com/bytedesk/core/action/ActionRestService.java
+++ modules/core/src/main/java/com/bytedesk/core/action/ActionRestService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-04-25 15:41:47
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-12-04 14:09:24
+ * @LastEditTime: 2025-02-10 20:35:59
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
