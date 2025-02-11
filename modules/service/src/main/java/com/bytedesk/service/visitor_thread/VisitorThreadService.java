9bfe0a9a23d935f4adccc1e2e3bed870c236feb0
jack ning <github@bytedesk.com>
2025-02-11 11:24:31 +0800

    update cicd/scripts: mod        4 files

--- modules/service/src/main/java/com/bytedesk/service/visitor_thread/VisitorThreadService.java
+++ modules/service/src/main/java/com/bytedesk/service/visitor_thread/VisitorThreadService.java
@@ -2,14 +2,14 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-06-29 13:08:52
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-10 22:35:50
+ * @LastEditTime: 2025-02-11 11:18:08
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
- *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
  *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
  *  contact: 270580156@qq.com 
- *  联系：270580156@qq.com
- * Copyright (c) 2024 by bytedesk.com, All Rights Reserved. 
+ * 
+ * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
  */
 package com.bytedesk.service.visitor_thread;
 
