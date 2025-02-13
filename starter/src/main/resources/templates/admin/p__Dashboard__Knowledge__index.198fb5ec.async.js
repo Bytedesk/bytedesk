82591b46ec93e00c47f19e8025f011669c3b4c7e
jack ning <github@bytedesk.com>
2025-02-13 23:46:09 +0800

    update starter/src: add      114 mod        4 del      192 files

--- starter/src/main/resources/templates/admin/p__Dashboard__Knowledge__index.198fb5ec.async.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkadmin=self.webpackChunkadmin||[]).push([[363],{57074:function(n,e,t){t.r(e);var u=t(74698),a=(t(44194),t(31549));e.default=function(){return(0,a.jsx)(a.Fragment,{children:(0,a.jsx)(u.Outlet,{})})}}}]);
--- starter/src/main/resources/templates/admin/p__Dashboard__Team__Thread__ThreadTable.eab455eb.async.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkadmin=self.webpackChunkadmin||[]).push([[9077],{52051:function(e,n,t){t.r(n);var a=t(73030),u=t(31549);n.default=function(){return(0,u.jsx)(u.Fragment,{children:(0,u.jsx)(a.Z,{type:"team"})})}}}]);
