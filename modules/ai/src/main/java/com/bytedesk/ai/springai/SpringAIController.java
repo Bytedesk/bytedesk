0dd3a6c6d1c83a121b84e176ca15cf9f40ba665d
jack ning <github@bytedesk.com>
2025-02-13 10:13:52 +0800

    update deploy/cloud: mod        4 files

--- modules/ai/src/main/java/com/bytedesk/ai/springai/SpringAIController.java
+++ modules/ai/src/main/java/com/bytedesk/ai/springai/SpringAIController.java
@@ -1,6 +1,23 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2025-02-12 12:15:53
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-02-13 10:12:23
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ * 
+ * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
+ */
 package com.bytedesk.ai.springai;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +35,10 @@ public class SpringAIController {
 
     private final ChatClient chatClient;
 
+    private final VectorStore vectorStore;
+
+    private final ChatModel chatModel;
+
     // http://127.0.0.1:9003/spring/ai/completion?message=hello&voice=agent
     // https://docs.spring.io/spring-ai/reference/api/chatclient.html
     @GetMapping("/completion")
@@ -31,6 +52,21 @@ public class SpringAIController {
                 .content();
         
         return ResponseEntity.ok(JsonResult.success(completion));
+    }
+
+    // rag 
+    // http://127.0.0.1:9003/spring/ai/rag?message=什么时间考试？
+    @GetMapping("/rag")
+    ResponseEntity<JsonResult<?>> rag(
+            @RequestParam(value = "message", defaultValue = "什么时间考试？") String message) {
+
+        ChatResponse response = ChatClient.builder(chatModel)
+                .build().prompt()
+                .advisors(new QuestionAnswerAdvisor(vectorStore))
+                .user(message)
+                .call()
+                .chatResponse();
+        return ResponseEntity.ok(JsonResult.success(response));
     }
 
 }
