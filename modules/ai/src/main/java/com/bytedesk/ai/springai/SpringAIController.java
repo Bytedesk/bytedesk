d02507ed517b911d53c9cca34e2adfe34f1d89fd
jack ning <github@bytedesk.com>
2025-02-13 10:43:58 +0800

    update modules/ai: add        3 mod        2 files

--- modules/ai/src/main/java/com/bytedesk/ai/springai/SpringAIController.java
+++ modules/ai/src/main/java/com/bytedesk/ai/springai/SpringAIController.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-02-12 12:15:53
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-13 10:12:23
+ * @LastEditTime: 2025-02-13 10:21:49
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -17,6 +17,7 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -50,23 +51,53 @@ public class SpringAIController {
                 .user(message)
                 .call()
                 .content();
-        
+
         return ResponseEntity.ok(JsonResult.success(completion));
     }
 
-    // rag 
+    // rag
+    // https://docs.spring.io/spring-ai/reference/api/retrieval-augmented-generation.html#_questionansweradvisor
     // http://127.0.0.1:9003/spring/ai/rag?message=什么时间考试？
     @GetMapping("/rag")
     ResponseEntity<JsonResult<?>> rag(
             @RequestParam(value = "message", defaultValue = "什么时间考试？") String message) {
-
+        // 创建qaAdvisor
+        var qaAdvisor = new QuestionAnswerAdvisor(
+                this.vectorStore,
+                SearchRequest.builder()
+                        .similarityThreshold(0.8d)
+                        .topK(6)
+                        .build());
+        // 使用chatClient
         ChatResponse response = ChatClient.builder(chatModel)
-                .build().prompt()
-                .advisors(new QuestionAnswerAdvisor(vectorStore))
+                .build()
+                .prompt()
+                .advisors(qaAdvisor)
                 .user(message)
                 .call()
                 .chatResponse();
         return ResponseEntity.ok(JsonResult.success(response));
     }
+
+    // filter
+    // http://127.0.0.1:9003/spring/ai/filter?message=什么时间考试？
+    @GetMapping("/filter")
+    ResponseEntity<JsonResult<?>> filter(
+            @RequestParam(value = "message", defaultValue = "什么时间考试？") String message) {
+
+        ChatClient chatClient = ChatClient.builder(chatModel)
+                .defaultAdvisors(new QuestionAnswerAdvisor(vectorStore, SearchRequest.builder().build()))
+                .build();
+
+        // Update filter expression at runtime
+        String content = chatClient.prompt()
+                .user(message)
+                .advisors(a -> a.param(QuestionAnswerAdvisor.FILTER_EXPRESSION, "type == 'Spring'"))
+                .call()
+                .content();
+
+        return ResponseEntity.ok(JsonResult.success(content));
+    }
+
 
 }
