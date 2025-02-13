806b6bb93d087a56ce8c5cea93bbfa2887d2a726
jack ning <github@bytedesk.com>
2025-02-13 15:20:16 +0800

    update modules/ticket: add        1 mod        1 files

---
+++ modules/ticket/src/main/java/com/bytedesk/ticket/ticket/TicketExcel.java
@@ -0,0 +1,78 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2024-05-22 22:12:03
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-02-13 15:09:22
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ *  联系：270580156@qq.com
+ * Copyright (c) 2024 by bytedesk.com, All Rights Reserved. 
+ */
+package com.bytedesk.ticket.ticket;
+
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.write.style.ColumnWidth;
+
+import lombok.Data;
+
+// https://github.com/alibaba/easyexcel
+// https://easyexcel.opensource.alibaba.com/docs/current/
+@Data
+public class TicketExcel {
+
+    @ExcelProperty(value = "标题")
+    @ColumnWidth(20)
+    private String title;
+
+    @ExcelProperty(value = "描述")
+    @ColumnWidth(20)
+    private String description;
+
+    @ExcelProperty(value = "状态")
+    @ColumnWidth(20)
+    private String status;
+
+    @ExcelProperty(value = "优先级")
+    @ColumnWidth(20)
+    private String priority;
+
+    @ExcelProperty(value = "类型")
+    @ColumnWidth(20)
+    private String type;
+
+    @ExcelProperty(value = "服务会话Topic")
+    @ColumnWidth(20)
+    private String serviceThreadTopic;
+
+    @ExcelProperty(value = "会话Uid")
+    @ColumnWidth(20)
+    private String threadUid;
+
+    @ExcelProperty(value = "客户")
+    @ColumnWidth(20)
+    private String user;
+
+    @ExcelProperty(value = "技能组")
+    @ColumnWidth(20)
+    private String workgroup;
+
+    @ExcelProperty(value = "指定处理人")
+    @ColumnWidth(20)
+    private String assignee;
+
+    @ExcelProperty(value = "报告人")
+    @ColumnWidth(20)
+    private String reporter;
+
+    @ExcelProperty(value = "创建时间")
+    @ColumnWidth(20)
+    private String createdAt;
+
+    @ExcelProperty(value = "更新时间")
+    @ColumnWidth(20)
+    private String updatedAt;
+
+}
--- modules/ticket/src/main/java/com/bytedesk/ticket/ticket/TicketRestController.java
+++ modules/ticket/src/main/java/com/bytedesk/ticket/ticket/TicketRestController.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-01-16 14:56:28
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-13 14:39:12
+ * @LastEditTime: 2025-02-13 15:09:46
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -13,6 +13,10 @@
  */
 package com.bytedesk.ticket.ticket;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,10 +24,18 @@ import org.springframework.web.multipart.MultipartFile;
 
 import lombok.AllArgsConstructor;
 
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
+import com.bytedesk.core.action.ActionAnnotation;
 import com.bytedesk.core.base.BaseRestController;
+import com.bytedesk.core.utils.DateUtils;
 import com.bytedesk.core.utils.JsonResult;
 import com.bytedesk.ticket.attachment.TicketAttachmentEntity;
 import com.bytedesk.ticket.comment.TicketCommentRequest;
+
+import jakarta.servlet.http.HttpServletResponse;
+
 import com.bytedesk.ticket.comment.TicketCommentEntity;
 
 @RestController
@@ -32,6 +44,8 @@ import com.bytedesk.ticket.comment.TicketCommentEntity;
 public class TicketRestController extends BaseRestController<TicketRequest> {
     
     private final TicketRestService ticketService;
+
+    private final ModelMapper modelMapper;
 
     @Override
     public ResponseEntity<?> queryByOrg(TicketRequest request) {
@@ -90,6 +104,53 @@ public class TicketRestController extends BaseRestController<TicketRequest> {
     @PostMapping("/{id}/attachments")
     public TicketAttachmentEntity uploadAttachment(@PathVariable Long id, @RequestParam MultipartFile file) {
         return ticketService.uploadAttachment(id, file);
+    }
+
+
+    // https://github.com/alibaba/easyexcel
+    // https://easyexcel.opensource.alibaba.com/docs/current/
+    @ActionAnnotation(title = "ticket", action = "export", description = "export ticket")
+    @GetMapping("/export")
+    public Object export(TicketRequest request, HttpServletResponse response) {
+        // query data to export
+        Page<TicketResponse> ticketPage = ticketService.queryByOrg(request);
+        // 
+        try {
+            //
+            response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+            response.setCharacterEncoding("utf-8");
+            // download filename
+            String fileName = "Ticket-" + DateUtils.formatDatetimeUid() + ".xlsx";
+            response.setHeader("Content-disposition", "attachment;filename*=utf-8''" + fileName);
+
+            // 转换数据
+            List<TicketExcel> excelList = ticketPage.getContent().stream().map(ticketResponse -> {
+                return modelMapper.map(ticketResponse, TicketExcel.class);
+            }).toList();
+
+            // write to excel
+            EasyExcel.write(response.getOutputStream(), TicketExcel.class)
+                    .autoCloseStream(Boolean.FALSE)
+                    .sheet("Ticket")
+                    .doWrite(excelList);
+
+        } catch (Exception e) {
+            // reset response
+            response.reset();
+            response.setContentType("application/json");
+            response.setCharacterEncoding("utf-8");
+            //
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("status", "failure");
+            jsonObject.put("message", "download failed " + e.getMessage());
+            try {
+                response.getWriter().println(JSON.toJSONString(jsonObject));
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+
+        return "";
     }
 
     
