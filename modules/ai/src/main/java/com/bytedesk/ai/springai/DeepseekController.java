4e6875a44ee52acbeb19b7084b25cc7a529accfa
jack ning <github@bytedesk.com>
2025-02-13 13:48:40 +0800

    update starter/src: add        1 mod        8 files

---
+++ modules/ai/src/main/java/com/bytedesk/ai/springai/DeepseekController.java
@@ -0,0 +1,63 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2025-02-13 13:41:56
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-02-13 13:44:15
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ * 
+ * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
+ */
+package com.bytedesk.ai.springai;
+
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bytedesk.core.utils.JsonResult;
+
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+
+/**
+ * deepseek
+ * https://docs.spring.io/spring-ai/reference/api/chat/deepseek-chat.html#chat-options
+ */
+@RestController
+@RequestMapping("/deepseek")
+@RequiredArgsConstructor
+public class DeepseekController {
+    
+    // deepseek chat model
+    // 在配置文件中修改：
+    // spring.ai.openai.base-url=https://api.deepseek.com
+    // spring.ai.openai.api-key=sk-xxx
+    // spring.ai.openai.chat.options.model=deepseek-chat
+    // spring.ai.openai.chat.options.temperature=0.7
+    // The DeepSeek API doesn't support embeddings, so we need to disable it.
+    // spring.ai.openai.embedding.enabled=false
+    private final OpenAiChatModel chatModel;
+
+    // http://127.0.0.1:9003/deepseek/ai/generate?message=hello
+    @GetMapping("/ai/generate")
+    public ResponseEntity<JsonResult<?>> generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return ResponseEntity.ok(JsonResult.success(this.chatModel.call(message)));
+    }
+
+    // http://127.0.0.1:9003/deepseek/ai/generateStream?message=hello
+    @GetMapping("/ai/generateStream")
+    public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        Prompt prompt = new Prompt(new UserMessage(message));
+        return this.chatModel.stream(prompt);
+    }
+    
+}
