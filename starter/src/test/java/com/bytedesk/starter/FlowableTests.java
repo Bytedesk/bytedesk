c948e2830a236dc8b1a85eafd7874b92570e3205
jack ning <github@bytedesk.com>
2025-02-14 14:16:57 +0800

    update starter/src: mod        2 files

--- starter/src/test/java/com/bytedesk/starter/FlowableTests.java
+++ starter/src/test/java/com/bytedesk/starter/FlowableTests.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-02-02 11:21:45
  * @LastEditors: jack ning github@bytedesk.com
- * @LastEditTime: 2025-02-14 13:13:14
+ * @LastEditTime: 2025-02-14 14:10:46
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -189,6 +189,159 @@ public class FlowableTests {
                 .tenantId(orgUid)                         // 设置租户ID
                 .deploy();
         log.info("部署租户流程成功: deploymentId={}, tenantId={}", deployment.getId(), deployment.getTenantId());
+    }
+
+    // 通过传入字符串作为流程图的定义
+    @Test
+    public void testAddProcessDefinitionByStringWithTenant() {
+        String orgUid = "df_org_uid";
+        
+        // BPMN流程定义XML字符串
+        String bpmnXml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+            "<definitions xmlns=\"http://www.omg.org/spec/BPMN/20100524/MODEL\"\n" +
+            "  xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
+            "  xmlns:flowable=\"http://flowable.org/bpmn\"\n" +
+            "  targetNamespace=\"Examples\">\n" +
+            "\n" +
+            "  <process id=\"simple-leave\" name=\"简单请假流程\">\n" +
+            "    <startEvent id=\"start\" name=\"开始\"/>\n" +
+            "    <sequenceFlow id=\"flow1\" sourceRef=\"start\" targetRef=\"approve\"/>\n" +
+            "    <userTask id=\"approve\" name=\"领导审批\" flowable:candidateGroups=\"managers\"/>\n" +
+            "    <sequenceFlow id=\"flow2\" sourceRef=\"approve\" targetRef=\"end\"/>\n" +
+            "    <endEvent id=\"end\" name=\"结束\"/>\n" +
+            "  </process>\n" +
+            "</definitions>";
+        
+        // 部署流程
+        Deployment deployment = repositoryService.createDeployment()
+                .name("简单请假流程")
+                .addString("simple-leave.bpmn20.xml", bpmnXml)
+                .tenantId(orgUid)
+                .deploy();
+        
+        // 验证部署结果
+        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
+                .deploymentId(deployment.getId())
+                .singleResult();
+        
+        log.info("部署租户流程成功: deploymentId={}, tenantId={}, processKey={}, version={}", 
+            deployment.getId(),
+            deployment.getTenantId(),
+            processDefinition.getKey(),
+            processDefinition.getVersion());
+    }
+
+    // 通过传入字符串作为流程图的定义，更新已有流程定义
+    @Test
+    public void testUpdateProcessDefinitionByStringWithTenant() {
+        String orgUid = "df_org_uid";
+        String processKey = "simple-leave";
+        
+        // 新的BPMN流程定义XML字符串 - 增加了一个审批节点
+        String bpmnXml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+            "<definitions xmlns=\"http://www.omg.org/spec/BPMN/20100524/MODEL\"\n" +
+            "  xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
+            "  xmlns:flowable=\"http://flowable.org/bpmn\"\n" +
+            "  targetNamespace=\"Examples\">\n" +
+            "\n" +
+            "  <process id=\"simple-leave\" name=\"简单请假流程-v2\">\n" +
+            "    <startEvent id=\"start\" name=\"开始\"/>\n" +
+            "    <sequenceFlow id=\"flow1\" sourceRef=\"start\" targetRef=\"leader_approve\"/>\n" +
+            "    <userTask id=\"leader_approve\" name=\"直接领导审批\" flowable:candidateGroups=\"leaders\"/>\n" +
+            "    <sequenceFlow id=\"flow2\" sourceRef=\"leader_approve\" targetRef=\"hr_approve\"/>\n" +
+            "    <userTask id=\"hr_approve\" name=\"人事审批\" flowable:candidateGroups=\"hr\"/>\n" +
+            "    <sequenceFlow id=\"flow3\" sourceRef=\"hr_approve\" targetRef=\"end\"/>\n" +
+            "    <endEvent id=\"end\" name=\"结束\"/>\n" +
+            "  </process>\n" +
+            "</definitions>";
+        
+        // 查询当前版本
+        ProcessDefinition currentProcess = repositoryService.createProcessDefinitionQuery()
+                .processDefinitionKey(processKey)
+                .processDefinitionTenantId(orgUid)
+                .latestVersion()
+                .singleResult();
+        
+        // 部署新版本流程
+        Deployment deployment = repositoryService.createDeployment()
+                .name("简单请假流程-v2")
+                .addString("simple-leave-v2.bpmn20.xml", bpmnXml)
+                .tenantId(orgUid)
+                .deploy();
+        
+        // 验证新版本
+        ProcessDefinition newProcess = repositoryService.createProcessDefinitionQuery()
+                .deploymentId(deployment.getId())
+                .singleResult();
+        
+        // 输出版本信息
+        log.info("原版本: version={}, resourceName={}", 
+            currentProcess.getVersion(),
+            currentProcess.getResourceName());
+        
+        log.info("新版本: version={}, resourceName={}", 
+            newProcess.getVersion(),
+            newProcess.getResourceName());
+        
+        // 可选：停用旧版本
+        repositoryService.suspendProcessDefinitionById(currentProcess.getId());
+        
+        // 查询所有版本
+        List<ProcessDefinition> allVersions = repositoryService.createProcessDefinitionQuery()
+                .processDefinitionKey(processKey)
+                .processDefinitionTenantId(orgUid)
+                .orderByProcessDefinitionVersion().desc()
+                .list();
+        
+        log.info("所有版本数量: {}", allVersions.size());
+        for (ProcessDefinition pd : allVersions) {
+            log.info("版本信息: version={}, resourceName={}, suspended={}", 
+                pd.getVersion(),
+                pd.getResourceName(),
+                pd.isSuspended());
+        }
+    }
+
+    // 多租户系统删除某个流程定义
+    @Test
+    public void testDeleteProcessDefinitionWithTenant() {
+        String orgUid = "df_org_uid";
+        String processKey = "simple-leave";
+        
+        // 查询指定租户的所有版本流程定义
+        List<ProcessDefinition> processes = repositoryService.createProcessDefinitionQuery()
+                .processDefinitionKey(processKey)
+                .processDefinitionTenantId(orgUid)
+                .orderByProcessDefinitionVersion().desc()
+                .list();
+        
+        log.info("删除前流程版本数量: {}", processes.size());
+        
+        for (ProcessDefinition pd : processes) {
+            log.info("流程定义: deploymentId={}, version={}", pd.getDeploymentId(), pd.getVersion());
+            
+            try {
+                // 第一种方式：级联删除，会删除和当前规则相关的所有信息，包括历史
+                // repositoryService.deleteDeployment(pd.getDeploymentId(), true);
+                
+                // 第二种方式：非级联删除，如果当前规则下有正在执行的流程，则抛出异常
+                repositoryService.deleteDeployment(pd.getDeploymentId(), false);
+                
+                log.info("成功删除流程定义: deploymentId={}", pd.getDeploymentId());
+            } catch (Exception e) {
+                log.error("删除流程定义失败: deploymentId={}, error={}", 
+                    pd.getDeploymentId(), e.getMessage());
+            }
+        }
+        
+        // 验证删除结果
+        List<ProcessDefinition> remainingProcesses = repositoryService.createProcessDefinitionQuery()
+                .processDefinitionKey(processKey)
+                .processDefinitionTenantId(orgUid)
+                .orderByProcessDefinitionVersion().desc()
+                .list();
+        
+        log.info("删除后流程版本数量: {}", remainingProcesses.size());
     }
 
     // 通过上传文件部署带租户ID的流程
