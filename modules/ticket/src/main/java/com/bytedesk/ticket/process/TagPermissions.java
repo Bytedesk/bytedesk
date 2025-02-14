efa0d7d483fb77c56e6ca890cca46a8e4d226d6f
jack ning <github@bytedesk.com>
2025-02-14 17:26:55 +0800

    update modules/ticket: add       10 mod        2 files

---
+++ modules/ticket/src/main/java/com/bytedesk/ticket/process/TagPermissions.java
@@ -0,0 +1,29 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2024-11-05 16:58:18
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-02-14 17:26:46
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ *  联系：270580156@qq.com
+ * Copyright (c) 2024 by bytedesk.com, All Rights Reserved. 
+ */
+package com.bytedesk.ticket.process;
+
+public class TicketProcessPermissions {
+
+    public static final String TAG_PREFIX = "TAG_";
+    // TicketProcess permissions
+    public static final String TAG_CREATE = "hasAuthority('TAG_CREATE')";
+    public static final String TAG_READ = "hasAuthority('TAG_READ')";
+    public static final String TAG_UPDATE = "hasAuthority('TAG_UPDATE')";
+    public static final String TAG_DELETE = "hasAuthority('TAG_DELETE')";
+    public static final String TAG_EXPORT = "hasAuthority('TAG_EXPORT')";
+
+    // 
+    
+    
+}
---
+++ modules/ticket/src/main/java/com/bytedesk/ticket/process/TicketProcessRestController.java
@@ -0,0 +1,76 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2024-05-11 18:25:36
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2024-11-23 14:20:33
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ *  联系：270580156@qq.com
+ * Copyright (c) 2024 by bytedesk.com, All Rights Reserved. 
+ */
+package com.bytedesk.core.tag;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bytedesk.core.base.BaseRestController;
+import com.bytedesk.core.rbac.role.RolePermissions;
+import com.bytedesk.core.utils.JsonResult;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/tag")
+@AllArgsConstructor
+public class TicketProcessRestController extends BaseRestController<TicketProcessRequest> {
+
+    private final TicketProcessRestService tagService;
+
+    @PreAuthorize(RolePermissions.ROLE_ADMIN)
+    @Override
+    public ResponseEntity<?> queryByOrg(TicketProcessRequest request) {
+        
+        Page<TicketProcessResponse> tags = tagService.queryByOrg(request);
+
+        return ResponseEntity.ok(JsonResult.success(tags));
+    }
+
+    @Override
+    public ResponseEntity<?> queryByUser(TicketProcessRequest request) {
+        
+        Page<TicketProcessResponse> tags = tagService.queryByUser(request);
+
+        return ResponseEntity.ok(JsonResult.success(tags));
+    }
+
+    @Override
+    public ResponseEntity<?> create(TicketProcessRequest request) {
+        
+        TicketProcessResponse tag = tagService.create(request);
+
+        return ResponseEntity.ok(JsonResult.success(tag));
+    }
+
+    @Override
+    public ResponseEntity<?> update(TicketProcessRequest request) {
+        
+        TicketProcessResponse tag = tagService.update(request);
+
+        return ResponseEntity.ok(JsonResult.success(tag));
+    }
+
+    @Override
+    public ResponseEntity<?> delete(TicketProcessRequest request) {
+        
+        tagService.delete(request);
+
+        return ResponseEntity.ok(JsonResult.success());
+    }
+    
+}
