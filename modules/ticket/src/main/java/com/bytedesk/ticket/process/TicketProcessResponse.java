486048a394330fdba805d8c432a1065d4572bab9
jack ning <github@bytedesk.com>
2025-02-15 13:00:39 +0800

    update modules/ticket: add        2 mod        2 files

--- modules/ticket/src/main/java/com/bytedesk/ticket/process/TicketProcessResponse.java
+++ modules/ticket/src/main/java/com/bytedesk/ticket/process/TicketProcessResponse.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-05-11 18:26:12
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-14 17:56:06
+ * @LastEditTime: 2025-02-15 12:59:36
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -41,6 +41,10 @@ public class TicketProcessResponse extends BaseResponse {
     // private String type;
 
     private String content;
+
+    private String userUid;
+
+    private String orgUid;
 
     private LocalDateTime createdAt;
 
