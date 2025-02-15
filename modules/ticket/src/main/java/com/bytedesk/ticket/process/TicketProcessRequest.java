9fb497e4a71a81ae601667962548656d454c20b7
jack ning <github@bytedesk.com>
2025-02-15 16:52:23 +0800

    update

--- modules/ticket/src/main/java/com/bytedesk/ticket/process/TicketProcessRequest.java
+++ modules/ticket/src/main/java/com/bytedesk/ticket/process/TicketProcessRequest.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-05-11 18:26:04
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-15 14:41:50
+ * @LastEditTime: 2025-02-15 16:45:53
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -43,7 +43,7 @@ public class TicketProcessRequest extends BaseRequest {
     // private String content;
 
     // 是否已部署流程
-    private boolean isDeployed;
+    private boolean deployed;
 
     // 部署id
     private String deploymentId;
