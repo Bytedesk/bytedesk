c344c50cabc4d0e53cc104b3e9d30b2f4bab1d72
jack ning <github@bytedesk.com>
2025-02-17 17:04:07 +0800

    update

--- modules/core/src/main/java/com/bytedesk/core/utils/MessageConvertUtils.java
+++ modules/core/src/main/java/com/bytedesk/core/utils/MessageConvertUtils.java
@@ -1,8 +1,8 @@
 /*
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-02-28 12:06:13
- * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-12-17 17:00:04
+ * @LastEditors: jack ning github@bytedesk.com
+ * @LastEditTime: 2025-02-17 16:47:24
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -19,6 +19,8 @@ import com.bytedesk.core.socket.protobuf.model.MessageProto;
 import com.google.protobuf.Message;
 import com.google.protobuf.util.JsonFormat;
 
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
 public class MessageConvertUtils {
     private MessageConvertUtils() {
     }
@@ -29,8 +31,16 @@ public class MessageConvertUtils {
 
     public static MessageProto.Message toProtoBean(MessageProto.Message.Builder targetBuilder, String json)
             throws IOException {
-        JsonFormat.parser().merge(json, targetBuilder);
-        return targetBuilder.build();
+        // JsonFormat.parser().merge(json, targetBuilder);
+        // return targetBuilder.build();
+        try {
+            // Use the builder pattern correctly
+            JsonFormat.parser().ignoringUnknownFields().merge(json, targetBuilder);
+            return targetBuilder.build();
+        } catch (Exception e) {
+            log.error("Error converting JSON to Proto: ", e);
+            return null;
+        }
     }
 
 }
