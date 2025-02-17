96faef0270611eef0f6f7b9ac675bc6e99823ff9
jack ning <github@bytedesk.com>
2025-02-17 15:32:15 +0800

    update modules/ticket: mod        5 files

--- modules/ticket/src/main/java/com/bytedesk/ticket/ticket/TicketStatusEnum.java
+++ modules/ticket/src/main/java/com/bytedesk/ticket/ticket/TicketStatusEnum.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-01-23 15:21:08
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-16 08:26:37
+ * @LastEditTime: 2025-02-17 15:05:05
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -17,12 +17,12 @@ package com.bytedesk.ticket.ticket;
 public enum TicketStatusEnum {
     NEW,            // 新建
     ASSIGNED,       // 已分配
-    IN_PROGRESS,    // 处理中
+    // IN_PROGRESS,    // 处理中
     PENDING,        // 待处理（等待客户响应/等待第三方）
-    ON_HOLD,        // 挂起（暂停处理）
-    REOPENED,       // 重新打开
+    // ON_HOLD,        // 挂起（暂停处理）
+    // REOPENED,       // 重新打开
     RESOLVED,       // 已解决
     ESCALATED,      // 已升级
-    CLOSED,         // 已关闭
-    CANCELLED;      // 已取消
+    // CLOSED,         // 已关闭
+    // CANCELLED;      // 已取消
 }
