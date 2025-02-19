1c4b4349c0b2968e74f63ff90b740d462a848b5c
jack ning <github@bytedesk.com>
2025-02-19 10:50:44 +0800

    update modules/ticket: mod        1 files

--- modules/ticket/src/main/java/com/bytedesk/ticket/ticket/TicketService.java
+++ modules/ticket/src/main/java/com/bytedesk/ticket/ticket/TicketService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-01-29 12:24:32
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-19 08:51:49
+ * @LastEditTime: 2025-02-19 10:21:22
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
