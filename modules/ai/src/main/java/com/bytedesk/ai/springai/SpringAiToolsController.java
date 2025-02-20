dc1f43e1567ed9648582bf5881066f558a53595b
jack ning <github@bytedesk.com>
2025-02-20 12:09:12 +0800

    update modules/ai: add        2 files

---
+++ modules/ai/src/main/java/com/bytedesk/ai/springai/SpringAiToolsController.java
@@ -0,0 +1,51 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2025-02-20 12:04:43
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-02-20 12:08:15
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ * 
+ * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
+ */
+package com.bytedesk.ai.springai;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bytedesk.ai.springai.tool_calling.DateTimeTools;
+import com.bytedesk.core.utils.JsonResult;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 工具调用
+ * https://docs.spring.io/spring-ai/reference/api/tools.html
+ */
+@RestController
+@RequestMapping("/tools")
+@RequiredArgsConstructor
+public class SpringAiToolsController {
+
+    private final ChatClient defaultChatClient;
+
+    // http://127.0.0.1:9003/tools/time
+    @GetMapping("/time")
+    public ResponseEntity<JsonResult<?>> time(@RequestParam(value = "message", defaultValue = "What day is tomorrow?") String message) {
+
+        String response = defaultChatClient
+                .prompt(message)
+                .tools(new DateTimeTools())
+                .call()
+                .content();
+
+        return ResponseEntity.ok(JsonResult.success(response));
+    }
+}
