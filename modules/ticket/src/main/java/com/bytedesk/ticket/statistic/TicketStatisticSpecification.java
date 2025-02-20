15fe731db8d40bd00a50cabc0143f1faed08c788
jack ning <github@bytedesk.com>
2025-02-20 13:46:26 +0800

    update modules/ticket: add        1 mod        2 del        1 files

---
+++ modules/ticket/src/main/java/com/bytedesk/ticket/statistic/TicketStatisticSpecification.java
@@ -0,0 +1,44 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2025-02-20 12:53:16
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-02-20 13:45:33
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ * 
+ * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
+ */
+package com.bytedesk.ticket.statistic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.criteria.Predicate;
+import org.springframework.data.jpa.domain.Specification;
+
+import com.bytedesk.core.base.BaseSpecification;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TicketStatisticSpecification extends BaseSpecification {
+    
+    public static Specification<TicketStatisticEntity> search(TicketStatisticRequest request) {
+        log.info("request: {}", request);
+        return (root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+            predicates.addAll(getBasicPredicates(root, criteriaBuilder, request.getOrgUid()));
+
+            // workgroupUid
+            if (StringUtils.hasText(request.getWorkgroupUid())) {
+                predicates.add(criteriaBuilder.equal(root.get("workgroupUid"), request.getWorkgroupUid()));
+            }
+
+            //
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        };
+    }
+}
