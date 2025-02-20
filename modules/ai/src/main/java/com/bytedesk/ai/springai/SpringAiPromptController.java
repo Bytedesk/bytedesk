22e65c838f4671e312ba402e2982e90aa8027804
jack ning <github@bytedesk.com>
2025-02-20 11:37:51 +0800

    update modules/ai: add        2 mod        1 del        2 files

--- modules/ai/src/main/java/com/bytedesk/ai/springai/SpringAiPromptController.java
+++ modules/ai/src/main/java/com/bytedesk/ai/springai/SpringAiPromptController.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-02-20 10:42:30
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-20 10:53:09
+ * @LastEditTime: 2025-02-20 11:37:31
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -20,6 +20,10 @@ import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.chat.prompt.SystemPromptTemplate;
+import org.springframework.ai.converter.BeanOutputConverter;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.JsonReader;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
@@ -28,13 +32,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bytedesk.ai.springai.output.ActorsFilms;
 import com.bytedesk.core.utils.JsonResult;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+/**
+ * https://docs.spring.io/spring-ai/reference/api/prompt.html
+ * https://github.com/Azure-Samples/spring-ai-azure-workshop
+ */
+@Slf4j
 @RestController
 @RequestMapping("/prompt")
 @RequiredArgsConstructor
@@ -42,15 +55,29 @@ public class SpringAiPromptController {
 
 	private final ChatClient defaultChatClient;
 
+	private final VectorStore ollamaRedisVectorStore;
+
 	@Value("classpath:/prompts/joke-prompt.st")
 	private Resource jokeResource;
 
 	@Value("classpath:/prompts/system-message.st")
 	private Resource systemResource;
 
-	// http://127.0.0.1:9003/prompt/joke?adjective=funny&topic=cows
-	@GetMapping("/joke")
-	public ResponseEntity<JsonResult<?>> joke(
+	@Value("classpath:/rag/wikipedia-curling.md")
+	private Resource docsToStuffResource;
+
+	@Value("classpath:/prompts/qa-prompt.st")
+	private Resource qaPromptResource;
+
+	@Value("classpath:/rag/bikes.json")
+	private Resource bikesResource;
+
+	@Value("classpath:/prompts/system-qa.st")
+	private Resource systemBikePrompt;
+
+	// http://127.0.0.1:9003/prompt/templating?adjective=funny&topic=cows
+	@GetMapping("/templating")
+	public ResponseEntity<JsonResult<?>> templating(
 			@RequestParam(value = "adjective", defaultValue = "funny") String adjective,
 			@RequestParam(value = "topic", defaultValue = "cows") String topic) {
 		// 使用prompt模板
@@ -71,6 +98,9 @@ public class SpringAiPromptController {
 			@RequestParam(value = "name", defaultValue = "Bob") String name,
 			@RequestParam(value = "voice", defaultValue = "pirate") String voice) {
 
+		// 使用prompt模板
+		// name, The name of the AI assistant. The default value is Bob
+		// voice, The style of voice that the AI assistant will use to reply. The default value is pirate
 		UserMessage userMessage = new UserMessage(message);
 		SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate(systemResource);
 		Message systemMessage = systemPromptTemplate.createMessage(Map.of("name", name, "voice", voice));
@@ -82,5 +112,106 @@ public class SpringAiPromptController {
 
 		return ResponseEntity.ok(JsonResult.success(response));
 	}
+
+	// 通过stuff=true，将文档内容添加到上下文
+    // http://127.0.0.1:9003/prompt/stuff?message=&stuff=true
+    @GetMapping("/stuff")
+	public ResponseEntity<JsonResult<?>> stuff(@RequestParam(value = "message",
+			defaultValue = "Which athletes won the mixed doubles gold medal in curling at the 2022 Winter Olympics?'") String message,
+			@RequestParam(value = "stuff", defaultValue = "false") boolean stuff) {
+        
+        // 使用prompt模板
+		PromptTemplate promptTemplate = new PromptTemplate(qaPromptResource);
+		Map<String, Object> map = new HashMap<>();
+		map.put("question", message);
+
+		if (stuff) {
+            // 将文档内容添加到上下文
+			map.put("context", docsToStuffResource);
+		}
+		else {
+            // 不将文档内容添加到上下文
+			map.put("context", "");
+		}
+		Prompt prompt = promptTemplate.create(map);
+		log.info("prompt: {}", prompt);
+	
+		ChatResponse response = defaultChatClient.prompt(prompt)
+				.call()
+				.chatResponse();
+
+		return ResponseEntity.ok(JsonResult.success(response));
+	}
+
+	// rag
+    // http://127.0.0.1:9003/prompt/rag?message=
+    @GetMapping("/rag")
+    public ResponseEntity<JsonResult<?>> rag(@RequestParam(value = "message", defaultValue = "What is the most popular bike brand?") String message) {
+        // Step 1 - Load JSON document as Documents
+
+		log.info("Loading JSON as Documents");
+		JsonReader jsonReader = new JsonReader(bikesResource, "name", "price", "shortDescription", "description");
+		List<Document> documents = jsonReader.get();
+		log.info("Loading JSON as Documents");
+
+		// Step 2 - Create embeddings and save to vector store
+		log.info("Creating Embeddings...");
+		ollamaRedisVectorStore.add(documents);
+		log.info("Embeddings created.");
+
+		// Step 3 retrieve related documents to query
+		log.info("Retrieving relevant documents");
+		List<Document> similarDocuments = ollamaRedisVectorStore.similaritySearch(message);
+		log.info(String.format("Found %s relevant documents.", similarDocuments.size()));
+
+		// Step 4 Embed documents into SystemMessage with the `system-qa.st` prompt
+		// template
+		Message systemMessage = getSystemMessage(similarDocuments);
+		UserMessage userMessage = new UserMessage(message);
+
+		// Step 4 - Ask the AI model
+		log.info("Asking AI model to reply to question.");
+		Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
+		log.info(prompt.toString());
+
+		ChatResponse chatResponse = defaultChatClient.prompt(prompt)
+				.call()
+				.chatResponse();
+		log.info("AI responded.");
+
+        // FIXME: [500] Internal Server Error - {"error":{}}
+        return ResponseEntity.ok(JsonResult.success(chatResponse)); 
+    }
+
+	// http://127.0.0.1:9003/prompt/format?actor=
+    @GetMapping("/format")
+	public ResponseEntity<JsonResult<?>> generate(@RequestParam(value = "actor", defaultValue = "Jeff Bridges") String actor) {
+		var outputParser = new BeanOutputConverter<>(ActorsFilms.class);
+
+		String format = outputParser.getFormat();
+		log.info("format: " + format);
+		String userMessage = """
+				Generate the filmography for the actor {actor}.
+				{format}
+				""";
+		PromptTemplate promptTemplate = new PromptTemplate(userMessage, Map.of("actor", actor, "format", format));
+		Prompt prompt = promptTemplate.create();
+		
+		String response = defaultChatClient.prompt(prompt)
+				.call()
+				.content();
+		log.info("response: " + response);
+
+		return ResponseEntity.ok(JsonResult.success(outputParser.convert(response)));
+	}
+    
+
+    private Message getSystemMessage(List<Document> similarDocuments) {
+		String documents = similarDocuments.stream().map(entry -> entry.getText()).collect(Collectors.joining("\n"));
+		SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate(systemBikePrompt);
+		Message systemMessage = systemPromptTemplate.createMessage(Map.of("documents", documents));
+		return systemMessage;
+	}
+    
 
 }
