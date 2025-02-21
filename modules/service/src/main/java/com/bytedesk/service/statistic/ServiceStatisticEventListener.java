1fbd7308f901afc981f0c9acc8e31be2f6c1240c
jack ning <github@bytedesk.com>
2025-02-21 16:22:48 +0800

    update modules/service: mod        3 files

--- modules/service/src/main/java/com/bytedesk/service/statistic/ServiceStatisticEventListener.java
+++ modules/service/src/main/java/com/bytedesk/service/statistic/ServiceStatisticEventListener.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-09-10 23:11:19
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-21 13:56:01
+ * @LastEditTime: 2025-02-21 16:06:42
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -16,70 +16,24 @@ package com.bytedesk.service.statistic;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-import com.bytedesk.core.message.MessageEntity;
-import com.bytedesk.core.message.MessageTypeEnum;
-import com.bytedesk.core.message.event.MessageCreateEvent;
-import com.bytedesk.core.message.event.MessageUpdateEvent;
-import com.bytedesk.service.statistic_thread.StatisticThreadCreateEvent;
-
+import com.bytedesk.core.quartz.event.QuartzHalfHourEvent;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-/**
- * TODO:
- * 统计thread和message的创建和更新
- */
+
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class ServiceStatisticEventListener {
 
+    private final ServiceStatisticService serviceStatisticService;
+
+    // 每半小时计算一次客服统计
     @EventListener
-    public void onStatisticThreadCreateEvent(StatisticThreadCreateEvent event) {
-        log.info("ServiceStatisticEventListener onServiceStatisticThreadCreateEvent: {}", event);
+    public void handleQuartzHalfHourEvent(QuartzHalfHourEvent event) {
+        log.info("ServiceStatisticEventListener handleQuartzHalfHourEvent: {}", event);
+        serviceStatisticService.calculateTodayStatistics();
     }
 
-    // @EventListener
-    // public void onThreadCreateEvent(ThreadCreateEvent event) {
-    // Thread thread = event.getThread();
-    // log.info("thread log ThreadCreateEvent: {}", thread.getUid());
-    // // if (thread.getType().equals(ThreadTypeEnum.AGENT.name())) {
-    // // // TODO:
-    // // } else if (thread.getType().equals(ThreadTypeEnum.WORKGROUP.name())) {
-    // // // TODO:
-    // // } else {
-    // // // TODO:
-    // // }
-    // }
-
-    // @EventListener
-    // public void onThreadUpdateEvent(ThreadUpdateEvent event) {
-    // Thread thread = event.getThread();
-    // log.info("thread log onThreadUpdateEvent: {}", thread.getUid());
-    // //
-    // // if (thread.getType().equals(ThreadTypeEnum.AGENT.name())) {
-    // // // TODO:
-    // // } else if (thread.getType().equals(ThreadTypeEnum.WORKGROUP.name())) {
-    // // // TODO:
-    // // } else {
-    // // // TODO:
-    // // }
-    // }
-
-    @EventListener
-    public void onMessageCreateEvent(MessageCreateEvent event) {
-        MessageEntity message = event.getMessage();
-        if (message.getType().equals(MessageTypeEnum.STREAM.name())) {
-            return;
-        }
-        // log.info("message unread create event: {}", message.getContent());
-    }
-
-    @EventListener
-    public void onMessageUpdateEvent(MessageUpdateEvent event) {
-        MessageEntity message = event.getMessage();
-        // log.info("message unread update event: {}", message.getContent());
-        if (message.getType().equals(MessageTypeEnum.STREAM.name())) {
-            return;
-        }
-    }
 
 }
