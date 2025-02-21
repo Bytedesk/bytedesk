df010247d8ab501f55d8da4fa8e8a0a017cedcf9
jack ning <github@bytedesk.com>
2025-02-21 17:50:03 +0800

    update

--- modules/service/src/main/java/com/bytedesk/service/statistic/ServiceStatisticRestController.java
+++ modules/service/src/main/java/com/bytedesk/service/statistic/ServiceStatisticRestController.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-07-17 17:08:55
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-21 17:19:17
+ * @LastEditTime: 2025-02-21 17:35:49
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -26,7 +26,7 @@ import com.bytedesk.core.utils.JsonResult;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/v1/vip/statistic")
+@RequestMapping("/api/v1/service/statistic")
 @RequiredArgsConstructor
 public class ServiceStatisticRestController extends BaseRestController<ServiceStatisticRequest> {
 
