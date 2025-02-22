a7748105868eb91bd9bb09697043e5d54a3063fd
jack ning <github@bytedesk.com>
2025-02-22 11:58:52 +0800

    update modules/ai: add        6 mod        2 del        1 files

--- modules/ai/src/main/java/com/bytedesk/ai/springai/demo/shopping/SpringAiShoppingController.java
+++ modules/ai/src/main/java/com/bytedesk/ai/springai/demo/shopping/SpringAiShoppingController.java
@@ -11,7 +11,7 @@
  * 
  * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
  */
-package com.bytedesk.ai.springai.demo;
+package com.bytedesk.ai.springai.demo.shopping;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
