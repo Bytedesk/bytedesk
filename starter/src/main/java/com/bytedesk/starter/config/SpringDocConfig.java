63f65393cc7d32dcef781e3b8fbdf5e62ffc557c
jack ning <github@bytedesk.com>
2025-02-22 13:01:56 +0800

    update starter/src: add        1 mod       14 files

--- starter/src/main/java/com/bytedesk/starter/config/SpringDocConfig.java
+++ starter/src/main/java/com/bytedesk/starter/config/SpringDocConfig.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-10-22 11:54:12
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-01-07 16:57:54
+ * @LastEditTime: 2025-02-22 12:37:29
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -13,43 +13,38 @@
  */
 package com.bytedesk.starter.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-
+/**
+ * http://127.0.0.1:9003/swagger-ui/index.html
+ */
 @Configuration
 public class SpringDocConfig {
 
-    @Value("${application.version}")
-    private String version;
+    // @Value("${application.version}")
+    // private String version;
     
-    @Value("${server.port}")
-    private String port;
+    // @Value("${server.port}")
+    // private String port;
     
-    @Bean
-    public OpenAPI customOpenApi() {
-        return new OpenAPI()
-        .components(new Components()
-                    .addSecuritySchemes("openApiSecurityScheme", new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .bearerFormat("JWT")
-                                        .in(SecurityScheme.In.HEADER)
-                                        .name("Authorization")
-                                        .scheme("Bearer")))
-        // set title and version
-        .info(new Info()
-              .title("Bytedesk API")
-              .version(version)
-              .description("Team IM && AI Customer Service")
-              .license(new License()
-                       .name("Apache 2.0")
-                       .url("https://www.apache.org/licenses/LICENSE-2.0.html")));
-    }
+    // @Bean
+    // public OpenAPI customOpenApi() {
+    //     return new OpenAPI()
+    //     .components(new Components()
+    //                 .addSecuritySchemes("openApiSecurityScheme", new SecurityScheme()
+    //                                     .type(SecurityScheme.Type.HTTP)
+    //                                     .bearerFormat("JWT")
+    //                                     .in(SecurityScheme.In.HEADER)
+    //                                     .name("Authorization")
+    //                                     .scheme("Bearer")))
+    //     // set title and version
+    //     .info(new Info()
+    //           .title("Bytedesk API")
+    //           .version(version)
+    //           .description("Team IM && AI Customer Service")
+    //           .license(new License()
+    //                    .name("Apache 2.0")
+    //                    .url("https://www.apache.org/licenses/LICENSE-2.0.html")));
+    // }
 }
 
