695d4e9c5f0cdcd850a34e83693eeb0ce988d730
jack ning <github@bytedesk.com>
2025-02-22 14:05:36 +0800

    update vendors/easyAi: add        3 mod        1 del      207 files

---
+++ modules/ai/src/main/java/com/bytedesk/ai/springai/SpringAiAudioController.java
@@ -0,0 +1,77 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2025-02-22 13:51:22
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-02-22 14:05:17
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ * 
+ * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
+ */
+package com.bytedesk.ai.springai;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.bytedesk.core.utils.JsonResult;
+
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/springai/audio")
+@RequiredArgsConstructor
+public class SpringAiAudioController {
+
+    private final SpringAiAudioService audioService;
+
+	/**
+	 * audio2text
+	 * 用于将音频转换为文本输出
+	 */
+	@UserIp
+	@PostMapping("/audio2text")
+	@Operation(summary = "DashScope Audio Transcription")
+	public Flux<JsonResult<?>> audioToText(@RequestParam("audio") MultipartFile audio) {
+
+		if (audio.isEmpty()) {
+			return Flux.just(JsonResult.error("No audio file provided"));
+		}
+
+		return audioService.audio2text(audio).map(JsonResult::success);
+	}
+
+	/**
+	 * text2audio
+	 * 用于将文本转换为语音输出
+	 */
+	@UserIp
+	@GetMapping("/text2audio/{prompt}")
+	@Operation(summary = "DashScope Speech Synthesis")
+	public JsonResult<byte[]> textToAudio(@PathVariable("prompt") String prompt) {
+
+		byte[] audioData = audioService.text2audio(prompt);
+
+		// 测试验证音频数据是否为空
+		try (FileOutputStream fos = new FileOutputStream("audio.wav")) {
+			fos.write(audioData);
+		} catch (IOException e) {
+			return JsonResult.error("Failed to save audio file: " + e.getMessage());
+		}
+
+		return JsonResult.success(audioData);
+	}
+    
+}
---
+++ modules/ai/src/main/java/com/bytedesk/ai/springai/SpringAiAudioService.java
@@ -0,0 +1,129 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2025-02-22 13:52:16
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-02-22 13:54:51
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ * 
+ * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
+ */
+package com.bytedesk.ai.springai;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.concurrent.CountDownLatch;
+
+import org.springframework.ai.audio.transcription.AudioTranscriptionPrompt;
+import org.springframework.ai.audio.transcription.AudioTranscriptionResponse;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.alibaba.cloud.ai.dashscope.audio.DashScopeAudioTranscriptionOptions;
+import com.alibaba.cloud.ai.dashscope.audio.synthesis.SpeechSynthesisModel;
+import com.alibaba.cloud.ai.dashscope.audio.synthesis.SpeechSynthesisPrompt;
+import com.alibaba.cloud.ai.dashscope.audio.synthesis.SpeechSynthesisResponse;
+import com.alibaba.cloud.ai.dashscope.audio.transcription.AudioTranscriptionModel;
+
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+
+
+@Service
+@RequiredArgsConstructor
+public class SpringAiAudioService {
+
+    private final AudioTranscriptionModel transcriptionModel;
+
+	private final SpeechSynthesisModel speechSynthesisModel;
+
+    /**
+	 * 将文本转为语音
+	 */
+	public byte[] text2audio(String text) {
+
+		Flux<SpeechSynthesisResponse> response = speechSynthesisModel.stream(
+				new SpeechSynthesisPrompt(text)
+		);
+
+		CountDownLatch latch = new CountDownLatch(1);
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+		try {
+			response.doFinally(
+					signal -> latch.countDown()
+			).subscribe(synthesisResponse -> {
+
+				ByteBuffer byteBuffer = synthesisResponse.getResult().getOutput().getAudio();
+				byte[] bytes = new byte[byteBuffer.remaining()];
+				byteBuffer.get(bytes);
+
+				try {
+					outputStream.write(bytes);
+				}
+				catch (IOException e) {
+					throw new RuntimeException("Error writing to output stream " + e.getMessage());
+				}
+			});
+
+			latch.await();
+		}
+		catch (InterruptedException e) {
+			throw new RuntimeException("Operation interrupted. " + e.getMessage());
+		}
+
+		return outputStream.toByteArray();
+	}
+
+	/**
+	 * 将语音转为文本
+	 */
+	public Flux<String> audio2text(MultipartFile audio) {
+
+		CountDownLatch latch = new CountDownLatch(1);
+		StringBuilder stringBuilder = new StringBuilder();
+
+		File tempFile;
+		try {
+			tempFile = File.createTempFile("audio", ".pcm");
+			audio.transferTo(tempFile);
+		}
+		catch (IOException e) {
+			throw new RuntimeException("Failed to create temporary file " + e.getMessage());
+		}
+
+		Flux<AudioTranscriptionResponse> response = transcriptionModel.stream(
+				new AudioTranscriptionPrompt(
+						new FileSystemResource(tempFile),
+						DashScopeAudioTranscriptionOptions.builder()
+								.withModel("paraformer-realtime-v2")
+								.withSampleRate(16000)
+								.withFormat(DashScopeAudioTranscriptionOptions.AudioFormat.PCM)
+								.withDisfluencyRemovalEnabled(false)
+								.build()
+				)
+		);
+
+		response.doFinally(signal -> latch.countDown())
+				.subscribe(resp -> stringBuilder.append(resp.getResult().getOutput()).append("\n"));
+
+		try {
+			latch.await();
+		}
+		catch (InterruptedException e) {
+			throw new RuntimeException("Transcription was interrupted " + e.getMessage());
+		}
+		finally {
+			tempFile.delete();
+		}
+
+		return Flux.just(stringBuilder.toString());
+	}
+
+}
