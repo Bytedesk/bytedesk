4ab760453d9aa318a4ad58ccbf808dc99088b51f
jack ning <github@bytedesk.com>
2025-02-22 15:40:58 +0800

    update modules/kbase: add       18 mod        3 files

--- modules/kbase/src/main/java/com/bytedesk/kbase/faq/FaqResponse.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/faq/FaqResponse.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-03-22 23:00:00
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-01-13 13:41:50
+ * @LastEditTime: 2025-02-22 15:40:00
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -57,6 +57,21 @@ public class FaqResponse extends BaseResponse {
     private String fileUid; // 对应文件
 
     private String docUid; // 对应文档
+
+    // 是否是常见问题/
+    private Boolean isCommon;
+
+    // 是否是快捷按钮
+    private Boolean isShortcut;
+
+    // 是否是猜你相问
+    private Boolean isGuess;
+
+    // 是否是热门问题
+    private Boolean isHot;
+
+    // 是否是快捷路径
+    private Boolean isShortcutPath;
 
     private String orgUid;
 
---
+++ modules/kbase/src/main/java/com/bytedesk/kbase/website/WebsiteEntity.java
@@ -0,0 +1,72 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2024-05-11 18:14:28
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-02-22 15:37:27
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ *  联系：270580156@qq.com
+ * Copyright (c) 2024 by bytedesk.com, All Rights Reserved. 
+ */
+package com.bytedesk.kbase.website;
+
+import com.bytedesk.core.base.BaseEntity;
+import com.bytedesk.core.constant.TypeConsts;
+import com.bytedesk.core.enums.LevelEnum;
+import com.bytedesk.core.enums.PlatformEnum;
+import com.bytedesk.core.message.MessageTypeEnum;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Entity
+@Data
+@Builder
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "bytedesk_kbase_website")
+public class WebsiteEntity extends BaseEntity {
+
+    // 网站名称
+    private String name;
+
+    // 网站地址
+    @Column(name = "website_url")
+    private String url;
+
+    // 网站描述
+    private String description;
+
+    // 抓取内容
+    @Column(columnDefinition = TypeConsts.COLUMN_TYPE_TEXT)
+    private String content;
+
+    // 网站类型
+    @Builder.Default
+    @Column(name = "website_type", nullable = false)
+    private String type = MessageTypeEnum.TEXT.name();
+
+    // @Builder.Default
+    // @Column(nullable = false)
+    // private String color = "red";
+
+    @Builder.Default
+    private String level = LevelEnum.ORGANIZATION.name();
+
+    @Builder.Default
+    private String platform = PlatformEnum.BYTEDESK.name();
+
+    private String userUid;
+}
