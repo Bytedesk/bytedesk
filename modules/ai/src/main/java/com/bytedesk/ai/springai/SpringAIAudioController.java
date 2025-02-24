af2ac1eeca8b478295ef503e699c895c3163b135
jack ning <github@bytedesk.com>
2025-02-24 14:44:11 +0800

    update

--- modules/ai/src/main/java/com/bytedesk/ai/springai/SpringAIAudioController.java
+++ modules/ai/src/main/java/com/bytedesk/ai/springai/SpringAIAudioController.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-02-22 13:51:22
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-22 14:18:27
+ * @LastEditTime: 2025-02-24 14:37:38
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
--- modules/ai/src/main/java/com/bytedesk/ai/springai/demo/airline/SpringAIAirlineConfig.java
+++ modules/ai/src/main/java/com/bytedesk/ai/springai/demo/airline/SpringAIAirlineConfig.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-02-22 11:15:31
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-22 11:56:18
+ * @LastEditTime: 2025-02-24 14:35:58
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -47,7 +47,7 @@ public class SpringAIAirlineConfig {
                         """)
                 .defaultAdvisors(
                         new PromptChatMemoryAdvisor(defaultChatMemory), // Chat Memory
-                        // VectorStoreChatMemoryAdvisor.builder(vectorStore).build(),
+                        // VectorStoreChatMemoryAdvisor.builder(ollamaRedisVectorStore).build(),
                         new QuestionAnswerAdvisor(ollamaRedisVectorStore, 
                                 SearchRequest.builder().topK(4).similarityThresholdAll().build()
                         ), // RAG
