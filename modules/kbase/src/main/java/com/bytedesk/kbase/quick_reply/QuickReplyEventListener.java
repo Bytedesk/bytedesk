923f5dc50bab944fecd8044c4f7b01df12e62e83
jack ning <github@bytedesk.com>
2025-02-24 16:30:25 +0800

    update modules/kbase: mod        1 files

--- modules/kbase/src/main/java/com/bytedesk/kbase/quick_reply/QuickReplyEventListener.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/quick_reply/QuickReplyEventListener.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-06-20 14:31:54
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-24 13:33:24
+ * @LastEditTime: 2025-02-24 16:26:34
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -28,8 +28,6 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 @AllArgsConstructor
 public class QuickReplyEventListener {
-
-    // private final QuickReplyRestService quickReplyRestService;
 
     @Order(7)
     @EventListener
