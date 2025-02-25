23924bd6b4b6fd388a752cdecc8a37e023f704ca
jack ning <github@bytedesk.com>
2025-02-25 09:04:03 +0800

    update

--- modules/ai/src/main/java/com/bytedesk/ai/springai/SpringAIZhipuaiConfig.java
+++ modules/ai/src/main/java/com/bytedesk/ai/springai/SpringAIZhipuaiConfig.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-05-31 10:53:11
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-24 23:43:54
+ * @LastEditTime: 2025-02-25 09:02:14
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
