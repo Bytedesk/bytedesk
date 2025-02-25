ec20a91869abbf3ed2631341f65ad46ae030879e
jack ning <github@bytedesk.com>
2025-02-25 10:34:00 +0800

    update

--- modules/kbase/src/main/java/com/bytedesk/kbase/file/FileEventListener.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/file/FileEventListener.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-02-25 09:44:18
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-25 09:52:53
+ * @LastEditTime: 2025-02-25 10:23:45
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -13,13 +13,13 @@
  */
 package com.bytedesk.kbase.file;
 
-// import java.io.IOException;
+import java.io.IOException;
 
-// import org.springframework.context.event.EventListener;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-// import com.bytedesk.kbase.upload.UploadEntity;
-// import com.bytedesk.kbase.upload.UploadTypeEnum;
-// import com.bytedesk.kbase.upload.event.UploadCreateEvent;
+import com.bytedesk.kbase.upload.UploadEntity;
+import com.bytedesk.kbase.upload.UploadTypeEnum;
+import com.bytedesk.kbase.upload.event.UploadCreateEvent;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -29,14 +29,14 @@ import lombok.extern.slf4j.Slf4j;
 @AllArgsConstructor
 public class FileEventListener {
     
-    // @EventListener
-    // public void onUploadCreateEvent(UploadCreateEvent event) throws IOException {
-    //     UploadEntity upload = event.getUpload();
-    //     log.info("UploadEventListener create: {}", upload.toString());
-    //     // 专门存储大模型上传文件记录
-    //     if (upload.getType().equals(UploadTypeEnum.LLM.name())) {
+    @EventListener
+    public void onUploadCreateEvent(UploadCreateEvent event) throws IOException {
+        UploadEntity upload = event.getUpload();
+        log.info("UploadEventListener create: {}", upload.toString());
+        // 专门存储大模型上传文件记录
+        if (upload.getType().equals(UploadTypeEnum.LLM.name())) {
   
-    //     }
-    // }
+        }
+    }
 }
 
