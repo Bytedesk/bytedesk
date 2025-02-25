e29d731654aa1d17e69ad3772c9d8737a926c0d7
jack ning <github@bytedesk.com>
2025-02-25 10:34:00 +0800

    update

--- modules/ai/src/main/java/com/bytedesk/ai/robot/RobotEventListener.java
+++ modules/ai/src/main/java/com/bytedesk/ai/robot/RobotEventListener.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-06-12 07:17:13
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-24 23:54:21
+ * @LastEditTime: 2025-02-25 10:22:32
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -245,4 +245,5 @@ public class RobotEventListener {
         }
     }
 
+    
 }
