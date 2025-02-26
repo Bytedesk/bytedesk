d62dfec797820e8b02359a06c87bf8c52608c9d5
jack ning <github@bytedesk.com>
2025-02-26 15:08:35 +0800

    update

--- modules/ai/src/main/java/com/bytedesk/ai/robot/RobotInitializer.java
+++ modules/ai/src/main/java/com/bytedesk/ai/robot/RobotInitializer.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-11-05 13:43:02
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-26 15:01:58
+ * @LastEditTime: 2025-02-26 15:08:01
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -69,7 +69,7 @@ public class RobotInitializer implements SmartInitializingSingleton {
             // 写入到redis vector 中
             for (FileContent file : files) {
                 springAIVectorService.readText(file.getFilename(), file.getContent(), kbUid, orgUid);
-                // 
+                // 根据文档内容，生成问答对
                 ollamaChatService.ifPresent(service -> {
                     String qaPairs = service.generateFaqPairsAsync(file.getContent());
                     log.info("generateFaqPairsAsync qaPairs {}", qaPairs);
@@ -80,8 +80,6 @@ public class RobotInitializer implements SmartInitializingSingleton {
             stringRedisTemplate.opsForValue().set(RobotConsts.ROBOT_INIT_DEMO_BYTEDESK_KEY, "true");
             // 删除 redis key
             // redisTemplate.delete(RobotConsts.ROBOT_INIT_DEMO_KEY);
-            // 根据文档内容，生成问答对
-            
         }
         
     }   
