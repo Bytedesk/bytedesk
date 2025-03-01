62328fc80683e3e935e5a0f6aaa60f7e84dc2dfb
jack ning <github@bytedesk.com>
2025-03-01 19:08:54 +0800

    update modules/service: mod        1 files

--- modules/service/src/main/java/com/bytedesk/service/unified/UnifiedRestController.java
+++ modules/service/src/main/java/com/bytedesk/service/unified/UnifiedRestController.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-05-11 18:25:36
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-11-23 14:20:33
+ * @LastEditTime: 2025-03-01 18:40:12
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
