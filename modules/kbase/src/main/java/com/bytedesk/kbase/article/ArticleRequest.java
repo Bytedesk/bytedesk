1aad98c77cefdb01d584f9143eb0e87ef6d5c515
jack ning <github@bytedesk.com>
2025-03-01 10:48:40 +0800

    update modules/kbase: mod        5 files

--- modules/kbase/src/main/java/com/bytedesk/kbase/article/ArticleRequest.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/article/ArticleRequest.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-03-22 22:59:48
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-01 09:54:36
+ * @LastEditTime: 2025-03-01 10:46:26
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -90,4 +90,6 @@ public class ArticleRequest extends BaseRequest {
     private String categoryUid;
 
     private String kbUid;
+
+    private String userUid;
 }
