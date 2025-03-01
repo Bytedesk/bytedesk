a4f759a1f02e3373ae2544b94d97263370db69af
jack ning <github@bytedesk.com>
2025-03-01 15:29:05 +0800

    update modules/kbase: add       14 files

---
+++ modules/kbase/src/main/java/com/bytedesk/kbase/comment/CommentRestService.java
@@ -0,0 +1,133 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2024-05-11 18:25:45
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2024-11-20 11:17:49
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ *  联系：270580156@qq.com
+ * Copyright (c) 2024 by bytedesk.com, All Rights Reserved. 
+ */
+package com.bytedesk.kbase.comment;
+
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
+import org.springframework.stereotype.Service;
+
+import com.bytedesk.core.base.BaseRestService;
+import com.bytedesk.core.uid.UidUtils;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class CommentRestService extends BaseRestService<CommentEntity, CommentRequest, CommentResponse> {
+
+    private final CommentRepository commentRepository;
+
+    private final ModelMapper modelMapper;
+
+    private final UidUtils uidUtils;
+
+    @Override
+    public Page<CommentResponse> queryByOrg(CommentRequest request) {
+        Pageable pageable = PageRequest.of(request.getPageNumber(), request.getPageSize(), Sort.Direction.ASC,
+                "updatedAt");
+        Specification<CommentEntity> spec = CommentSpecification.search(request);
+        Page<CommentEntity> page = commentRepository.findAll(spec, pageable);
+        return page.map(this::convertToResponse);
+    }
+
+    @Override
+    public Page<CommentResponse> queryByUser(CommentRequest request) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'queryByUser'");
+    }
+
+    @Cacheable(value = "comment", key = "#uid", unless="#result==null")
+    @Override
+    public Optional<CommentEntity> findByUid(String uid) {
+        return commentRepository.findByUid(uid);
+    }
+
+    @Override
+    public CommentResponse create(CommentRequest request) {
+        
+        CommentEntity entity = modelMapper.map(request, CommentEntity.class);
+        entity.setUid(uidUtils.getUid());
+
+        CommentEntity savedEntity = save(entity);
+        if (savedEntity == null) {
+            throw new RuntimeException("Create comment failed");
+        }
+        return convertToResponse(savedEntity);
+    }
+
+    @Override
+    public CommentResponse update(CommentRequest request) {
+        Optional<CommentEntity> optional = commentRepository.findByUid(request.getUid());
+        if (optional.isPresent()) {
+            CommentEntity entity = optional.get();
+            modelMapper.map(request, entity);
+            //
+            CommentEntity savedEntity = save(entity);
+            if (savedEntity == null) {
+                throw new RuntimeException("Update comment failed");
+            }
+            return convertToResponse(savedEntity);
+        }
+        else {
+            throw new RuntimeException("Comment not found");
+        }
+    }
+
+    @Override
+    public CommentEntity save(CommentEntity entity) {
+        try {
+            return commentRepository.save(entity);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @Override
+    public void deleteByUid(String uid) {
+        Optional<CommentEntity> optional = commentRepository.findByUid(uid);
+        if (optional.isPresent()) {
+            optional.get().setDeleted(true);
+            save(optional.get());
+            // commentRepository.delete(optional.get());
+        }
+        else {
+            throw new RuntimeException("Comment not found");
+        }
+    }
+
+    @Override
+    public void delete(CommentRequest request) {
+        deleteByUid(request.getUid());
+    }
+
+    @Override
+    public void handleOptimisticLockingFailureException(ObjectOptimisticLockingFailureException e, CommentEntity entity) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handleOptimisticLockingFailureException'");
+    }
+
+    @Override
+    public CommentResponse convertToResponse(CommentEntity entity) {
+        return modelMapper.map(entity, CommentResponse.class);
+    }
+    
+}
