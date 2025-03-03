6a0e9ed24d6396baa6cbdf1cb7603f14f4c521b4
jack ning <github@bytedesk.com>
2025-03-03 23:39:28 +0800

    update modules/service: mod        2 files

--- modules/core/src/main/java/com/bytedesk/core/base/BaseRequest.java
+++ modules/core/src/main/java/com/bytedesk/core/base/BaseRequest.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-18 23:30:00
+ * @LastEditTime: 2025-03-03 23:38:35
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -49,4 +49,5 @@ public abstract class BaseRequest implements Serializable {
         }
         return PageRequest.of(pageNumber, pageSize, Sort.Direction.DESC, "updatedAt");
     }
+
 }
