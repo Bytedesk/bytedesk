3e40325c6d0414593f5ffa8228eaaddbea3daa5f
jack ning <github@bytedesk.com>
2025-03-04 13:01:08 +0800

    update

--- modules/kbase/src/main/java/com/bytedesk/kbase/text/TextExcel.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/text/TextExcel.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-05-22 22:12:03
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-04 12:56:39
+ * @LastEditTime: 2025-03-04 12:57:03
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -11,15 +11,16 @@
  *  联系：270580156@qq.com
  * Copyright (c) 2024 by bytedesk.com, All Rights Reserved. 
  */
-package com.bytedesk.kbase.faq;
+package com.bytedesk.kbase.text;
 
 import com.alibaba.excel.annotation.ExcelProperty;
 import com.alibaba.excel.annotation.write.style.ColumnWidth;
 
 import lombok.Data;
 
+
 @Data
-public class FaqExcel {
+public class TextExcel {
 
     @ExcelProperty(index = 0, value = "分类")
     @ColumnWidth(20)
