c1ec2dda055dfb308557db654c6af54ac6299fc7
jack ning <github@bytedesk.com>
2025-03-05 10:52:06 +0800

    update modules/kbase: mod        3 files

--- modules/kbase/src/main/java/com/bytedesk/kbase/auto_reply/keyword/AutoReplyKeywordSpecification.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/auto_reply/keyword/AutoReplyKeywordSpecification.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-07-06 10:05:59
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-24 16:52:27
+ * @LastEditTime: 2025-03-05 10:39:42
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -37,6 +37,13 @@ public class AutoReplyKeywordSpecification extends BaseSpecification {
             for (String keyword : keywords) {
                 if (StringUtils.hasText(keyword)) {
                     predicates.add(criteriaBuilder.like(root.get("keywordList").as(String.class), "%" + keyword + "%"));
+                }
+            }
+            // replyList
+            List<String> replies = request.getReplyList();
+            for (String reply : replies) {
+                if (StringUtils.hasText(reply)) {
+                    predicates.add(criteriaBuilder.like(root.get("replyList").as(String.class), "%" + reply + "%"));
                 }
             }
             // 
