e4bb74dac19d7723171c93c334e6d495ffcd61e6
jack ning <github@bytedesk.com>
2025-03-06 09:13:19 +0800

    update modules/core: mod        1 files

--- modules/core/src/main/java/com/bytedesk/core/base/BaseRestService.java
+++ modules/core/src/main/java/com/bytedesk/core/base/BaseRestService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-05-10 12:13:37
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-05 16:58:14
+ * @LastEditTime: 2025-03-06 08:45:28
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
