fead4554277b378944924312429a5c5611731348
jack ning <github@bytedesk.com>
2025-03-06 18:16:26 +0800

    update modules/ai: add       14 mod        2 files

--- modules/core/src/main/java/com/bytedesk/core/tag/TagEntityListener.java
+++ modules/core/src/main/java/com/bytedesk/core/tag/TagEntityListener.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-02-25 09:52:34
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-05 18:22:38
+ * @LastEditTime: 2025-03-06 18:14:18
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
