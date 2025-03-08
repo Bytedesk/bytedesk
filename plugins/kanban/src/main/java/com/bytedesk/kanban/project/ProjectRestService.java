b730103d0291c867a4209140b89b1f17a93015c0
jack ning <github@bytedesk.com>
2025-03-08 23:27:35 +0800

    update

--- plugins/kanban/src/main/java/com/bytedesk/kanban/project/ProjectRestService.java
+++ plugins/kanban/src/main/java/com/bytedesk/kanban/project/ProjectRestService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-05-11 18:25:45
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-08 22:56:19
+ * @LastEditTime: 2025-03-08 23:19:03
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -122,6 +122,9 @@ public class ProjectRestService extends BaseRestService<ProjectEntity, ProjectRe
             throw new RuntimeException("Project not found");
         }
     }
+
+    
+    
 
     /**
      * 保存标签，失败时自动重试
