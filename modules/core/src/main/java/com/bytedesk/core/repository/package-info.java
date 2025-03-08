4334b59007cd48fdc3aa72cf76e5064f4c438a31
jack ning <github@bytedesk.com>
2025-03-08 10:54:31 +0800

    update

--- modules/core/src/main/java/com/bytedesk/core/repository/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Repository package containing data access interfaces
- * 
- * @author bytedesk.com
- */
-@NonNullApi
-package com.bytedesk.core.repository;
-
-import org.springframework.lang.NonNullApi; 
--- plugins/kanban/src/main/java/com/bytedesk/kanban/project/ProjectEntity.java
+++ plugins/kanban/src/main/java/com/bytedesk/kanban/project/ProjectEntity.java
@@ -1,18 +1,59 @@
 /*
  * @Author: jackning 270580156@qq.com
- * @Date: 2025-02-09 09:59:43
+ * @Date: 2024-05-11 18:14:28
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-09 10:01:06
+ * @LastEditTime: 2025-03-08 10:51:34
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
- *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
  *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
  *  contact: 270580156@qq.com 
- * 
- * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
+ *  联系：270580156@qq.com
+ * Copyright (c) 2024 by bytedesk.com, All Rights Reserved. 
  */
 package com.bytedesk.kanban.project;
 
-public class ProjectEntity {
-    
+import com.bytedesk.core.base.BaseEntity;
+import com.bytedesk.core.enums.LevelEnum;
+import com.bytedesk.core.enums.PlatformEnum;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Entity
+@Data
+@Builder
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners({ProjectEntityListener.class})
+@Table(name = "bytedesk_kanban_project")
+public class ProjectEntity extends BaseEntity {
+
+    private String name;
+
+    @Builder.Default
+    @Column(name = "project_type", nullable = false)
+    private String type = ProjectTypeEnum.CUSTOMER.name();
+
+    @Builder.Default
+    @Column(nullable = false)
+    private String color = "red";
+
+    @Builder.Default
+    private String level = LevelEnum.ORGANIZATION.name();
+
+    @Builder.Default
+    private String platform = PlatformEnum.BYTEDESK.name();
+
+    private String userUid;
 }
