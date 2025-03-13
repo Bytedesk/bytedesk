da934c6ce20248768b0c4d7722b2d445835ead1f
jack ning <github@bytedesk.com>
2025-03-13 22:18:23 +0800

    update modules/service: add        2 mod        2 files

--- modules/core/src/main/java/com/bytedesk/core/thread/ThreadEntity.java
+++ modules/core/src/main/java/com/bytedesk/core/thread/ThreadEntity.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-13 20:09:17
+ * @LastEditTime: 2025-03-12 18:21:54
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -14,9 +14,6 @@
  */
 package com.bytedesk.core.thread;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.alibaba.fastjson2.JSON;
 import com.bytedesk.core.base.BaseEntity;
 import com.bytedesk.core.constant.BytedeskConsts;
@@ -25,7 +22,6 @@ import com.bytedesk.core.enums.ClientEnum;
 import com.bytedesk.core.rbac.user.UserEntity;
 import com.bytedesk.core.rbac.user.UserProtobuf;
 import com.bytedesk.core.utils.ConvertUtils;
-import com.bytedesk.core.utils.StringListConverter;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
@@ -153,9 +149,7 @@ public class ThreadEntity extends BaseEntity {
 
     // 标签
     @Builder.Default
-    @Convert(converter = StringListConverter.class)
-    @Column(columnDefinition = TypeConsts.COLUMN_TYPE_TEXT)
-    private List<String> tagList = new ArrayList<>();
+    private String tags = BytedeskConsts.EMPTY_ARRAY_STRING;
 
     @Builder.Default
     private String client = ClientEnum.WEB.name();
