c5fc39cb3b2dae2c3c771bd858565479b87439d8
jack ning <github@bytedesk.com>
2025-03-13 18:54:31 +0800

    Update VisitorResponse.java

--- modules/service/src/main/java/com/bytedesk/service/visitor/VisitorResponse.java
+++ modules/service/src/main/java/com/bytedesk/service/visitor/VisitorResponse.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-04-04 17:05:59
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-10 17:57:29
+ * @LastEditTime: 2025-03-13 18:51:54
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -62,6 +62,8 @@ public class VisitorResponse extends BaseResponse {
 	// private LocalDateTime updatedAt;
 
 	private String status;
+
+	private String tags;
 
 	private String extra;
 
