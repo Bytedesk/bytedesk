e4ceb05ea4861110b5bf90de0f13aa11f629b47c
jack ning <github@bytedesk.com>
2025-03-13 23:21:32 +0800

    update modules/kbase: mod        1 files

--- modules/kbase/src/main/java/com/bytedesk/kbase/quick_reply/QuickReplyRestService.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/quick_reply/QuickReplyRestService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-03-22 22:59:18
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-11 08:59:14
+ * @LastEditTime: 2025-03-13 23:07:37
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -78,6 +78,7 @@ public class QuickReplyRestService extends BaseRestService<QuickReplyEntity, Qui
             throw new RuntimeException("User not found");
         }
         request.setUserUid(user.getUid());
+        // 
         return queryByOrg(request);
     }
 
