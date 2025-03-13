2b36a51d14d229bd5145eafb3dd948210db123d7
jack ning <github@bytedesk.com>
2025-03-13 13:56:58 +0800

    update modules/ai: mod        1 files

--- modules/ai/src/main/java/com/bytedesk/ai/springai/ollama/SpringAIOllamaService.java
+++ modules/ai/src/main/java/com/bytedesk/ai/springai/ollama/SpringAIOllamaService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-02-26 16:59:14
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-13 13:16:19
+ * @LastEditTime: 2025-03-13 13:27:54
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -112,8 +112,6 @@ public class SpringAIOllamaService extends BaseSpringAIService {
     @Override
     protected void processPromptSSE(Prompt prompt, ThreadProtobuf threadProtobuf,
             MessageProtobuf messageProtobuf, SseEmitter emitter) {
-        
-        // 
         
         //
         bytedeskOllamaChatModel.ifPresentOrElse(
