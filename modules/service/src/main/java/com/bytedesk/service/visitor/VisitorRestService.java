822b7a185f616c65e68f5003996362183931613d
jack ning <github@bytedesk.com>
2025-03-13 16:49:07 +0800

    Update VisitorRestService.java

--- modules/service/src/main/java/com/bytedesk/service/visitor/VisitorRestService.java
+++ modules/service/src/main/java/com/bytedesk/service/visitor/VisitorRestService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-13 16:32:22
+ * @LastEditTime: 2025-03-13 16:37:38
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -92,6 +92,9 @@ public class VisitorRestService extends BaseRestService<VisitorEntity, VisitorRe
                 visitor.setIpLocation(visitorRequest.getIpLocation());
                 save(visitor);
             }
+            visitor.getDevice().setBrowser(visitorRequest.getBrowser());
+            visitor.getDevice().setOs(visitorRequest.getOs());
+            visitor.getDevice().setDevice(visitorRequest.getDevice());
             visitor.setExtra(visitorRequest.getExtra());
             //
             return convertToResponse(visitor);
