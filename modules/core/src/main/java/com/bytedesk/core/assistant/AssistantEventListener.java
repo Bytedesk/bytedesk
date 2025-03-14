dda2d5a9078e793922370a791cf9aadeb489c7df
jack ning <github@bytedesk.com>
2025-03-14 14:31:18 +0800

    update

--- modules/core/src/main/java/com/bytedesk/core/assistant/AssistantEventListener.java
+++ modules/core/src/main/java/com/bytedesk/core/assistant/AssistantEventListener.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-08-04 11:32:22
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-12-22 17:43:24
+ * @LastEditTime: 2025-03-14 14:29:44
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -38,7 +38,7 @@ public class AssistantEventListener {
         // 每创建一个用户，自动给此用户生成一条文件助理的会话
         threadService.createFileAssistantThread(user);
         // 每创建一个用户，自动给此用户生成一条剪贴助理的会话
-        threadService.createClipboardAssistantThread(user);
+        // threadService.createClipboardAssistantThread(user);
     }
     
     
