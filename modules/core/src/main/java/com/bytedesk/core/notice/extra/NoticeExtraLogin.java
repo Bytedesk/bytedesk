8087704f396ab047fcca92c94062676c45f8c971
jack ning <github@bytedesk.com>
2025-03-14 15:29:26 +0800

    update

---
+++ modules/core/src/main/java/com/bytedesk/core/notice/extra/NoticeExtraLogin.java
@@ -0,0 +1,18 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2025-03-14 15:24:26
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-03-14 15:24:29
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ * 
+ * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
+ */
+package com.bytedesk.core.notice.extra;
+
+public class NoticeExtraLogin {
+    
+}
