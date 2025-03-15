6fad9bf4e363465beb4532ea02627ba2cf390f77
jack ning <github@bytedesk.com>
2025-03-15 08:54:43 +0800

    update modules/core: add        1 mod        1 files

---
+++ modules/core/src/main/java/com/bytedesk/core/notice/NoticeProtobuf.java
@@ -0,0 +1,44 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2025-03-15 08:46:20
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-03-15 08:47:27
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ * 
+ * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
+ */
+package com.bytedesk.core.notice;
+
+import java.io.Serializable;
+
+import com.alibaba.fastjson2.JSON;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class NoticeProtobuf implements Serializable {
+
+    private String title;
+
+    private String content;
+
+    private String type;
+
+    private String extra;
+
+    public String toJson() {
+        return JSON.toJSONString(this);
+    }
+}
