b02353faa26e32e6d19cbbc74aed1e01495be035
jack ning <github@bytedesk.com>
2025-03-16 10:44:21 +0800

    update modules/core: mod        2 files

--- modules/core/src/main/java/com/bytedesk/core/notice/extra/NoticeExtraInvite.java
+++ modules/core/src/main/java/com/bytedesk/core/notice/extra/NoticeExtraInvite.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-08-05 08:06:43
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-15 14:45:50
+ * @LastEditTime: 2025-03-16 10:29:07
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -44,6 +44,9 @@ public class NoticeExtraInvite extends BaseExtra {
 
     // 发起邀请客服
     private UserProtobuf sender;
+
+    // 转接状态：invite_pending/invite_accepted/invite_rejected/invite_timeout
+    private String status;
 
     // 邀请消息uid，用于invite_accept/invite_reject
     private String messageUid;
