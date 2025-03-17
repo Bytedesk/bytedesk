3e7cb8dd60de4874be72582ed10cdc28fbcacf5c
jack ning <github@bytedesk.com>
2025-03-17 12:40:42 +0800

    update

--- modules/service/src/main/java/com/bytedesk/service/constant/I18ServiceConsts.java
+++ modules/service/src/main/java/com/bytedesk/service/constant/I18ServiceConsts.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-12-04 10:50:45
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-12-04 10:51:03
+ * @LastEditTime: 2025-03-17 12:19:25
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -19,6 +19,26 @@ public class I18ServiceConsts {
     private I18ServiceConsts() {}
 
     public static final String I18N_AGENT_EXISTS = I18Consts.I18N_PREFIX + "agent.exists";
+    // transfer notice title
+    public static final String I18N_TRANSFER_NOTICE_TITLE = I18Consts.I18N_PREFIX + "transfer.notice.title";
+    // transfer notice content
+    public static final String I18N_TRANSFER_NOTICE_CONTENT = I18Consts.I18N_PREFIX + "transfer.notice.content";
+    // transfer accept notice title
+    public static final String I18N_TRANSFER_ACCEPT_NOTICE_TITLE = I18Consts.I18N_PREFIX + "transfer.accept.notice.title";
+    // transfer accept notice content
+    public static final String I18N_TRANSFER_ACCEPT_NOTICE_CONTENT = I18Consts.I18N_PREFIX + "transfer.accept.notice.content";
+    // transfer reject notice title
+    public static final String I18N_TRANSFER_REJECT_NOTICE_TITLE = I18Consts.I18N_PREFIX + "transfer.reject.notice.title";
+    // transfer reject notice content
+    public static final String I18N_TRANSFER_REJECT_NOTICE_CONTENT = I18Consts.I18N_PREFIX + "transfer.reject.notice.content";
+    // transfer timeout notice title
+    public static final String I18N_TRANSFER_TIMEOUT_NOTICE_TITLE = I18Consts.I18N_PREFIX + "transfer.timeout.notice.title";
+    // transfer timeout notice content
+    public static final String I18N_TRANSFER_TIMEOUT_NOTICE_CONTENT = I18Consts.I18N_PREFIX + "transfer.timeout.notice.content";
+    // transfer cancel notice title
+    public static final String I18N_TRANSFER_CANCEL_NOTICE_TITLE = I18Consts.I18N_PREFIX + "transfer.cancel.notice.title";
+    // transfer cancel notice content
+    public static final String I18N_TRANSFER_CANCEL_NOTICE_CONTENT = I18Consts.I18N_PREFIX + "transfer.cancel.notice.content";
 
 
     
