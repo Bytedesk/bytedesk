fc17b460bca53fdba4f0f161f5688a670fb195e2
jack ning <github@bytedesk.com>
2025-03-17 21:36:49 +0800

    update

--- modules/kbase/src/main/java/com/bytedesk/kbase/faq/FaqRestController.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/faq/FaqRestController.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-03-22 22:59:07
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-17 21:19:14
+ * @LastEditTime: 2025-03-17 21:27:25
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -68,19 +68,19 @@ public class FaqRestController extends BaseRestController<FaqRequest> {
     }
 
     // up
-    @PostMapping("/up")
+    @PostMapping("/rate/up")
     public ResponseEntity<?> rateUp(@RequestBody FaqRequest request) {
 
-        FaqResponse faq = faqService.upVote(request.getUid());
+        FaqResponse faq = faqService.rateUp(request.getUid());
 
         return ResponseEntity.ok(JsonResult.success(faq));
     }
 
     // down
-    @PostMapping("/down")
+    @PostMapping("/rate/down")
     public ResponseEntity<?> rateDown(@RequestBody FaqRequest request) {
 
-        FaqResponse faq = faqService.downVote(request.getUid());
+        FaqResponse faq = faqService.rateDown(request.getUid());
 
         return ResponseEntity.ok(JsonResult.success(faq));
     }
