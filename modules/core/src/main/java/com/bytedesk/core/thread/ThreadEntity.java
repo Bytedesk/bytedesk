944b64ead67df91c8f1839b7f9b18ff5e2041bb3
jack ning <github@bytedesk.com>
2025-03-17 13:17:37 +0800

    update modules/service: mod        2 files

--- modules/core/src/main/java/com/bytedesk/core/thread/ThreadEntity.java
+++ modules/core/src/main/java/com/bytedesk/core/thread/ThreadEntity.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-17 12:27:34
+ * @LastEditTime: 2025-03-17 12:57:03
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -68,7 +68,9 @@ public class ThreadEntity extends AbstractThreadEntity {
     }
 
     public Boolean isStarted() {
-        return getState().equals(ThreadStateEnum.STARTED.name());
+        // return getState().equals(ThreadStateEnum.STARTED.name());
+        // not closed && not queuing
+        return !getState().equals(ThreadStateEnum.CLOSED.name()) && !getState().equals(ThreadStateEnum.QUEUING.name());
     }
 
     // is transfer pending
