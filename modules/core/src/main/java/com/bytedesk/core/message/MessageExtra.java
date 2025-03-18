bc8cb9c5757c28a5b9d05971b4da10703ad53293
jack ning <github@bytedesk.com>
2025-03-18 12:10:51 +0800

    update saas/core: mod        4 files

--- modules/core/src/main/java/com/bytedesk/core/message/MessageExtra.java
+++ modules/core/src/main/java/com/bytedesk/core/message/MessageExtra.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-15 09:49:22
+ * @LastEditTime: 2025-03-18 11:57:23
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -45,7 +45,8 @@ public class MessageExtra extends BaseExtra {
     private String feedback = BytedeskConsts.EMPTY_JSON_STRING;
     
     // 自动回复
-    private Boolean isAutoReply;
+    @Builder.Default
+    private Boolean isAutoReply = false;
     
     private String autoReplyType;
 
