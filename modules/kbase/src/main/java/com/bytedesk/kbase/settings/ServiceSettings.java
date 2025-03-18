4b0a884968993e3e43b3cae4d0253e0771eb2a4c
jack ning <github@bytedesk.com>
2025-03-18 18:24:28 +0800

    update

--- modules/kbase/src/main/java/com/bytedesk/kbase/settings/ServiceSettings.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/settings/ServiceSettings.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-06-14 10:45:08
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-17 22:39:36
+ * @LastEditTime: 2025-03-18 18:23:26
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -74,6 +74,11 @@ public class ServiceSettings implements Serializable {
     @NotBlank
     @Builder.Default
     private boolean showRateBtn = false;
+
+    // 关闭会话时自动发送满意度评价
+    @NotBlank
+    @Builder.Default
+    private boolean autoInviteRate = false;
     
     // TODO: 自定义评价最低消息数量，未达到最低对话消息数，禁止评价
     @NotBlank
