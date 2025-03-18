ae162891e6122a59eb354b46bdfb5c86821c91bb
jack ning <github@bytedesk.com>
2025-03-18 09:40:25 +0800

    update modules/service: add       18 mod       10 files

--- modules/core/src/main/java/com/bytedesk/core/thread/AbstractThreadEntity.java
+++ modules/core/src/main/java/com/bytedesk/core/thread/AbstractThreadEntity.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-06-29 13:00:33
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-17 14:39:59
+ * @LastEditTime: 2025-03-18 09:40:21
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
