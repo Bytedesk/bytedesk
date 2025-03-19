34f45df367cb7503c0d1a6be6ead91bc86c1c68d
jack ning <github@bytedesk.com>
2025-03-19 21:53:42 +0800

    Update TicketService.java

--- modules/ticket/src/main/java/com/bytedesk/ticket/ticket/TicketService.java
+++ modules/ticket/src/main/java/com/bytedesk/ticket/ticket/TicketService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-01-29 12:24:32
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-19 21:50:45
+ * @LastEditTime: 2025-03-19 21:51:57
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -34,6 +34,7 @@ import com.bytedesk.core.rbac.user.UserProtobuf;
 import com.bytedesk.core.rbac.user.UserTypeEnum;
 import com.bytedesk.core.thread.ThreadEntity;
 import com.bytedesk.core.thread.ThreadRestService;
+import com.bytedesk.core.topic.TopicService;
 // import com.bytedesk.core.thread.ThreadRestService;
 import com.bytedesk.service.agent.AgentEntity;
 import com.bytedesk.service.agent.AgentRestService;
@@ -74,6 +75,7 @@ public class TicketService {
     // private final ApplicationEventPublisher eventPublisher;
     private final TicketRestService ticketRestService;
     private final ThreadRestService threadRestService;
+    private final TopicService topicService;
 
     /**
      * 查询工单，并过滤掉没有任务的工单
@@ -374,12 +376,6 @@ public class TicketService {
                 if (thread != null) {
                     ticket.setThreadUid(thread.getUid());
                 }
-                // Optional<ThreadEntity> threadOptional = threadRestService.findByUid(ticket.getThreadUid());
-                // if (threadOptional.isPresent()) {
-                //     thread = threadOptional.get();
-                // } else {
-                //     thread = ticketRestService.createTicketThread(ticket);
-                // }
             }
             
             // 将claimer添加到会话中
@@ -390,9 +386,9 @@ public class TicketService {
                 thread.getTicketors().add(assigneeJson);
                 // 保存
                 threadRestService.save(thread);
-                // 同步到ticket
-                String acceptUserUid = acceptAgentOptional.get().getUserUid();
-                topicService.create(threadEntity.getTopic(), acceptUserUid);
+                // 添加订阅
+                String acceptUserUid = assigneeOptional.get().getUserUid();
+                topicService.create(thread.getTopic(), acceptUserUid);
             }
             
             // 6. 发布工单分配消息事件
