9ab602a85d535e9088f78a5f9153490cb8080b0f
jack ning <github@bytedesk.com>
2025-03-20 10:40:14 +0800

    update modules/service: mod        3 del        1 files

--- modules/core/src/main/java/com/bytedesk/core/rbac/role/RoleRestService.java
+++ modules/core/src/main/java/com/bytedesk/core/rbac/role/RoleRestService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-20 09:15:56
+ * @LastEditTime: 2025-03-20 10:14:22
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -130,7 +130,7 @@ public class RoleRestService extends BaseRestService<RoleEntity, RoleRequest, Ro
                 if (savedEntity == null) {
                         throw new RuntimeException("role " + request.getName() + " create failed");
                 }
-
+                // 
                 return convertToResponse(savedEntity);
         }
 
