997d53fc9f58bd45b14db5918c6dc5f5ad3e6db0
jack ning <github@bytedesk.com>
2025-03-20 13:16:56 +0800

    update

--- modules/core/src/main/java/com/bytedesk/core/rbac/authority/AuthorityRequest.java
+++ modules/core/src/main/java/com/bytedesk/core/rbac/authority/AuthorityRequest.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-20 12:37:54
+ * @LastEditTime: 2025-03-20 13:15:14
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -14,8 +14,11 @@
 package com.bytedesk.core.rbac.authority;
 
 import com.bytedesk.core.base.BaseRequest;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 import lombok.experimental.SuperBuilder;
 
@@ -23,6 +26,8 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @Accessors(chain = true)
 @EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthorityRequest extends BaseRequest {
 
     private String name;
