0e05b0658ce2c25e426ffd3a23e3e3207d167e97
jack ning <github@bytedesk.com>
2025-03-20 09:07:31 +0800

    update modules/core: mod        3 files

--- modules/core/src/main/java/com/bytedesk/core/rbac/role/RoleEntity.java
+++ modules/core/src/main/java/com/bytedesk/core/rbac/role/RoleEntity.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-15 10:24:38
+ * @LastEditTime: 2025-03-20 09:03:10
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -45,7 +45,8 @@ import com.bytedesk.core.rbac.authority.AuthorityEntity;
 @NoArgsConstructor
 @EntityListeners({ RoleEntityListener.class })
 @Table(name = "bytedesk_core_role", uniqueConstraints = {
-	// @UniqueConstraint(columnNames = { "name", "orgUid" }), // is_deleted=false 时不加限定
+// @UniqueConstraint(columnNames = { "name", "orgUid" }), // is_deleted=false
+// 时不加限定
 })
 public class RoleEntity extends BaseEntity {
 
@@ -66,14 +67,9 @@ public class RoleEntity extends BaseEntity {
 	@Column(name = "is_system", nullable = false)
 	private boolean system = false;
 
-
 	@Builder.Default
 	@ManyToMany
-	@JoinTable(
-		name = "bytedesk_core_role_authority", 
-		joinColumns = @JoinColumn(name = "role_id"), 
-		inverseJoinColumns = @JoinColumn(name = "authority_id")
-	)
+	@JoinTable(name = "bytedesk_core_role_authority", joinColumns = @JoinColumn(name = "role_id"), inverseJoinColumns = @JoinColumn(name = "authority_id"))
 	private Set<AuthorityEntity> authorities = new HashSet<>();
 
 	// 除系统自带角色之外，允许管理员-自己创建角色
