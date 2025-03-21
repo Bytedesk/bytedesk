eba30110002d03053e8703280c55a2b15c5aad1e
jack ning <github@bytedesk.com>
2025-03-21 12:12:10 +0800

    update modules/core: mod        1 files

--- modules/core/src/main/java/com/bytedesk/core/rbac/organization/OrganizationInitializer.java
+++ modules/core/src/main/java/com/bytedesk/core/rbac/organization/OrganizationInitializer.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-11-05 13:43:02
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-20 18:20:13
+ * @LastEditTime: 2025-03-21 12:08:58
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -52,7 +52,6 @@ public class OrganizationInitializer implements SmartInitializingSingleton {
     @Override
     public void afterSingletonsInstantiated() {
         // 
-        // authorityInitializer.init();
         roleInitializer.init();
         userInitializer.init();
         // 
@@ -78,7 +77,6 @@ public class OrganizationInitializer implements SmartInitializingSingleton {
                     .description(bytedeskProperties.getOrganizationName() + " Description")
                     .user(user)
                     .build();
-            // organization.setUid(BytedeskConsts.DEFAULT_ORGANIZATION_UID);
             //
            OrganizationEntity savedOrganization = organizationService.save(organization);
             if (savedOrganization != null) {
