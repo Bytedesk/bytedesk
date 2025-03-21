9f16249391f1802c0913c681f8b541d0e053e3fb
jack ning <github@bytedesk.com>
2025-03-21 11:42:01 +0800

    update modules/core: mod        3 files

--- modules/core/src/main/java/com/bytedesk/core/rbac/role/RoleEventListener.java
+++ modules/core/src/main/java/com/bytedesk/core/rbac/role/RoleEventListener.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-11-07 16:27:34
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-21 09:47:20
+ * @LastEditTime: 2025-03-21 11:41:44
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
