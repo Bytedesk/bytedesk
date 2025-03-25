eb7852f8d85b05008bb41332bb2eafbfe23ea89e
jack ning <github@bytedesk.com>
2025-03-25 17:52:15 +0800

    update modules/service: mod        1 files

--- modules/service/src/main/java/com/bytedesk/service/agent/AgentRestService.java
+++ modules/service/src/main/java/com/bytedesk/service/agent/AgentRestService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:19:51
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-21 16:56:07
+ * @LastEditTime: 2025-03-25 17:50:03
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -288,6 +288,7 @@ public class AgentRestService extends BaseRestService<AgentEntity, AgentRequest,
             }
             return convertToResponse(updatedAgent);
         }
+        
         return null;
     }
 
