894318c427a48fc6936472e04ba4d86465a2a0c3
jack ning <github@bytedesk.com>
2025-03-26 16:05:35 +0800

    update

--- modules/ticket/src/main/java/com/bytedesk/ticket/ticket/TicketRequest.java
+++ modules/ticket/src/main/java/com/bytedesk/ticket/ticket/TicketRequest.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-01-16 14:58:38
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-26 15:47:42
+ * @LastEditTime: 2025-03-26 15:53:44
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -15,9 +15,6 @@ package com.bytedesk.ticket.ticket;
 
 import java.util.Set;
 
-import org.springframework.util.StringUtils;
-
-import com.alibaba.fastjson2.JSON;
 import com.bytedesk.core.base.BaseRequest;
 import com.bytedesk.core.rbac.user.UserProtobuf;
 
@@ -49,15 +46,16 @@ public class TicketRequest extends BaseRequest {
     private String threadUid;
     private String categoryUid;
     // 
+    // private String workgroupUid;
     private String departmentUid;
     // 
     private Boolean assignmentAll;
-    private UserProtobuf assignee;
-    private UserProtobuf reporter;
-    
-    // 添加原始字符串字段存储JSON
-    private String assigneeString;
-    private String reporterString;
+    private String assigneeUid; // 方便get查询
+    // private String assignee; // 原始json字符串
+    private UserProtobuf assignee; // 方便post存储
+    private String reporterUid; // 方便get查询
+    // private String reporter;  // 原始 JSON 字符串
+    private UserProtobuf reporter; // 方便post存储
     // 
     private String startDate;
     private String endDate;
@@ -76,50 +74,17 @@ public class TicketRequest extends BaseRequest {
     private Boolean verified;
 
     public String getAssigneeJson() {
-        if (assignee != null) {
-            return assignee.toJson();
-        } else if (StringUtils.hasText(assigneeString)) {
-            try {
-                assignee = JSON.parseObject(assigneeString, UserProtobuf.class);
-                return assigneeString;
-            } catch (Exception e) {
-                log.error("解析assignee字符串失败: {}", assigneeString, e);
-            }
+        if (assignee == null) {
+            return null;
         }
-        return null;
+        return assignee.toJson();
+
     }
-        
+
     public String getReporterJson() {
-        if (reporter != null) {
-            return reporter.toJson();
-        } else if (StringUtils.hasText(reporterString)) {
-            try {
-                reporter = JSON.parseObject(reporterString, UserProtobuf.class);
-                return reporterString;
-            } catch (Exception e) {
-                log.error("解析reporter字符串失败: {}", reporterString, e);
-            }
+        if (reporter == null) {
+            return null;
         }
-        return null;
+        return reporter.toJson();
     }
-    
-    // 便捷方法，用于前端直接传uid的情况
-    public void setReporterUid(String uid) {
-        if (StringUtils.hasText(uid)) {
-            if (reporter == null) {
-                reporter = new UserProtobuf();
-            }
-            reporter.setUid(uid);
-        }
-    }
-    
-    // 便捷方法，用于前端直接传uid的情况
-    public void setAssigneeUid(String uid) {
-        if (StringUtils.hasText(uid)) {
-            if (assignee == null) {
-                assignee = new UserProtobuf();
-            }
-            assignee.setUid(uid);
-        }
-    }
-}
+} 
