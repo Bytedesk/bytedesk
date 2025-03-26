c926731ca9ea63e85545c3078ced28f4b1e675bb
jack ning <github@bytedesk.com>
2025-03-26 14:45:03 +0800

    update modules/ai: mod        1 files

--- modules/ai/src/main/java/com/bytedesk/ai/robot/RobotService.java
+++ modules/ai/src/main/java/com/bytedesk/ai/robot/RobotService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-03-11 17:29:51
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-21 14:44:58
+ * @LastEditTime: 2025-03-26 14:43:39
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -63,7 +63,7 @@ public class RobotService {
     private final RobotRestService robotRestService;
     private final MessageService messageService;
 
-    // 处理员工/客服SSE请求消息
+    // 处理内部成员SSE请求消息
     public void processSseMemberMessage(String messageJson, SseEmitter emitter) {
         log.info("processSseMemberMessage: messageJson: {}", messageJson);
         Assert.notNull(messageJson, "messageJson is null");
