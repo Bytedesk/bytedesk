678ed5ad3f155a80bca30e6904ec474d02918112
jack ning <github@bytedesk.com>
2025-03-27 21:00:44 +0800

    update

--- modules/core/src/main/java/com/bytedesk/core/message/MessageSendServiceImpl.java
+++ modules/core/src/main/java/com/bytedesk/core/message/MessageSendServiceImpl.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-10-15 16:27:35
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-15 16:52:16
+ * @LastEditTime: 2025-03-27 20:47:43
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -16,6 +16,8 @@ package com.bytedesk.core.message;
 import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson2.JSON;
+import com.bytedesk.core.annotation.BlackIpFilter;
+import com.bytedesk.core.annotation.BlackUserFilter;
 import com.bytedesk.core.annotation.TabooJsonFilter;
 import com.bytedesk.core.annotation.TabooProtobufFilter;
 import com.bytedesk.core.config.BytedeskEventPublisher;
@@ -27,6 +29,8 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class MessageSendServiceImpl implements IMessageSendService {
 
+    @BlackIpFilter(title = "black", action = "sendJsonMessage")
+    @BlackUserFilter(title = "black", action = "sendJsonMessage")
     @TabooJsonFilter(title = "taboo", action = "sendJsonMessage")
     @Override
     public void sendJsonMessage(String json) {
@@ -34,6 +38,8 @@ public class MessageSendServiceImpl implements IMessageSendService {
         publishMessageJsonEvent(json);
     }
 
+    @BlackIpFilter(title = "black", action = "sendProtobufMessage")
+    @BlackUserFilter(title = "black", action = "sendProtobufMessage")
     @TabooProtobufFilter(title = "taboo", action = "sendProtobufMessage")
     @Override
     public void sendProtobufMessage(MessageProtobuf messageProtobuf) {
