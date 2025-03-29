f78cd4af64690cf24d51f58b02c4ccffab3d5e45
jack ning <github@bytedesk.com>
2025-03-29 09:55:54 +0800

    update starter/src: add     2721 mod        1 files

---
+++ deploy/server/docs/zh-TW/assets/js/acecf23e.fcd005f4.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/docs/zh-TW/blog","blogTitle":"Blog","authorsListPath":"/docs/zh-TW/blog/authors"}')}}]);
---
+++ starter/src/main/resources/static/docs/zh-CN/assets/js/acecf23e.e30dda02.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[4284],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/docs/zh-CN/blog","blogTitle":"Blog","authorsListPath":"/docs/zh-CN/blog/authors"}')}}]);
