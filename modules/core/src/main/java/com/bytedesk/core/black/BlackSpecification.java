eeeec6d2c1d0ae06b0c6b3b74f2b39914f90d429
jack ning <github@bytedesk.com>
2025-03-29 23:33:46 +0800

    update modules/core: mod        2 files

--- modules/core/src/main/java/com/bytedesk/core/black/BlackSpecification.java
+++ modules/core/src/main/java/com/bytedesk/core/black/BlackSpecification.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-06-27 12:21:44
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-01-17 16:52:44
+ * @LastEditTime: 2025-03-29 23:32:54
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -28,7 +28,12 @@ public class BlackSpecification extends BaseSpecification {
      public static Specification<BlackEntity> search(BlackRequest request) {
         return (root, query, criteriaBuilder) -> {
             List<Predicate> predicates = new ArrayList<>();
-            predicates.addAll(getBasicPredicates(root, criteriaBuilder, request.getOrgUid()));
+            // predicates.addAll(getBasicPredicates(root, criteriaBuilder, request.getOrgUid()));
+            predicates.add(criteriaBuilder.equal(root.get("deleted"), false));
+            // 
+            if (StringUtils.hasText(request.getOrgUid())) {
+                predicates.add(criteriaBuilder.equal(root.get("orgUid"), request.getOrgUid()));
+            }
             // blackNickname
             if (StringUtils.hasText(request.getBlackNickname())) {
                 predicates.add(criteriaBuilder.like(root.get("blackNickname"), "%" + request.getBlackNickname() + "%"));
