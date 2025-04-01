33946d14488ebed8b301824a07089cce7fd5c3cb
jack ning <github@bytedesk.com>
2025-04-01 18:04:21 +0800

    update modules/service: mod        2 files

--- modules/voc/src/main/java/com/bytedesk/voc/complaint/ComplaintResponse.java
+++ modules/voc/src/main/java/com/bytedesk/voc/complaint/ComplaintResponse.java
@@ -1,8 +1,8 @@
 /*
  * @Author: jackning 270580156@qq.com
- * @Date: 2024-09-03 09:51:08
+ * @Date: 2024-09-03 09:51:28
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-09-03 09:51:11
+ * @LastEditTime: 2024-09-03 09:51:31
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -11,8 +11,8 @@
  *  联系：270580156@qq.com
  * Copyright (c) 2024 by bytedesk.com, All Rights Reserved. 
  */
-package com.bytedesk.service.complaint;
+package com.bytedesk.voc.complaint;
 
-public class ComplaintService {
+public class ComplaintResponse {
     
 }
