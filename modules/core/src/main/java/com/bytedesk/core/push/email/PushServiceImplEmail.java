cac1eb6ab837529300520418cf750a61a28efb0b
jack ning <github@bytedesk.com>
2025-04-01 23:32:46 +0800

    update modules/core: mod        1 files

--- modules/core/src/main/java/com/bytedesk/core/push/email/PushServiceImplEmail.java
+++ modules/core/src/main/java/com/bytedesk/core/push/email/PushServiceImplEmail.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-03-31 15:30:19
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-11-19 10:59:43
+ * @LastEditTime: 2025-04-01 23:11:41
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -193,4 +193,5 @@ public class PushServiceImplEmail extends PushNotifier {
             e.printStackTrace();
         }
     }
+
 }
