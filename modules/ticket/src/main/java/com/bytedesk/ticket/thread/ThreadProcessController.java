adc01d2a035dd4d7becd3a055ddfd97c49318580
jack ning <github@bytedesk.com>
2025-04-04 13:29:39 +0800

    update

---
+++ modules/ticket/src/main/java/com/bytedesk/ticket/thread/ThreadProcessController.java
@@ -0,0 +1,26 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2025-04-04 13:26:31
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-04-04 13:26:34
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ * 
+ * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
+ */
+package com.bytedesk.ticket.thread;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/thread/process")
+@AllArgsConstructor
+public class ThreadProcessController {
+    
+}
