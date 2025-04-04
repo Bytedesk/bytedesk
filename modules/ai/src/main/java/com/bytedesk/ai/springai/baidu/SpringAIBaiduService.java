4dd585bd5a584a5e135ce2c768df86d859234b04
jack ning <github@bytedesk.com>
2025-04-04 12:29:58 +0800

    update modules/ai: add        3 mod        7 files

---
+++ modules/ai/src/main/java/com/bytedesk/ai/springai/baidu/SpringAIBaiduService.java
@@ -0,0 +1,220 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2025-02-28 11:44:03
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-04-04 11:33:58
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ * 
+ * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
+ */
+package com.bytedesk.ai.springai.baidu;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.model.Generation;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import com.alibaba.fastjson2.JSON;
+import com.aliyun.oss.common.utils.StringUtils;
+import com.bytedesk.ai.robot.RobotRestService;
+import com.bytedesk.ai.springai.base.BaseSpringAIService;
+import com.bytedesk.ai.springai.spring.SpringAIVectorService;
+import com.bytedesk.core.message.IMessageSendService;
+import com.bytedesk.core.message.MessagePersistCache;
+import com.bytedesk.core.message.MessageProtobuf;
+import com.bytedesk.core.message.MessageTypeEnum;
+import com.bytedesk.core.thread.ThreadRestService;
+import com.bytedesk.core.uid.UidUtils;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@ConditionalOnProperty(name = "spring.ai.baidu.chat.enabled", havingValue = "true", matchIfMissing = false)
+public class SpringAIBaiduService extends BaseSpringAIService {
+
+    private final Optional<OpenAiChatModel> baiduChatModel;
+
+    public SpringAIBaiduService(
+            Optional<OpenAiChatModel> baiduChatModel,
+            Optional<SpringAIVectorService> springAIVectorService,
+            IMessageSendService messageSendService,
+            UidUtils uidUtils,
+            RobotRestService robotRestService,
+            ThreadRestService threadRestService,
+            MessagePersistCache messagePersistCache) {
+        super(springAIVectorService, messageSendService, uidUtils, robotRestService, threadRestService,
+                messagePersistCache);
+        this.baiduChatModel = baiduChatModel;
+
+    }
+
+    @Override
+    protected void processPrompt(Prompt prompt, MessageProtobuf messageProtobuf) {
+        baiduChatModel.ifPresent(model -> model.stream(prompt).subscribe(
+                response -> {
+                    if (response != null) {
+                        log.info("Baidu API response metadata: {}", response.getMetadata());
+                        List<Generation> generations = response.getResults();
+                        for (Generation generation : generations) {
+                            AssistantMessage assistantMessage = generation.getOutput();
+                            String textContent = assistantMessage.getText();
+
+                            messageProtobuf.setType(MessageTypeEnum.STREAM);
+                            messageProtobuf.setContent(textContent);
+                            messageSendService.sendProtobufMessage(messageProtobuf);
+                        }
+                    }
+                },
+                error -> {
+                    log.error("Baidu API error: ", error);
+                    messageProtobuf.setType(MessageTypeEnum.ERROR);
+                    messageProtobuf.setContent("服务暂时不可用，请稍后重试");
+                    messageSendService.sendProtobufMessage(messageProtobuf);
+                },
+                () -> {
+                    log.info("Chat stream completed");
+                    // 发送流结束标记
+                    // messageProtobuf.setType(MessageTypeEnum.STREAM_END);
+                    // messageProtobuf.setContent(""); // 或者可以是任何结束标记
+                    // messageSendService.sendProtobufMessage(messageProtobuf);
+                }));
+    }
+
+    @Override
+    protected String generateFaqPairs(String prompt) {
+        return baiduChatModel.map(model -> model.call(prompt)).orElse("");
+    }
+
+    @Override
+    protected String processPromptSync(String message) {
+        try {
+            return baiduChatModel.map(model -> model.call(message))
+                    .orElse("Baidu service is not available");
+        } catch (Exception e) {
+            log.error("Baidu API sync error: ", e);
+            return "服务暂时不可用，请稍后重试";
+        }
+    }
+
+    @Override
+    protected void processPromptSSE(Prompt prompt, MessageProtobuf messageProtobuf, SseEmitter emitter) {
+        baiduChatModel.ifPresentOrElse(
+                model -> {
+                    model.stream(prompt).subscribe(
+                            response -> {
+                                try {
+                                    if (response != null) {
+                                        List<Generation> generations = response.getResults();
+                                        for (Generation generation : generations) {
+                                            AssistantMessage assistantMessage = generation.getOutput();
+                                            String textContent = assistantMessage.getText();
+                                            log.info("Baidu API response metadata: {}, text {}",
+                                                    response.getMetadata(), textContent);
+                                            //
+                                            if (StringUtils.hasValue(textContent)) {
+                                                messageProtobuf.setContent(textContent);
+                                                messageProtobuf.setType(MessageTypeEnum.STREAM);
+                                                // 保存消息到数据库
+                                                String messageJson = JSON.toJSONString(messageProtobuf);
+                                                persistMessage(messageJson);
+                                                // 发送SSE事件
+                                                emitter.send(SseEmitter.event()
+                                                        .data(messageJson)
+                                                        .id(messageProtobuf.getUid())
+                                                        .name("message"));
+                                            }
+                                        }
+                                    }
+                                } catch (Exception e) {
+                                    log.error("Error sending SSE event", e);
+                                    messageProtobuf.setType(MessageTypeEnum.ERROR);
+                                    messageProtobuf.setContent("服务暂时不可用，请稍后重试");
+                                    // 保存消息到数据库
+                                    String messageJson = JSON.toJSONString(messageProtobuf);
+                                    persistMessage(messageJson);
+                                    //
+                                    try {
+                                        emitter.send(SseEmitter.event()
+                                                .data(messageJson)
+                                                .id(messageProtobuf.getUid())
+                                                .name("error"));
+                                        emitter.complete();
+                                    } catch (Exception ex) {
+                                        emitter.completeWithError(ex);
+                                    }
+                                }
+                            },
+                            error -> {
+                                log.error("Baidu API SSE error: ", error);
+                                //
+                                try {
+                                    messageProtobuf.setType(MessageTypeEnum.ERROR);
+                                    messageProtobuf.setContent("服务暂时不可用，请稍后重试");
+                                    // 保存消息到数据库
+                                    String messageJson = JSON.toJSONString(messageProtobuf);
+                                    persistMessage(messageJson);
+                                    // 发送SSE事件
+                                    emitter.send(SseEmitter.event()
+                                            .data(messageJson)
+                                            .id(messageProtobuf.getUid())
+                                            .name("message"));
+                                    emitter.complete();
+                                } catch (Exception ex) {
+                                    emitter.completeWithError(ex);
+                                }
+                            },
+                            () -> {
+                                log.info("Baidu API SSE complete");
+                                try {
+                                    // 发送流结束标记
+                                    messageProtobuf.setType(MessageTypeEnum.STREAM_END);
+                                    messageProtobuf.setContent(""); // 或者可以是任何结束标记
+                                    // 保存消息到数据库
+                                    String messageJson = JSON.toJSONString(messageProtobuf);
+                                    persistMessage(messageJson);
+                                    // 发送SSE事件
+                                    emitter.send(SseEmitter.event()
+                                            .data(messageJson)
+                                            .id(messageProtobuf.getUid())
+                                            .name("message"));
+                                    emitter.complete();
+                                } catch (Exception e) {
+                                    log.error("Error completing SSE", e);
+                                }
+                            });
+                },
+                () -> {
+                    messageProtobuf.setType(MessageTypeEnum.ERROR);
+                    messageProtobuf.setContent("服务暂时不可用，请稍后重试");
+                    // 保存消息到数据库
+                    String messageJson = JSON.toJSONString(messageProtobuf);
+                    persistMessage(messageJson);
+                    //
+                    try {
+                        emitter.send(SseEmitter.event()
+                                .data(messageJson)
+                                .id(messageProtobuf.getUid())
+                                .name("message"));
+                        emitter.complete();
+                    } catch (Exception ex) {
+                        emitter.completeWithError(ex);
+                    }
+                });
+    }
+
+    public Optional<OpenAiChatModel> getBaiduChatModel() {
+        return baiduChatModel;
+    }
+
+}
--- modules/ticket/src/main/java/com/bytedesk/ticket/thread/ThreadProcessEventListener.java
+++ modules/ticket/src/main/java/com/bytedesk/ticket/thread/ThreadProcessEventListener.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-04-01 14:08:03
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-04 11:11:59
+ * @LastEditTime: 2025-04-04 12:28:29
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -57,15 +57,12 @@ public class ThreadProcessEventListener {
         Map<String, Object> variables = new HashMap<>();
         // 基本变量
         variables.put(TicketConsts.TICKET_VARIABLE_THREAD_UID, thread.getUid());
-        // variables.put(TicketConsts.TICKET_VARIABLE_DEPARTMENT_UID, thread.getDepartmentUid());
         // variables.put(TicketConsts.TICKET_VARIABLE_REPORTER_UID, thread.getReporter().getUid());
         variables.put(TicketConsts.TICKET_VARIABLE_ORGUID, thread.getOrgUid());
         //
         // variables.put(TicketConsts.TICKET_VARIABLE_DESCRIPTION, thread.getDescription());
         // variables.put(TicketConsts.TICKET_VARIABLE_START_USER_ID, thread.getReporter().getUid());
         // variables.put(TicketConsts.TICKET_VARIABLE_STATUS, thread.getStatus());
-        // variables.put(TicketConsts.TICKET_VARIABLE_PRIORITY, thread.getPriority());
-        // variables.put(TicketConsts.TICKET_VARIABLE_CATEGORY_UID, thread.getCategoryUid());
         
         // 2. 启动流程实例
         ProcessInstance processInstance = runtimeService.createProcessInstanceBuilder()
