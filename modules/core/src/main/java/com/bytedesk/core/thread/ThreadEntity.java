1b44f0edb7acc105605f498cc359bf7d7f4e44bc
jack ning <github@bytedesk.com>
2025-04-05 12:03:41 +0800

    update

--- modules/core/src/main/java/com/bytedesk/core/thread/ThreadEntity.java
+++ modules/core/src/main/java/com/bytedesk/core/thread/ThreadEntity.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-04 14:05:38
+ * @LastEditTime: 2025-04-05 11:57:57
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
