e602cb101090f687b67ff7fd57da788675c74d9b
jack ning <github@bytedesk.com>
2025-04-08 18:36:59 +0800

    update modules/core: mod        1 files

--- modules/core/src/main/java/com/bytedesk/core/message/MessageEntity.java
+++ modules/core/src/main/java/com/bytedesk/core/message/MessageEntity.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-08 18:22:03
+ * @LastEditTime: 2025-04-08 18:35:17
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -41,7 +41,6 @@ public class MessageEntity extends AbstractMessageEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "thread_id", referencedColumnName = "id")
     private ThreadEntity thread;
-
 
     // 可以在这里添加 MessageEntity 特有的字段（如果有的话）
     public UserProtobuf getUserProtobuf() {
