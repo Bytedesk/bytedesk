6a3c408c2770b2378e3a7c5159cb935d137fc17c
jack ning <github@bytedesk.com>
2025-04-08 22:49:34 +0800

    update

--- modules/core/src/main/java/com/bytedesk/core/topic/TopicService.java
+++ modules/core/src/main/java/com/bytedesk/core/topic/TopicService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-04-13 16:14:36
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-08 22:42:36
+ * @LastEditTime: 2025-04-08 22:47:30
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -117,17 +117,16 @@ public class TopicService {
                 return;
             }
             topicElement.getTopics().add(topic);
-            // 
             save(topicElement);
         } else {
             // create
             final String uid = clientId.split("/")[0];
             TopicRequest topicRequest = TopicRequest.builder()
                     .topic(topic)
-                    // .userUid(uid)
+                    .userUid(uid)
                     // .qos(qos)
                     .build();
-            topicRequest.setUserUid(uid);
+            // topicRequest.setUserUid(uid);
             topicRequest.getClientIds().add(clientId);
             create(topicRequest);
         }
