d67497de650b70b5b4b78a6d1f687e0d88abf9a3
jack ning <github@bytedesk.com>
2025-04-08 19:34:14 +0800

    update modules/core: mod        5 files

--- modules/core/src/main/java/com/bytedesk/core/thread/AbstractThreadEntity.java
+++ modules/core/src/main/java/com/bytedesk/core/thread/AbstractThreadEntity.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-06-29 13:00:33
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-08 18:22:16
+ * @LastEditTime: 2025-04-08 18:59:14
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -132,9 +132,14 @@ public abstract class AbstractThreadEntity extends BaseEntity {
     private String note;
 
     // 是否离线
-    @Builder.Default
-    @Column(name = "is_offline")
-    private boolean offline = false;
+    // @Builder.Default
+    // @Column(name = "is_offline")
+    // private boolean offline = false;
+
+    // 是否robot
+    // @Builder.Default
+    // @Column(name = "is_robot")
+    // private boolean isRobot = false;
 
     // 标签
     @Builder.Default
--- modules/service/src/main/java/com/bytedesk/service/utils/ThreadMessageUtil.java
+++ modules/service/src/main/java/com/bytedesk/service/utils/ThreadMessageUtil.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-08-29 22:22:38
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-08 10:46:22
+ * @LastEditTime: 2025-04-08 18:48:30
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -70,7 +70,8 @@ public class ThreadMessageUtil {
                 .type(MessageTypeEnum.WELCOME.name())
                 .status(MessageStatusEnum.READ.name())
                 .client(ClientEnum.SYSTEM.name())
-                .user(thread.getAgent())
+                // .user(thread.getAgent())
+                .user(thread.getRobot())
                 .orgUid(thread.getOrgUid())
                 .extra(extra.toJson())
                 .createdAt(LocalDateTime.now())
