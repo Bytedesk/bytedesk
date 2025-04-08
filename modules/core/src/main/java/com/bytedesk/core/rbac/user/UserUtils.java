b71b1cad0b802a9e8d0e94ec850f8c8ff7f760e1
jack ning <github@bytedesk.com>
2025-04-08 10:34:23 +0800

    udpate

--- modules/core/src/main/java/com/bytedesk/core/rbac/user/UserUtils.java
+++ modules/core/src/main/java/com/bytedesk/core/rbac/user/UserUtils.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-08-31 16:20:44
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-03 10:44:46
+ * @LastEditTime: 2025-04-08 10:23:41
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -20,12 +20,14 @@ import com.bytedesk.core.constant.I18Consts;
 public class UserUtils {
     
     public static UserProtobuf getFileAssistantUser() {
-        UserProtobuf user = UserProtobuf.builder()
+        return UserProtobuf.builder()
+                .uid(BytedeskConsts.DEFAULT_FILE_ASSISTANT_UID)
                 .nickname(I18Consts.I18N_FILE_ASSISTANT_NAME)
                 .avatar(AvatarConsts.getDefaultFileAssistantAvatarUrl())
+                .type(UserTypeEnum.SYSTEM.name())
                 .build();
-        user.setUid(BytedeskConsts.DEFAULT_FILE_ASSISTANT_UID);
-        return user;
+        // user.setUid(BytedeskConsts.DEFAULT_FILE_ASSISTANT_UID);
+        // return user;
     }
 
     // public static UserProtobuf getClipboardAssistantUser() {
@@ -38,12 +40,12 @@ public class UserUtils {
     // }
 
     public static UserProtobuf getSystemUser() {
-        UserProtobuf user = UserProtobuf.builder()
+        return UserProtobuf.builder()
                 .uid(BytedeskConsts.DEFAULT_SYSTEM_UID)
                 .nickname(I18Consts.I18N_SYSTEM_NOTIFICATION_NAME)
                 .avatar(AvatarConsts.getDefaultSystemNotificationAvatarUrl())
+                .type(UserTypeEnum.SYSTEM.name())
                 .build();
-        return user;
     }
 
 }
