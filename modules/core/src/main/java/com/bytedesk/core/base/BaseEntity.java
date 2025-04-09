2d6a691ec1e5cc8a71b221002aaeaed8b75cb5b6
jack ning <github@bytedesk.com>
2025-04-09 22:06:01 +0800

    update modules/core: mod        7 files

--- modules/core/src/main/java/com/bytedesk/core/base/BaseEntity.java
+++ modules/core/src/main/java/com/bytedesk/core/base/BaseEntity.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-20 12:39:40
+ * @LastEditTime: 2025-04-09 21:37:11
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesa
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -93,6 +93,7 @@ public abstract class BaseEntity implements Serializable {
     private LocalDateTime updatedAt;
 
     // soft delete
+	@Builder.Default
 	@Column(name = "is_deleted")
 	private boolean deleted = false;
 
--- modules/core/src/main/java/com/bytedesk/core/base/BaseRequest.java
+++ modules/core/src/main/java/com/bytedesk/core/base/BaseRequest.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-21 12:53:56
+ * @LastEditTime: 2025-04-09 21:37:46
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -62,6 +62,7 @@ public abstract class BaseRequest implements Serializable {
     /**
      * 每页大小，默认10
      */
+    @Builder.Default
     protected int pageSize = 10;
 
     /**
