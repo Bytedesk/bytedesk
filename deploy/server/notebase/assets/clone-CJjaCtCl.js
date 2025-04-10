32da5845d1744b14f20df1e3498628d7baf64e67
jack ning <github@bytedesk.com>
2025-04-10 11:32:54 +0800

    update

---
+++ deploy/server/notebase/assets/clone-CJjaCtCl.js
@@ -0,0 +1,1 @@
+import{c as r}from"./graph-BonT1D2Y.js";var e=4;function a(o){return r(o,e)}export{a as c};
--- modules/service/src/main/java/com/bytedesk/service/message_leave/MessageLeaveNotifyTypeEnum.java
+++
@@ -1,7 +0,0 @@
-package com.bytedesk.service.message_leave;
-
-public enum MessageLeaveNotifyTypeEnum {
-    EMAIL,
-    MOBILE,
-    BOTH;
-}
