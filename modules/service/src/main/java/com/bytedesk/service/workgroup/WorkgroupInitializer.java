c974cac27e47f2ebd431a791bc8886c1b99b6017
jack ning <github@bytedesk.com>
2025-04-12 21:15:14 +0800

    update starter/src: mod        3 files

--- modules/service/src/main/java/com/bytedesk/service/workgroup/WorkgroupInitializer.java
+++ modules/service/src/main/java/com/bytedesk/service/workgroup/WorkgroupInitializer.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-11-05 13:43:02
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-03 17:29:45
+ * @LastEditTime: 2025-04-12 21:13:56
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -37,7 +37,6 @@ public class WorkgroupInitializer implements SmartInitializingSingleton {
 
     private final MemberInitializer memberInitializer;
     private final AgentInitializer agentInitializer;
-    // private final WorkgroupRepository workgroupRepository;
     private final WorkgroupRestService workgroupService;
     private final AuthorityRestService authorityService;
 
