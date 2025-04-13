81ed2bc3d97e7d7a788e35575d3b033435afa3d0
jack ning <github@bytedesk.com>
2025-04-13 21:48:31 +0800

    update modules/core: mod        3 files

--- modules/service/src/main/java/com/bytedesk/service/message_unread/MessageUnreadRequest.java
+++ modules/service/src/main/java/com/bytedesk/service/message_unread/MessageUnreadRequest.java
@@ -1,8 +1,8 @@
 /*
  * @Author: jackning 270580156@qq.com
- * @Date: 2024-08-01 06:18:10
+ * @Date: 2024-07-01 12:39:03
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-03 23:16:39
+ * @LastEditTime: 2025-04-13 21:02:29
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -11,30 +11,20 @@
  *  联系：270580156@qq.com
  * Copyright (c) 2024 by bytedesk.com, All Rights Reserved. 
  */
-package com.bytedesk.core.message_unread;
+package com.bytedesk.service.message_unread;
 
-import com.alibaba.excel.annotation.ExcelProperty;
-import com.alibaba.excel.annotation.write.style.ColumnWidth;
+import com.bytedesk.core.base.BaseRequest;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.SuperBuilder;
 
-/**
- * https://github.com/alibaba/easyexcel
- */
 @Data
-public class MessageUnreadExcel {
+@EqualsAndHashCode(callSuper = false)
+@SuperBuilder
+@AllArgsConstructor
+public class MessageUnreadRequest extends BaseRequest {
 
-    @ExcelProperty(index = 0, value = "Name")
-    @ColumnWidth(20)
-    private String name;
-
-    @ExcelProperty(index = 1, value = "Type")
-    @ColumnWidth(20)
-    private String type;
-
-    @ExcelProperty(index = 2, value = "Color")
-    @ColumnWidth(20)
-    private String color;
-
-    
+    // private String userUid;
 }
