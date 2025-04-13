917259ae1aefa4724ad89440b2937fa2059653bb
jack ning <github@bytedesk.com>
2025-04-13 23:33:37 +0800

    update

--- modules/kbase/src/main/java/com/bytedesk/kbase/taboo_message/TabooMessageEntity.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/taboo_message/TabooMessageEntity.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-02-22 16:51:20
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-20 12:58:22
+ * @LastEditTime: 2025-04-13 23:30:51
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -13,25 +13,14 @@
  */
 package com.bytedesk.kbase.taboo_message;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import com.bytedesk.core.message.AbstractMessageEntity;
 
-import com.bytedesk.core.base.BaseEntity;
-import com.bytedesk.core.constant.TypeConsts;
-import com.bytedesk.core.converter.StringListConverter;
-import com.bytedesk.core.message.MessageTypeEnum;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 import lombok.experimental.SuperBuilder;
 
@@ -42,39 +31,12 @@ import lombok.experimental.SuperBuilder;
 @Accessors(chain = true)
 @EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
-@NoArgsConstructor
+// @NoArgsConstructor
 @EntityListeners({TabooMessageEntityListener.class})
 @Table(name = "bytedesk_kbase_taboo_message")
-public class TabooMessageEntity extends BaseEntity {
+public class TabooMessageEntity  extends AbstractMessageEntity  {
 
-    private String content;
+    private static final long serialVersionUID = 1L;
 
-    @Builder.Default
-    @Column(name = "taboo_message_type", nullable = false)
-    private String type = MessageTypeEnum.TEXT.name();
-
-    // @Builder.Default
-    // private String level = LevelEnum.ORGANIZATION.name();
-
-    // @Builder.Default
-    // private String platform = PlatformEnum.BYTEDESK.name();
-
-    @Builder.Default
-    @Convert(converter = StringListConverter.class)
-    @Column(columnDefinition = TypeConsts.COLUMN_TYPE_TEXT)
-    private List<String> tagList = new ArrayList<>();
-
-    @Builder.Default
-    @Column(name = "is_enabled")
-    private boolean enabled = true;
-
-    // 有效开始日期
-    private LocalDateTime startDate;
-
-    // 有效结束日期
-    private LocalDateTime endDate;
-
-    private String categoryUid;
-
-    private String kbUid; // 对应知识库
+    // 
 }
--- modules/kbase/src/main/java/com/bytedesk/kbase/taboo_message/TabooMessageEventListener.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/taboo_message/TabooMessageEventListener.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-09-07 16:20:02
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-10 15:31:34
+ * @LastEditTime: 2025-04-13 23:32:28
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -13,11 +13,7 @@
  */
 package com.bytedesk.kbase.taboo_message;
 
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-
-import com.bytedesk.core.upload.UploadEntity;
-import com.bytedesk.core.upload.event.UploadCreateEvent;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -31,29 +27,10 @@ public class TabooMessageEventListener {
 
     // private final TabooMessageRestService taboo_messageService;
 
-    @EventListener
-    public void onUploadCreateEvent(UploadCreateEvent event) {
-        UploadEntity upload = event.getUpload();
-        log.info("UploadEventListener create: {}", upload.toString());
-        // if (upload.getType().equalsIgnoreCase(UploadTypeEnum.TABOO.name())) {
-        //     try {
-        //         // 导入Excel文件
-        //         Resource resource = uploadRestService.loadAsResource(upload.getFileName());
-        //         if (resource.exists()) {
-        //             String filePath = resource.getFile().getAbsolutePath();
-        //             log.info("UploadEventListener loadAsResource: {}", filePath);
-        //             // 导入敏感词
-        //             // 这里 需要指定读用哪个class去读，然后读取第一个sheet 文件流会自动关闭
-        //             // https://easyexcel.opensource.alibaba.com/docs/current/quickstart/read
-        //             EasyExcel.read(filePath, TabooMessageExcel.class, new TabooMessageExcelListener(taboo_messageService,
-        //                     // upload.getCategoryUid(),
-        //                     upload.getKbUid(),
-        //                     upload.getOrgUid())).sheet().doRead();
-        //         }
-        //     } catch (Exception e) {
-        //         log.error("TabooMessageEventListener onUploadCreateEvent error: {}", e.getMessage());
-        //     }
-        // }
-    }
+    // @EventListener
+    // public void onUploadCreateEvent(UploadCreateEvent event) {
+    //     UploadEntity upload = event.getUpload();
+    //     log.info("UploadEventListener create: {}", upload.toString());
+    // }
 
 }
