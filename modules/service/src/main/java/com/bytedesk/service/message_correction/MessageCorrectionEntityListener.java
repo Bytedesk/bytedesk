2b4d76258899fc992f36af40119677ad6e2e6e03
jack ning <github@bytedesk.com>
2025-04-13 23:12:38 +0800

    update

--- modules/service/src/main/java/com/bytedesk/service/message_correction/MessageCorrectionEntityListener.java
+++ modules/service/src/main/java/com/bytedesk/service/message_correction/MessageCorrectionEntityListener.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-02-25 09:52:34
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-06 18:14:18
+ * @LastEditTime: 2025-04-13 23:11:48
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -15,23 +15,20 @@ package com.bytedesk.service.message_correction;
 
 import org.springframework.stereotype.Component;
 
-import jakarta.persistence.PostPersist;
-import jakarta.persistence.PostUpdate;
-
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
 public class MessageCorrectionEntityListener {
 
-    @PostPersist
-    public void onPostPersist(MessageCorrectionEntity message_correction) {
-        log.info("onPostPersist: {}", message_correction);
-    }
+    // @PostPersist
+    // public void onPostPersist(MessageCorrectionEntity message_correction) {
+    //     log.info("onPostPersist: {}", message_correction);
+    // }
 
-    @PostUpdate
-    public void onPostUpdate(MessageCorrectionEntity message_correction) {
-        log.info("onPostUpdate: {}", message_correction);
-    }
+    // @PostUpdate
+    // public void onPostUpdate(MessageCorrectionEntity message_correction) {
+    //     log.info("onPostUpdate: {}", message_correction);
+    // }
     
 }
