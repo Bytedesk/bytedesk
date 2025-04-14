554896623f43c9cae6db80b8797b5acd18c1643e
jack ning <github@bytedesk.com>
2025-04-14 12:26:59 +0800

    update modules/ai: mod        4 files

--- modules/ai/src/main/java/com/bytedesk/ai/robot_message/RobotMessageExcel.java
+++ modules/ai/src/main/java/com/bytedesk/ai/robot_message/RobotMessageExcel.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-04-14 07:12:12
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-14 07:12:16
+ * @LastEditTime: 2025-04-14 12:24:19
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -13,16 +13,31 @@
  */
 package com.bytedesk.ai.robot_message;
 
-import com.bytedesk.core.base.BaseExcel;
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.write.style.ColumnWidth;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 @Data
-@EqualsAndHashCode(callSuper = false)
-// @SuperBuilder
-@AllArgsConstructor
-public class RobotMessageExcel extends BaseExcel {
+public class RobotMessageExcel {
     
+    @ExcelProperty(value = "内容")
+    @ColumnWidth(20)
+    private String content;
+
+    @ExcelProperty(value = "类型")
+    @ColumnWidth(20)
+    private String type;
+
+    @ExcelProperty(value = "访客")
+    @ColumnWidth(20)
+    private String user;
+
+    @ExcelProperty(value = "机器人")
+    @ColumnWidth(20)
+    private String robot;
+
+    @ExcelProperty(value = "时间")
+    @ColumnWidth(20)
+    private String createdAt;
 }
--- modules/core/src/main/java/com/bytedesk/core/message/MessageRestService.java
+++ modules/core/src/main/java/com/bytedesk/core/message/MessageRestService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-14 11:53:54
+ * @LastEditTime: 2025-04-14 11:57:23
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -197,7 +197,7 @@ public class MessageRestService extends BaseRestServiceWithExcel<MessageEntity, 
         MessageExcel messageExcel = new MessageExcel();
         messageExcel.setType(entity.getType());
         messageExcel.setContent(entity.getContent());
-        messageExcel.setSender(entity.getUser());
+        messageExcel.setSender(entity.getUserProtobuf().getNickname());
         messageExcel.setCreatedAt(entity.getCreatedAt().toString());
         return messageExcel;
     }
