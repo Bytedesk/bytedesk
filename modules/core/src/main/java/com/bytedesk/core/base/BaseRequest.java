548b98ce99fd5f03aa1a451c3f151ce1240e3205
jack ning <github@bytedesk.com>
2025-04-15 17:02:13 +0800

    update modules/core: mod        4 files

--- modules/core/src/main/java/com/bytedesk/core/base/BaseRequest.java
+++ modules/core/src/main/java/com/bytedesk/core/base/BaseRequest.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-09 21:37:46
+ * @LastEditTime: 2025-04-15 16:49:32
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -98,6 +98,9 @@ public abstract class BaseRequest implements Serializable {
     // 默认bytedesk平台
     @Builder.Default
     private String platform = PlatformEnum.BYTEDESK.name();
+
+    @Builder.Default
+    private boolean isSuperUser = false;
 
     /**
      * 获取分页对象
