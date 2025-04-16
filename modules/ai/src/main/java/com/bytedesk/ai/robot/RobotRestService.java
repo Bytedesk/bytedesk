d50ccd4dac7efbfb309b7852142e71eed9e07e70
jack ning <github@bytedesk.com>
2025-04-16 13:29:24 +0800

    update modules/ai: mod        1 files

--- modules/ai/src/main/java/com/bytedesk/ai/robot/RobotRestService.java
+++ modules/ai/src/main/java/com/bytedesk/ai/robot/RobotRestService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-03-22 16:44:41
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-16 12:27:49
+ * @LastEditTime: 2025-04-16 12:46:25
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -544,8 +544,8 @@ public class RobotRestService extends BaseRestService<RobotEntity, RobotRequest,
     }
 
     @Override
-    public void delete(RobotRequest entity) {
-        deleteByUid(entity.getUid());
+    public void delete(RobotRequest request) {
+        deleteByUid(request.getUid());
     }
 
     @Override
