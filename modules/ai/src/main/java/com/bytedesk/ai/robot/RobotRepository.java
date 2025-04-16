2e8f9d2b92ddcf4ebbb90f9e661c6d5c87c29c40
jack ning <github@bytedesk.com>
2025-04-16 14:02:59 +0800

    update modules/ai: mod        3 files

--- modules/ai/src/main/java/com/bytedesk/ai/robot/RobotRepository.java
+++ modules/ai/src/main/java/com/bytedesk/ai/robot/RobotRepository.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-03-22 16:44:54
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-24 18:03:30
+ * @LastEditTime: 2025-04-16 13:59:44
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -36,9 +36,6 @@ public interface RobotRepository extends JpaRepository<RobotEntity, Long>, JpaSp
 
     Boolean existsByNicknameAndOrgUidAndDeleted(String nickname, String orgUid, Boolean deleted);
 
-    // 查找 level === orgnization 且 deleted === false 且 type === SERVICE 的机器人
     List<RobotEntity> findByLevelAndDeletedFalseAndType(String level, String type);
-
-    // 查找 level === orgnization 且 deleted === false 的机器人
 
 }
