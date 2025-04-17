a07e98bfd7b0db650c7f349ec07212b708e2dd0e
jack ning <github@bytedesk.com>
2025-04-17 10:18:29 +0800

    update modules/ai: mod        4 files

--- modules/ai/src/main/java/com/bytedesk/ai/vector_store/impl/ElasticsearchVectorDBService.java
+++ modules/ai/src/main/java/com/bytedesk/ai/vector_store/impl/ElasticsearchVectorDBService.java
@@ -7,7 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.SearchRequest;
-import org.springframework.ai.elasticsearch.vectorstore.ElasticsearchVectorStore;
+import org.springframework.ai.vectorstore.elasticsearch.ElasticsearchVectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
