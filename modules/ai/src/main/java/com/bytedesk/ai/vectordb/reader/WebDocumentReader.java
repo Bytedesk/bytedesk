3e8f48bbbdfc88a33d97d2d24f22d387cd0beb3d
jack ning <github@bytedesk.com>
2025-04-17 09:43:44 +0800

    update modules/ai: mod        5 files

--- modules/ai/src/main/java/com/bytedesk/ai/vectordb/reader/WebDocumentReader.java
+++ modules/ai/src/main/java/com/bytedesk/ai/vectordb/reader/WebDocumentReader.java
@@ -11,7 +11,7 @@
  * 
  * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
  */
-package com.bytedesk.ai.utils.reader;
+package com.bytedesk.ai.vectordb.reader;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
