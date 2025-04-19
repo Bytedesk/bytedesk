a9b36b8498ee0a92342d3b946b09b04f1fc90470
jack ning <github@bytedesk.com>
2025-04-19 15:22:41 +0800

    update

--- modules/kbase/src/main/java/com/bytedesk/kbase/quick_reply/QuickReplyInitializer.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/quick_reply/QuickReplyInitializer.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-11-05 13:43:02
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-24 13:20:02
+ * @LastEditTime: 2025-04-19 15:16:24
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -16,22 +16,29 @@ package com.bytedesk.kbase.quick_reply;
 import org.springframework.beans.factory.SmartInitializingSingleton;
 import org.springframework.stereotype.Component;
 
+import com.bytedesk.core.constant.BytedeskConsts;
+
 import lombok.AllArgsConstructor;
 
 @Component
 @AllArgsConstructor
 public class QuickReplyInitializer implements SmartInitializingSingleton {
 
-    // private final QuickReplyRestService quickReplyRestService;
+    private final QuickReplyRestService quickReplyRestService;
 
     @Override
     public void afterSingletonsInstantiated() {
         // 为保证执行顺序，迁移到KbaseInitializer中
-        // String orgUid = BytedeskConsts.DEFAULT_ORGANIZATION_UID;
+        
+    }
+
+    // 迁移到kbaseInitializer
+    public void init() {
+        String orgUid = BytedeskConsts.DEFAULT_ORGANIZATION_UID;
         // 初始化快捷回复分类
-        // quickReplyRestService.initQuickReplyCategory(orgUid);
+        quickReplyRestService.initQuickReplyCategory(orgUid);
         // 初始化快捷回复
-        // quickReplyRestService.initQuickReply(orgUid);
+        quickReplyRestService.initQuickReply(orgUid);
     }
 
     
