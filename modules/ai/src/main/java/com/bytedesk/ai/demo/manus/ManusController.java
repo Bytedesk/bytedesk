688229a4d99e697ff2fccc5a847ecf7131d5881d
jack ning <github@bytedesk.com>
2025-04-19 13:22:20 +0800

    update modules/ai: mod        2 del        1 files

--- modules/ai/src/main/java/com/bytedesk/ai/demo/manus/ManusController.java
+++ modules/ai/src/main/java/com/bytedesk/ai/demo/manus/ManusController.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 202ManusControllerT4
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-25 09:58:09
+ * @LastEditTime: 2025-04-19 13:08:49
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -13,35 +13,35 @@
  */
 package com.bytedesk.ai.demo.manus;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.chrome.ChromeDriver;
+// import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RestController;
 
-import lombok.RequiredArgsConstructor;
+// import lombok.RequiredArgsConstructor;
 
-@RestController
-@RequestMapping("/manus")
-@RequiredArgsConstructor
-@ConditionalOnProperty(name = "spring.ai.manus.enabled", havingValue = "true", matchIfMissing = false)
-public class ManusController {
+// @RestController
+// @RequestMapping("/manus")
+// @RequiredArgsConstructor
+// @ConditionalOnProperty(name = "spring.ai.manus.enabled", havingValue = "true", matchIfMissing = false)
+// public class ManusController {
 
-    // private final WebDriver chromeDriver = new ChromeDriver();
+//     // private final WebDriver chromeDriver = new ChromeDriver();
 
-    // http://127.0.0.1:9003/manus/open?url=https://weiyuai.cn
-    @RequestMapping("/open")
-    public void open(@RequestParam(value = "url", defaultValue = "https://weiyuai.cn") String url) {
-        WebDriver chromeDriver = new ChromeDriver();
-        chromeDriver.get(url);
-    }
+//     // http://127.0.0.1:9003/manus/open?url=https://weiyuai.cn
+//     @RequestMapping("/open")
+//     public void open(@RequestParam(value = "url", defaultValue = "https://weiyuai.cn") String url) {
+//         WebDriver chromeDriver = new ChromeDriver();
+//         chromeDriver.get(url);
+//     }
 
-    // http://127.0.0.1:9003/manus/close
-    @RequestMapping("/close")
-    public void close() {
-        // chromeDriver.quit();
-    }
+//     // http://127.0.0.1:9003/manus/close
+//     @RequestMapping("/close")
+//     public void close() {
+//         // chromeDriver.quit();
+//     }
     
     
-}
+// }
