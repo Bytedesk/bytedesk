99aec7cdd4b9c821d9690868164a10de6abadf15
jack ning <github@bytedesk.com>
2025-04-19 11:06:36 +0800

    update modules/ai: mod        1 files

--- modules/ai/src/main/java/com/bytedesk/ai/robot/RobotRequest.java
+++ modules/ai/src/main/java/com/bytedesk/ai/robot/RobotRequest.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-03-22 16:45:07
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-27 16:12:28
+ * @LastEditTime: 2025-04-19 11:02:23
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -65,9 +65,6 @@ public class RobotRequest extends BaseRequest {
     // 机器人分类
     private String categoryUid;
 
-    // @Builder.Default
-    // private String level = LevelEnum.ORGANIZATION.name();
-    
     @Builder.Default
     private Boolean published = false;
 
