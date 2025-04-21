0f2460c7dc682b08177f10dd358904f6572dbe66
jack ning <github@bytedesk.com>
2025-04-21 18:32:39 +0800

    update modules/ai: mod        3 files

--- modules/ai/src/main/java/com/bytedesk/ai/demo/airline/AirlineConfig.java
+++ modules/ai/src/main/java/com/bytedesk/ai/demo/airline/AirlineConfig.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-02-22 11:15:31
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-21 18:25:03
+ * @LastEditTime: 2025-04-21 18:27:00
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -13,14 +13,7 @@
  */
 package com.bytedesk.ai.demo.airline;
 
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
-import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
-import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
-import org.springframework.ai.chat.memory.InMemoryChatMemory;
-import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -44,34 +37,34 @@ public class AirlineConfig {
     // proceeding.
     // """;
 
-    private String customerSupportAssistantSystemPrompt = """
-            	您是"Funnair"航空公司的客户聊天支持代理。请以友好、乐于助人且愉快的方式来回复。
-            	您正在通过在线聊天系统与客户互动。
-            	您能够支持已有机票的预订详情查询、机票日期改签、机票预订取消等操作，其余功能将在后续版本中添加，如果用户问的问题不支持请告知详情。
-               在提供有关机票预订详情查询、机票日期改签、机票预订取消等操作之前，您必须始终从用户处获取以下信息：预订号、客户姓名。
-               在询问用户之前，请检查消息历史记录以获取预订号、客户姓名等信息，尽量避免重复询问给用户造成困扰。
-               在更改预订之前，您必须确保条款允许这样做。
-               如果更改需要收费，您必须在继续之前征得用户同意。
-               使用提供的功能获取预订详细信息、更改预订和取消预订。
-               如果需要，您可以调用相应函数辅助完成。
-               请讲中文。
-               今天的日期是 {current_date}.
-            """;
+//     private String customerSupportAssistantSystemPrompt = """
+//             	您是"Funnair"航空公司的客户聊天支持代理。请以友好、乐于助人且愉快的方式来回复。
+//             	您正在通过在线聊天系统与客户互动。
+//             	您能够支持已有机票的预订详情查询、机票日期改签、机票预订取消等操作，其余功能将在后续版本中添加，如果用户问的问题不支持请告知详情。
+//                在提供有关机票预订详情查询、机票日期改签、机票预订取消等操作之前，您必须始终从用户处获取以下信息：预订号、客户姓名。
+//                在询问用户之前，请检查消息历史记录以获取预订号、客户姓名等信息，尽量避免重复询问给用户造成困扰。
+//                在更改预订之前，您必须确保条款允许这样做。
+//                如果更改需要收费，您必须在继续之前征得用户同意。
+//                使用提供的功能获取预订详细信息、更改预订和取消预订。
+//                如果需要，您可以调用相应函数辅助完成。
+//                请讲中文。
+//                今天的日期是 {current_date}.
+//             """;
 
-    @Bean("dashScopeCustomerSupportAssistant")
-    @ConditionalOnProperty(name = "spring.ai.dashscope.chat.enabled", havingValue = "true", matchIfMissing = false)
-    public ChatClient dashScopeCustomerSupportAssistant(
-            ChatClient.Builder bytedeskDashScopeChatClientBuilder,
-            InMemoryChatMemory defaultChatMemory,
-            VectorStore bytedeskOllamaRedisVectorStore) {
+//     @Bean("dashScopeCustomerSupportAssistant")
+//     @ConditionalOnProperty(name = "spring.ai.dashscope.chat.enabled", havingValue = "true", matchIfMissing = false)
+//     public ChatClient dashScopeCustomerSupportAssistant(
+//             ChatClient.Builder bytedeskDashScopeChatClientBuilder,
+//             InMemoryChatMemory defaultChatMemory,
+//             VectorStore bytedeskOllamaRedisVectorStore) {
 
-        return bytedeskDashScopeChatClientBuilder
-                .defaultSystem(customerSupportAssistantSystemPrompt)
-                .defaultAdvisors(
-                        new MessageChatMemoryAdvisor(defaultChatMemory),
-                        new QuestionAnswerAdvisor(bytedeskOllamaRedisVectorStore),
-                        new SimpleLoggerAdvisor())
-                .defaultTools("getBookingDetails", "changeBooking", "cancelBooking")
-                .build();
-    }
+//         return bytedeskDashScopeChatClientBuilder
+//                 .defaultSystem(customerSupportAssistantSystemPrompt)
+//                 .defaultAdvisors(
+//                         new MessageChatMemoryAdvisor(defaultChatMemory),
+//                         new QuestionAnswerAdvisor(bytedeskOllamaRedisVectorStore),
+//                         new SimpleLoggerAdvisor())
+//                 .defaultTools("getBookingDetails", "changeBooking", "cancelBooking")
+//                 .build();
+//     }
 }
