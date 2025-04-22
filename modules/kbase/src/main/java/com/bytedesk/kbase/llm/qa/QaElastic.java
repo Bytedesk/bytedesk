15286a65359f1319fb9df52a3d167d475990e722
jack ning <github@bytedesk.com>
2025-04-22 17:10:40 +0800

    update

--- modules/kbase/src/main/java/com/bytedesk/kbase/llm/qa/QaElastic.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/llm/qa/QaElastic.java
@@ -1,10 +1,12 @@
 package com.bytedesk.kbase.llm.qa;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
