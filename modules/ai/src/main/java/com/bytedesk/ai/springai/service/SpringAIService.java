61c7da2b0d898342b30994df5773fb0e463bfe2c
jack ning <github@bytedesk.com>
2025-04-22 18:04:58 +0800

    update modules/ai: mod        4 files

--- modules/ai/src/main/java/com/bytedesk/ai/springai/service/SpringAIService.java
+++ modules/ai/src/main/java/com/bytedesk/ai/springai/service/SpringAIService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-02-26 14:48:03
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-22 11:42:53
+ * @LastEditTime: 2025-04-22 17:43:56
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -58,6 +58,4 @@ public interface SpringAIService {
     // void persistMessage(String messageJson);
     void persistMessage(MessageProtobuf messageProtobufQuery, MessageProtobuf messageProtobufReply);
 
-
-    
 }
