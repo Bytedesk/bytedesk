e721fb0a03ae6e101433f3314cc53bae80d749be
jack ning <github@bytedesk.com>
2025-04-22 22:55:05 +0800

    update modules/kbase: add        1 mod        3 files

--- modules/kbase/src/main/java/com/bytedesk/kbase/llm/qa/QaRepository.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/llm/qa/QaRepository.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-03-22 22:59:32
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-01-13 11:14:22
+ * @LastEditTime: 2025-04-22 22:36:08
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -24,6 +24,8 @@ public interface QaRepository extends JpaRepository<QaEntity, Long>, JpaSpecific
     Optional<QaEntity> findByUid(String uid);
 
     Boolean existsByUid(String uid);
+
+    List<QaEntity> findByKbaseEntity_Uid(String kbUid);
 
     // auto complete, 根据问题关键字查询
     List<QaEntity> findByQuestionContains(String question);
