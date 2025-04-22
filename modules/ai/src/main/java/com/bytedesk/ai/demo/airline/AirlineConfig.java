647a2cb4b46e403aa707341dcb234cf7f9cbf83e
jack ning <github@bytedesk.com>
2025-04-22 18:37:08 +0800

    update modules/ai: mod        7 files

--- modules/ai/src/main/java/com/bytedesk/ai/demo/airline/AirlineConfig.java
+++ modules/ai/src/main/java/com/bytedesk/ai/demo/airline/AirlineConfig.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-02-22 11:15:31
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-21 18:27:00
+ * @LastEditTime: 2025-04-22 18:29:55
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -13,14 +13,14 @@
  */
 package com.bytedesk.ai.demo.airline;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+// import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ConditionalOnProperty(name = {
-        "spring.ai.ollama.embedding.enabled",
-        "spring.ai.vectorstore.redis.initialize-schema"
-}, havingValue = "true", matchIfMissing = true)
+// @Configuration
+// @ConditionalOnProperty(name = {
+//         "spring.ai.ollama.embedding.enabled",
+//         "spring.ai.vectorstore.redis.initialize-schema"
+// }, havingValue = "true", matchIfMissing = true)
 public class AirlineConfig {
 
     // https://java2ai.com/docs/1.0.0-M5.1/practices/playground-flight-booking/?spm=4347728f.3651acb5.0.0.491e3bbcfEUpsq
--- modules/ai/src/main/java/com/bytedesk/ai/springai/config/SpringAIConfig.java
+++ modules/ai/src/main/java/com/bytedesk/ai/springai/config/SpringAIConfig.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-02-12 12:09:13
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-07 17:17:43
+ * @LastEditTime: 2025-04-22 18:31:08
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -13,46 +13,46 @@
  */
 package com.bytedesk.ai.springai.config;
 
-import java.util.Optional;
+// import java.util.Optional;
 
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.memory.InMemoryChatMemory;
-import org.springframework.ai.ollama.OllamaChatModel;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+// import org.springframework.ai.chat.client.ChatClient;
+// import org.springframework.ai.chat.memory.InMemoryChatMemory;
+// import org.springframework.ai.ollama.OllamaChatModel;
+// import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Primary;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+// import lombok.RequiredArgsConstructor;
+// import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
-@Configuration
-@ConditionalOnProperty(name = "spring.ai.ollama.chat.enabled", havingValue = "true", matchIfMissing = true)
-@RequiredArgsConstructor
-public class SpringAIConfig {
+// @Slf4j
+// @Configuration
+// @ConditionalOnProperty(name = "spring.ai.ollama.chat.enabled", havingValue = "true", matchIfMissing = true)
+// @RequiredArgsConstructor
+// public class SpringAIConfig {
 
-    private final Optional<OllamaChatModel> ollamaChatModel;
+//     private final Optional<OllamaChatModel> ollamaChatModel;
 
-    // https://docs.spring.io/spring-ai/reference/api/chatclient.html
-    @Primary
-    @Bean("defaultChatClient")
-    @ConditionalOnProperty(name = "spring.ai.ollama.chat.enabled", havingValue = "true", matchIfMissing = false)
-    ChatClient defaultChatClient() {
-        if (ollamaChatModel.isPresent()) {
-            return ChatClient.builder(ollamaChatModel.get())
-                .defaultSystem("You are a friendly chat bot that answers question in the voice of a {voice}")
-                .build();
-        }
-        return null;
-    }
+//     // https://docs.spring.io/spring-ai/reference/api/chatclient.html
+//     @Primary
+//     @Bean("defaultChatClient")
+//     @ConditionalOnProperty(name = "spring.ai.ollama.chat.enabled", havingValue = "true", matchIfMissing = false)
+//     ChatClient defaultChatClient() {
+//         if (ollamaChatModel.isPresent()) {
+//             return ChatClient.builder(ollamaChatModel.get())
+//                 .defaultSystem("You are a friendly chat bot that answers question in the voice of a {voice}")
+//                 .build();
+//         }
+//         return null;
+//     }
 
-    // chatMemory
-    @Bean("defaultChatMemory")
-    InMemoryChatMemory defaultChatMemory() {
-        return new InMemoryChatMemory();
-    }
+//     // chatMemory
+//     @Bean("defaultChatMemory")
+//     InMemoryChatMemory defaultChatMemory() {
+//         return new InMemoryChatMemory();
+//     }
 
    
 
-}
+// }
