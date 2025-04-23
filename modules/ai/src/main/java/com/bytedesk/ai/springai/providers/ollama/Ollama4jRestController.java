aed2a18824309e24a26a507e0936d80a0bef60ee
jack ning <github@bytedesk.com>
2025-04-23 13:18:46 +0800

    Update Ollama4jRestController.java

--- modules/ai/src/main/java/com/bytedesk/ai/springai/providers/ollama/Ollama4jRestController.java
+++ modules/ai/src/main/java/com/bytedesk/ai/springai/providers/ollama/Ollama4jRestController.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-11-11 13:19:23
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-22 18:55:55
+ * @LastEditTime: 2025-04-23 13:10:29
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
