1f1969672b887ffffe82ab3f5b6f68cbe233503b
jack ning <github@bytedesk.com>
2025-04-23 18:10:28 +0800

    update modules/core: mod        3 files

--- modules/core/src/main/java/com/bytedesk/core/upload/UploadSpecification.java
+++ modules/core/src/main/java/com/bytedesk/core/upload/UploadSpecification.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-07-26 12:03:07
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-22 14:48:17
+ * @LastEditTime: 2025-04-23 18:09:50
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -30,7 +30,8 @@ public class UploadSpecification extends BaseSpecification {
         log.info("request: {}", request);
         return (root, query, criteriaBuilder) -> {
             List<Predicate> predicates = new ArrayList<>();
-            predicates.addAll(getBasicPredicates(root, criteriaBuilder, request.getOrgUid()));
+            // predicates.addAll(getBasicPredicates(root, criteriaBuilder, request.getOrgUid()));
+            predicates.add(criteriaBuilder.equal(root.get("deleted"), false));
 
             // nickname 查询 user
             if (StringUtils.hasText(request.getNickname())) {
@@ -50,6 +51,11 @@ public class UploadSpecification extends BaseSpecification {
             // 知识库
             if (StringUtils.hasText(request.getKbUid())) {
                 predicates.add(criteriaBuilder.equal(root.get("kbUid"), request.getKbUid()));
+            }
+
+            // orgUid
+            if (StringUtils.hasText(request.getOrgUid())) {
+                predicates.add(criteriaBuilder.equal(root.get("orgUid"), request.getOrgUid()));
             }
             //
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
