5f6b7c222798e9397e3245fe625268162d7f535c
jack ning <github@bytedesk.com>
2025-04-23 19:04:10 +0800

    Update QaElastic.java

--- modules/kbase/src/main/java/com/bytedesk/kbase/llm/qa/QaElastic.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/llm/qa/QaElastic.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-04-22 16:49:05
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-23 18:59:56
+ * @LastEditTime: 2025-04-23 19:02:12
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
