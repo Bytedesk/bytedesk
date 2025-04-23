94eb91b6b70cb80b0347ce729480cacafdaf2266
jack ning <github@bytedesk.com>
2025-04-23 11:10:14 +0800

    update modules/ai: mod        5 files

--- modules/ai/src/main/java/com/bytedesk/ai/robot/RobotLlm.java
+++ modules/ai/src/main/java/com/bytedesk/ai/robot/RobotLlm.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-06-05 10:02:51
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-23 09:28:16
+ * @LastEditTime: 2025-04-23 11:05:47
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -42,14 +42,6 @@ public class RobotLlm {
     @Column(name = "is_llm_enabled")
     private boolean enabled = true;
 
-    @Builder.Default
-    @Column(name = "llm_top_k")
-    private int topK = 3;
-
-    @Builder.Default
-    @Column(name = "llm_score_threshold")
-    private Double scoreThreshold = 0.5;
-
     // chat model provider
     @Builder.Default
     @Column(name = "llm_provider")
@@ -85,6 +77,14 @@ public class RobotLlm {
     // private String embeddingModel = "embedding-v2";
     // // private String embeddingModel = defaultEmbeddingModel;
 
+    @Builder.Default
+    @Column(name = "llm_top_k")
+    private int topK = 3;
+
+    @Builder.Default
+    @Column(name = "llm_score_threshold")
+    private Double scoreThreshold = 0.5;
+
     // 温度越低，回答结果越固定，随机性越低
     @Builder.Default
     @Column(name = "llm_temperature")
@@ -97,6 +97,7 @@ public class RobotLlm {
     @Builder.Default
     @Column(name = "llm_prompt", columnDefinition = TypeConsts.COLUMN_TYPE_TEXT)
     private String prompt = RobotConsts.ROBOT_LLM_DEFAULT_PROMPT;
+    
     // private String prompt = "";
     // private String promptTemplate =
     // "请根据上下文信息回答问题：\n\n上下文信息：\n{context}\n\n问题：{question}\n\n答案：";
