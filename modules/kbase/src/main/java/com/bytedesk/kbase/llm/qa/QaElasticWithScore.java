78e8e39be7be0c701fb716590bffa1c4998d7fa3
jack ning <github@bytedesk.com>
2025-04-24 09:08:07 +0800

    update modules/kbase: mod        2 files

--- modules/kbase/src/main/java/com/bytedesk/kbase/llm/qa/QaElasticWithScore.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/llm/qa/QaElasticWithScore.java
@@ -1,5 +1,23 @@
 package com.bytedesk.kbase.llm.qa;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class QaElasticWithScore {
+    // QA内容
+    private QaElastic qaElastic;
+    // 搜索得分
+    private float score;
     
+    // 构造函数
+    public QaElasticWithScore(QaElastic qaElastic, float score) {
+        this.qaElastic = qaElastic;
+        this.score = score;
+    }
 }
