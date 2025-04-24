757dba793a86b97900771a254ac4f3b22d83900e
jack ning <github@bytedesk.com>
2025-04-24 16:16:02 +0800

    update

--- modules/kbase/src/main/java/com/bytedesk/kbase/faq/FaqExcelListener.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/faq/FaqExcelListener.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-07-30 21:02:37
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-24 08:46:09
+ * @LastEditTime: 2025-04-24 16:13:04
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -30,7 +30,6 @@ public class FaqExcelListener implements ReadListener<FaqExcel> {
 
     private final FaqRestService faqService;
 
-    // private final String categoryUid;
     private final String uploadType;
 
     private final String fileUid;
