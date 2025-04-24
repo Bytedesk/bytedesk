208a4d350beb83c281b5c2eb78e80dbe7330edfd
jack ning <github@bytedesk.com>
2025-04-24 17:07:59 +0800

    update

--- modules/kbase/src/main/java/com/bytedesk/kbase/faq/FaqRestService.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/faq/FaqRestService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-03-22 22:59:18
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-24 15:45:30
+ * @LastEditTime: 2025-04-24 17:03:19
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -134,7 +134,7 @@ public class FaqRestService extends BaseRestServiceWithExcel<FaqEntity, FaqReque
     }
 
     public Boolean existsByQuestionAndAnswerAndKbUidAndOrgUid(String question, String answer, String kbUid, String orgUid) {
-        return faqRepository.existsByQuestionAndAnswerAndKbase_UidAndOrgUid(question, answer, kbUid, orgUid);
+        return faqRepository.existsByQuestionAndAnswerAndKbase_UidAndOrgUidAndDeletedFalse(question, answer, kbUid, orgUid);
     }
 
     public Boolean existsByUid(String uid) {
