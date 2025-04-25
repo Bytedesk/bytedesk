f7421327c9ed7362890420805e740cc21f9c3ac2
jack ning <github@bytedesk.com>
2025-04-25 13:34:59 +0800

    update modules/ai: mod        1 files

--- modules/ai/src/main/java/com/bytedesk/ai/robot/RobotRequest.java
+++ modules/ai/src/main/java/com/bytedesk/ai/robot/RobotRequest.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-03-22 16:45:07
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-25 12:49:47
+ * @LastEditTime: 2025-04-25 12:55:50
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -73,6 +73,7 @@ public class RobotRequest extends BaseRequest {
 
     private String kbUid;
 
+    // 优先判断是否启用流程，流程优先于知识库
     // flow enabled
     private Boolean flowEnabled;
 
