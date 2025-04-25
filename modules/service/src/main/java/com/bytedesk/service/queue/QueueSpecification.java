4fb2f797c181c33061a672bf9fc64e07dd7c01df
jack ning <github@bytedesk.com>
2025-04-25 21:57:57 +0800

    update modules/service: mod        1 files

--- modules/service/src/main/java/com/bytedesk/service/queue/QueueSpecification.java
+++ modules/service/src/main/java/com/bytedesk/service/queue/QueueSpecification.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-06-17 14:47:08
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-08 23:10:50
+ * @LastEditTime: 2025-04-25 21:32:40
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -31,7 +31,7 @@ import lombok.extern.slf4j.Slf4j;
 public class QueueSpecification extends BaseSpecification {
 
     public static Specification <QueueEntity> search (QueueRequest request) {
-        log.info("request: {}", request);
+        // log.info("request: {}", request);
         return (root, query, criteriaBuilder) -> {
             List<Predicate> predicates = new ArrayList<>();
             predicates.addAll(getBasicPredicates(root, criteriaBuilder, request.getOrgUid()));
