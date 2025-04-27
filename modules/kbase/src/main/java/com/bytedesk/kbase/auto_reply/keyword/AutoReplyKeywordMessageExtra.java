588d10fb4f6c043c5f71ab050bba53fa9ed39754
jack ning <github@bytedesk.com>
2025-04-27 13:21:14 +0800

    update

---
+++ modules/kbase/src/main/java/com/bytedesk/kbase/auto_reply/keyword/AutoReplyKeywordMessageExtra.java
@@ -0,0 +1,35 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2025-04-27 13:19:03
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-04-27 13:20:21
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ * 
+ * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
+ */
+package com.bytedesk.kbase.auto_reply.keyword;
+
+import com.bytedesk.core.message.MessageExtra;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AutoReplyKeywordMessageExtra extends MessageExtra {
+
+    private static final long serialVersionUID = 1L;
+
+    private String content;
+
+}
