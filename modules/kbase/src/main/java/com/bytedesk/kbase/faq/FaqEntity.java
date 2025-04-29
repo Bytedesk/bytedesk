5ceba99653fd5566f23bf6abc62116d2cd8e2c87
jack ning <github@bytedesk.com>
2025-04-29 14:02:04 +0800

    update modules/kbase: add        1 mod        3 files

--- modules/kbase/src/main/java/com/bytedesk/kbase/faq/FaqEntity.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/faq/FaqEntity.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-02-22 16:16:42
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-29 09:55:30
+ * @LastEditTime: 2025-04-29 13:47:01
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -106,7 +106,6 @@ public class FaqEntity extends BaseEntity {
     @ManyToMany(fetch = FetchType.LAZY)
     private List<FaqEntity> relatedFaqs = new ArrayList<>();
 
-    
     @Builder.Default
     private String status = FaqStatusEnum.NEW.name();
     /**
