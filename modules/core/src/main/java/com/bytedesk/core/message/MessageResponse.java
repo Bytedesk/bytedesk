d3d6c47b919d50e9336e68ee489b3bc81fc1d28d
jack ning <github@bytedesk.com>
2025-04-29 13:01:52 +0800

    Update MessageResponse.java

--- modules/core/src/main/java/com/bytedesk/core/message/MessageResponse.java
+++ modules/core/src/main/java/com/bytedesk/core/message/MessageResponse.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-02-21 10:00:55
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-29 12:46:07
+ * @LastEditTime: 2025-04-29 12:52:20
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -16,6 +16,7 @@ package com.bytedesk.core.message;
 import com.bytedesk.core.base.BaseResponse;
 import com.bytedesk.core.rbac.user.UserProtobuf;
 import com.bytedesk.core.thread.ThreadResponse;
+// import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -57,9 +58,6 @@ public class MessageResponse extends BaseResponse {
     // extra格式不固定，前端需要根据type字段来解析，所以此处不能使用MessageExtra
     private String extra;
 
-	// 有帮助、没帮助
-    // private String helpful;
-
     // 通过解析user字段中的type字段来判断 type=robot则为机器人，否则为访客
     public boolean isRobot() {
         // 忽略大小写
@@ -79,7 +77,8 @@ public class MessageResponse extends BaseResponse {
     }
 
     // 将createdAt转换为时间戳
-    public Long timestamp() {
+    // @JsonProperty("timestamp")
+    public Long getTimestamp() {
         // 使用中国时区，与BdDateUtils保持一致
         return this.createdAt.atZone(java.time.ZoneId.of("Asia/Shanghai")).toInstant().toEpochMilli();
     }
