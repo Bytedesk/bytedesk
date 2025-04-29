faf17788a142e8ea64492b6ad9191b62b29521ce
jack ning <github@bytedesk.com>
2025-04-29 18:03:09 +0800

    update modules/kbase: mod        5 files

--- modules/kbase/src/main/java/com/bytedesk/kbase/faq_rating/FaqRatingResponse.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/faq_rating/FaqRatingResponse.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-05-11 18:26:12
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-13 21:09:50
+ * @LastEditTime: 2025-04-29 17:59:09
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -14,21 +14,46 @@
 package com.bytedesk.kbase.faq_rating;
 
 
-import com.bytedesk.core.base.BaseResponse;
+import java.util.List;
 
+import com.bytedesk.core.base.BaseResponse;
+import com.bytedesk.kbase.faq.FaqResponseSimple;
+
+
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
+import lombok.experimental.SuperBuilder;
 
 @Data
-@Builder
+@SuperBuilder
 @Accessors(chain = true)
 @EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
-// @NoArgsConstructor
+@NoArgsConstructor
 public class FaqRatingResponse extends BaseResponse {
 
-    // private LocalDateTime createdAt;
+     private String messageUid;
+
+    private String threadUid;
+
+    // 区分是 rateUp 还是 rateDown
+    private String rateType;
+    
+    // 点踩的情况下的反馈意见
+    private List<String> rateDownTagList;
+
+    // 点踩的原因
+    private String rateDownReason;
+
+    // 关联FaqEntity
+    @ManyToOne(fetch = FetchType.LAZY)
+    private FaqResponseSimple faq;
+
+    // 评价者信息的JSON表示
+    private String user;
 }
--- modules/kbase/src/main/java/com/bytedesk/kbase/faq_rating/FaqRatingRestService.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/faq_rating/FaqRatingRestService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-05-11 18:25:45
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-14 09:29:27
+ * @LastEditTime: 2025-04-29 18:00:52
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -160,7 +160,9 @@ public class FaqRatingRestService extends BaseRestService<FaqRatingEntity, FaqRa
 
     @Override
     public FaqRatingResponse convertToResponse(FaqRatingEntity entity) {
-        return modelMapper.map(entity, FaqRatingResponse.class);
+        FaqRatingResponse response = modelMapper.map(entity, FaqRatingResponse.class);
+        
+        return response;
     }
 
     @Override
