01563cbac2924995ba84722ba50a68c335636252
jack ning <github@bytedesk.com>
2025-04-29 23:06:48 +0800

    update

--- modules/kbase/src/main/java/com/bytedesk/kbase/faq/FaqRestService.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/faq/FaqRestService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-03-22 22:59:18
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-29 18:42:28
+ * @LastEditTime: 2025-04-29 22:53:18
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -416,6 +416,7 @@ public class FaqRestService extends BaseRestServiceWithExcel<FaqEntity, FaqReque
                         .rateDownTagList(request.getRateDownTagList())
                         .rateDownReason(request.getRateDownReason())
                         .faqUid(faqUid)
+                        .user(request.getUser())
                         .orgUid(savedMessage.getOrgUid())
                         .build();
                 faqRatingRestService.create(faqRatingRequest);
