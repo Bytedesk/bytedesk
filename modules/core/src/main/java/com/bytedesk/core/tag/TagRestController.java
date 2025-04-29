6285bbaab60deb368d88e552e97e41b749527520
jack ning <github@bytedesk.com>
2025-04-29 16:59:42 +0800

    update modules/kbase: mod       24 files

--- modules/core/src/main/java/com/bytedesk/core/tag/TagRestController.java
+++ modules/core/src/main/java/com/bytedesk/core/tag/TagRestController.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-05-11 18:25:36
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-05 16:37:26
+ * @LastEditTime: 2025-04-29 16:36:18
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -16,11 +16,12 @@ package com.bytedesk.core.tag;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bytedesk.core.annotation.ActionAnnotation;
 import com.bytedesk.core.base.BaseRestController;
-import com.bytedesk.core.rbac.role.RolePermissions;
 import com.bytedesk.core.utils.JsonResult;
 
 import jakarta.servlet.http.HttpServletResponse;
@@ -33,7 +34,8 @@ public class TagRestController extends BaseRestController<TagRequest> {
 
     private final TagRestService tagService;
 
-    @PreAuthorize(RolePermissions.ROLE_ADMIN)
+    // @PreAuthorize(RolePermissions.ROLE_ADMIN)
+    @ActionAnnotation(title = "标签", action = "查询组织", description = "query tag by org")
     @Override
     public ResponseEntity<?> queryByOrg(TagRequest request) {
         
@@ -42,6 +44,7 @@ public class TagRestController extends BaseRestController<TagRequest> {
         return ResponseEntity.ok(JsonResult.success(tags));
     }
 
+    @ActionAnnotation(title = "标签", action = "查询用户", description = "query tag by user")
     @Override
     public ResponseEntity<?> queryByUser(TagRequest request) {
         
@@ -50,7 +53,18 @@ public class TagRestController extends BaseRestController<TagRequest> {
         return ResponseEntity.ok(JsonResult.success(tags));
     }
 
+    @ActionAnnotation(title = "标签", action = "查询详情", description = "query tag by uid")
     @Override
+    public ResponseEntity<?> queryByUid(TagRequest request) {
+        
+        TagResponse tag = tagService.queryByUid(request);
+
+        return ResponseEntity.ok(JsonResult.success(tag));
+    }
+
+    @ActionAnnotation(title = "标签", action = "新建", description = "create tag")
+    @Override
+    @PreAuthorize("hasAuthority('TAG_CREATE')")
     public ResponseEntity<?> create(TagRequest request) {
         
         TagResponse tag = tagService.create(request);
@@ -58,7 +72,9 @@ public class TagRestController extends BaseRestController<TagRequest> {
         return ResponseEntity.ok(JsonResult.success(tag));
     }
 
+    @ActionAnnotation(title = "标签", action = "更新", description = "update tag")
     @Override
+    @PreAuthorize("hasAuthority('TAG_UPDATE')")
     public ResponseEntity<?> update(TagRequest request) {
         
         TagResponse tag = tagService.update(request);
@@ -66,7 +82,9 @@ public class TagRestController extends BaseRestController<TagRequest> {
         return ResponseEntity.ok(JsonResult.success(tag));
     }
 
+    @ActionAnnotation(title = "标签", action = "删除", description = "delete tag")
     @Override
+    @PreAuthorize("hasAuthority('TAG_DELETE')")
     public ResponseEntity<?> delete(TagRequest request) {
         
         tagService.delete(request);
@@ -74,16 +92,21 @@ public class TagRestController extends BaseRestController<TagRequest> {
         return ResponseEntity.ok(JsonResult.success());
     }
 
+    @ActionAnnotation(title = "标签", action = "导出", description = "export tag")
     @Override
+    @PreAuthorize("hasAuthority('TAG_EXPORT')")
+    @GetMapping("/export")
     public Object export(TagRequest request, HttpServletResponse response) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'export'");
+        return exportTemplate(
+            request,
+            response,
+            tagService,
+            TagExcel.class,
+            "标签",
+            "tag"
+        );
     }
 
-    @Override
-    public ResponseEntity<?> queryByUid(TagRequest request) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'queryByUid'");
-    }
+    
     
 }
