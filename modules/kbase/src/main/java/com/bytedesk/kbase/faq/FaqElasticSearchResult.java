025efee5dcd1dae34a6d0412fc8a471f10bbc362
jack ning <github@bytedesk.com>
2025-04-29 09:32:10 +0800

    update modules/kbase: mod        1 files

--- modules/kbase/src/main/java/com/bytedesk/kbase/faq/FaqElasticSearchResult.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/faq/FaqElasticSearchResult.java
@@ -1,3 +1,16 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2025-04-28 21:31:59
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-04-29 09:21:23
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ * 
+ * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
+ */
 package com.bytedesk.kbase.faq;
 
 import java.io.Serializable;
@@ -15,7 +28,6 @@ public class FaqElasticSearchResult implements Serializable {
 
     private static final long serialVersionUID = 1L;
     
-    // QA内容
     private FaqElastic faqElastic;
    
     private float score;
