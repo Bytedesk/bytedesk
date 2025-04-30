358eb557b273d09713313781d91e34585e22d367
jack ning <github@bytedesk.com>
2025-04-30 21:11:11 +0800

    update modules/service: mod        2 files

--- modules/service/src/main/java/com/bytedesk/service/queue/QueueEntity.java
+++ modules/service/src/main/java/com/bytedesk/service/queue/QueueEntity.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-02-22 16:12:53
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-28 13:46:34
+ * @LastEditTime: 2025-04-30 21:06:09
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -16,7 +16,6 @@ package com.bytedesk.service.queue;
 import com.bytedesk.core.base.BaseEntity;
 import com.bytedesk.core.thread.ThreadTypeEnum;
 import com.bytedesk.service.queue_member.QueueMemberEntity;
-// import com.bytedesk.service.queue_member.QueueMemberSourceEnum;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
--- modules/service/src/main/java/com/bytedesk/service/queue/QueueExcel.java
+++ modules/service/src/main/java/com/bytedesk/service/queue/QueueExcel.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-08-01 06:18:10
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-08 23:20:01
+ * @LastEditTime: 2025-04-30 21:09:57
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -23,44 +23,56 @@ import lombok.Data;
 @Data
 public class QueueExcel {
 
-    @ExcelProperty(index = 0, value = "队列名称")
+    @ExcelProperty(value = "队列名称")
     @ColumnWidth(20)
     private String nickname;
 
-    @ExcelProperty(index = 1, value = "队列类型")
+    @ExcelProperty(value = "队列类型")
     @ColumnWidth(15)
     private String type;
 
-    @ExcelProperty(index = 2, value = "队列主题")
+    @ExcelProperty(value = "队列主题")
     @ColumnWidth(20)
     private String topic;
 
-    @ExcelProperty(index = 3, value = "队列日期")
+    @ExcelProperty(value = "队列日期")
     @ColumnWidth(15)
     private String day;
 
-    @ExcelProperty(index = 4, value = "队列状态")
+    @ExcelProperty(value = "队列状态")
     @ColumnWidth(15)
     private String status;
 
-    @ExcelProperty(index = 5, value = "请求服务人次")
-    @ColumnWidth(10)
-    private Integer newCount;
+    @ExcelProperty(value = "今日请求服务人数")
+    @ColumnWidth(15)
+    private Integer totalCount;
 
-    @ExcelProperty(index = 6, value = "离线留言人次")
-    @ColumnWidth(10)
+    @ExcelProperty(value = "机器人服务中人次")
+    @ColumnWidth(15)
+    private Integer robotingCount;
+
+    @ExcelProperty(value = "客服离线人次")
+    @ColumnWidth(15)
     private Integer offlineCount;
 
-    @ExcelProperty(index = 7, value = "当前等待中人次")
-    @ColumnWidth(10)
+    @ExcelProperty(value = "留言数")
+    @ColumnWidth(15)
+    private Integer leaveMsgCount;
+
+    @ExcelProperty(value = "转人工数")
+    @ColumnWidth(15)
+    private Integer robotToAgentCount;
+
+    @ExcelProperty(value = "排队中人数")
+    @ColumnWidth(15)
     private Integer queuingCount;
 
-    @ExcelProperty(index = 8, value = "服务中人次")
-    @ColumnWidth(10)
+    @ExcelProperty(value = "正在服务人数")
+    @ColumnWidth(15)
     private Integer chattingCount;
 
-    @ExcelProperty(index = 9, value = "服务结束人次")
-    @ColumnWidth(10)
+    @ExcelProperty(value = "对话结束人数")
+    @ColumnWidth(15)
     private Integer closedCount;
 
 }
