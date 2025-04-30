78144d4220846162ccb6fba41e6256558d0793b6
jack ning <github@bytedesk.com>
2025-04-30 12:56:34 +0800

    update modules/service: mod        6 files

--- modules/ai/src/main/java/com/bytedesk/ai/robot/RobotRestControllerVisitor.java
+++ modules/ai/src/main/java/com/bytedesk/ai/robot/RobotRestControllerVisitor.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-04-30 10:44:16
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-30 10:45:44
+ * @LastEditTime: 2025-04-30 12:55:57
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -13,15 +13,8 @@
  */
 package com.bytedesk.ai.robot;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.bytedesk.core.message.MessageResponse;
-import com.bytedesk.core.utils.JsonResult;
-import com.bytedesk.kbase.faq.FaqRequest;
 
 import lombok.AllArgsConstructor;
 
@@ -30,42 +23,42 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class RobotRestControllerVisitor {
 
-    private final RobotRestService robotRestService;
+    // private final RobotRestService robotRestService;
 
-    // rate message helpful
-    @PostMapping("/rate/message/helpful")
-    public ResponseEntity<?> rateMessageHelpful(@RequestBody FaqRequest request) {
+    // // rate message helpful
+    // @PostMapping("/rate/message/helpful")
+    // public ResponseEntity<?> rateMessageHelpful(@RequestBody FaqRequest request) {
 
-        MessageResponse message = robotRestService.rateUp(request);
+    //     MessageResponse message = robotRestService.rateUp(request);
 
-        return ResponseEntity.ok(JsonResult.success(message));
-    }
+    //     return ResponseEntity.ok(JsonResult.success(message));
+    // }
 
-    // rate message not helpful
-    @PostMapping("/rate/message/unhelpful")
-    public ResponseEntity<?> rateMessageNotHelpful(@RequestBody FaqRequest request) {
+    // // rate message not helpful
+    // @PostMapping("/rate/message/unhelpful")
+    // public ResponseEntity<?> rateMessageNotHelpful(@RequestBody FaqRequest request) {
         
-        MessageResponse message = robotRestService.rateDown(request);
+    //     MessageResponse message = robotRestService.rateDown(request);
 
-        return ResponseEntity.ok(JsonResult.success(message));
-    }
+    //     return ResponseEntity.ok(JsonResult.success(message));
+    // }
 
-    // rate message feedback
-    @PostMapping("/rate/message/feedback")
-    public ResponseEntity<?> rateMessageFeedback(@RequestBody FaqRequest request) {
+    // // rate message feedback
+    // @PostMapping("/rate/message/feedback")
+    // public ResponseEntity<?> rateMessageFeedback(@RequestBody FaqRequest request) {
 
-        MessageResponse message = robotRestService.rateFeedback(request);
+    //     MessageResponse message = robotRestService.rateFeedback(request);
 
-        return ResponseEntity.ok(JsonResult.success(message));
-    }
+    //     return ResponseEntity.ok(JsonResult.success(message));
+    // }
 
-    // rate message transfer
-    @PostMapping("/rate/message/transfer")
-    public ResponseEntity<?> rateMessageTransfer(@RequestBody FaqRequest request) {
+    // // rate message transfer
+    // @PostMapping("/rate/message/transfer")
+    // public ResponseEntity<?> rateMessageTransfer(@RequestBody FaqRequest request) {
 
-        MessageResponse message = robotRestService.rateTransfer(request);
+    //     MessageResponse message = robotRestService.rateTransfer(request);
 
-        return ResponseEntity.ok(JsonResult.success(message));
-    }
+    //     return ResponseEntity.ok(JsonResult.success(message));
+    // }
     
 }
