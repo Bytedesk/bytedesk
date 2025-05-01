f2202443b44c35f265cd700b97fcd8c7468aff89
jack ning <github@bytedesk.com>
2025-05-01 14:18:35 +0800

    update modules/kbase: mod        1 files

--- modules/kbase/src/main/java/com/bytedesk/kbase/faq/FaqExcel.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/faq/FaqExcel.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-05-22 22:12:03
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-08 13:14:54
+ * @LastEditTime: 2025-05-01 14:15:27
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -13,7 +13,10 @@
  */
 package com.bytedesk.kbase.faq;
 
+import java.time.LocalDateTime;
+
 import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.format.DateTimeFormat;
 import com.alibaba.excel.annotation.write.style.ColumnWidth;
 
 import lombok.Data;
@@ -21,59 +24,61 @@ import lombok.Data;
 @Data
 public class FaqExcel {
 
-    @ExcelProperty(index = 0, value = "分类")
+    @ExcelProperty(value = "分类")
     @ColumnWidth(20)
     private String category;
 
-    @ExcelProperty(index = 1, value = "类型")
+    @ExcelProperty(value = "类型")
     @ColumnWidth(20)
     private String type;
 
-    @ExcelProperty(index = 2, value = "问题")
+    @ExcelProperty(value = "问题")
     @ColumnWidth(20)
     private String question;
 
-    @ExcelProperty(index = 3, value = "答案")
+    @ExcelProperty(value = "答案")
     @ColumnWidth(20)
     private String answer;
 
     // tagList
-    @ExcelProperty(index = 4, value = "标签")
+    @ExcelProperty(value = "标签")
     @ColumnWidth(20)
     private String tagList;
 
     // clickCount
-    @ExcelProperty(index = 5, value = "点击次数")
+    @ExcelProperty(value = "点击次数")
     @ColumnWidth(20)
     private int clickCount;
 
     // upCount
-    @ExcelProperty(index = 6, value = "点赞次数")
+    @ExcelProperty(value = "点赞次数")
     @ColumnWidth(20)
     private int upCount;
 
     // downCount
-    @ExcelProperty(index = 7, value = "点踩次数")
+    @ExcelProperty(value = "点踩次数")
     @ColumnWidth(20)
     private int downCount;
 
     // enabled
-    @ExcelProperty(index = 8, value = "是否启用")   
+    @ExcelProperty(value = "是否启用")   
     @ColumnWidth(20)
     private String enabled;
 
     // startDate
-    @ExcelProperty(index = 9, value = "有效开始日期")
-    @ColumnWidth(20)
-    private String startDate;
+    @DateTimeFormat("yyyy-MM-dd HH:mm:ss")
+    @ExcelProperty(value = "有效开始日期")
+    @ColumnWidth(25)
+    private LocalDateTime startDate;
 
     // endDate
-    @ExcelProperty(index = 10, value = "有效结束日期")
-    @ColumnWidth(20)
-    private String endDate;
+    @DateTimeFormat("yyyy-MM-dd HH:mm:ss")
+    @ExcelProperty(value = "有效结束日期")
+    @ColumnWidth(25)
+    private LocalDateTime endDate;
 
     // answerList
-    @ExcelProperty(index = 11, value = "扩展问答")
+    @ExcelProperty(value = "扩展问答")
     @ColumnWidth(20)
     private String answerList;
 
