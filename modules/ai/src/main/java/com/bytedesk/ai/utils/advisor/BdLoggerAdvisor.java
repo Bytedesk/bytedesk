505a294991eee7092eca06d2560bc336f33119a2
jack ning <github@bytedesk.com>
2025-05-04 08:58:32 +0800

    update

--- modules/ai/src/main/java/com/bytedesk/ai/utils/advisor/BdLoggerAdvisor.java
+++ modules/ai/src/main/java/com/bytedesk/ai/utils/advisor/BdLoggerAdvisor.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-02-13 10:25:32
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-22 11:56:08
+ * @LastEditTime: 2025-05-04 08:57:10
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -13,14 +13,12 @@
  */
 package com.bytedesk.ai.utils.advisor;
 
-import org.springframework.ai.chat.client.advisor.api.AdvisedRequest;
-import org.springframework.ai.chat.client.advisor.api.AdvisedResponse;
-import org.springframework.ai.chat.client.advisor.api.CallAroundAdvisor;
+import org.springframework.ai.chat.client.advisor.api.CallAdvisor;
 import org.springframework.ai.chat.client.advisor.api.CallAroundAdvisorChain;
-import org.springframework.ai.chat.client.advisor.api.StreamAroundAdvisor;
+import org.springframework.ai.chat.client.advisor.api.StreamAdvisor;
 import org.springframework.ai.chat.client.advisor.api.StreamAroundAdvisorChain;
-import org.springframework.ai.chat.model.MessageAggregator;
-
+import org.springframework.ai.chat.client.ChatClientRequest;
+import org.springframework.ai.chat.client.ChatClientResponse;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 
@@ -29,7 +27,7 @@ import reactor.core.publisher.Flux;
  * https://docs.spring.io/spring-ai/reference/api/advisors.html#_logging_advisor
  */
 @Slf4j
-public class BdLoggerAdvisor implements CallAroundAdvisor, StreamAroundAdvisor {
+public class BdLoggerAdvisor implements CallAdvisor, StreamAdvisor {
 
 	@Override
 	public String getName() { 
@@ -42,25 +40,18 @@ public class BdLoggerAdvisor implements CallAroundAdvisor, StreamAroundAdvisor {
 	}
 
 	@Override
-	public AdvisedResponse aroundCall(AdvisedRequest advisedRequest, CallAroundAdvisorChain chain) {
-
-		log.debug("BEFORE: {}", advisedRequest);
-
-		AdvisedResponse advisedResponse = chain.nextAroundCall(advisedRequest);
-
-		log.debug("AFTER: {}", advisedResponse);
-
-		return advisedResponse;
+	public Flux<ChatClientResponse> adviseStream(ChatClientRequest chatClientRequest, StreamAroundAdvisorChain chain) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'adviseStream'");
 	}
 
 	@Override
-	public Flux<AdvisedResponse> aroundStream(AdvisedRequest advisedRequest, StreamAroundAdvisorChain chain) {
-
-		log.debug("BEFORE: {}", advisedRequest);
-
-		Flux<AdvisedResponse> advisedResponses = chain.nextAroundStream(advisedRequest);
-
-        return new MessageAggregator().aggregateAdvisedResponse(advisedResponses,
-                    advisedResponse -> log.debug("AFTER: {}", advisedResponse)); 
+	public ChatClientResponse adviseCall(ChatClientRequest chatClientRequest, CallAroundAdvisorChain chain) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'adviseCall'");
 	}
+
+	
+
+	
 }
