d4e96678afe7ce4e75d02d36d48636d5f211ac9d
jack ning <github@bytedesk.com>
2025-05-04 16:18:38 +0800

    update modules/ai: mod        2 files

--- modules/ai/src/main/java/com/bytedesk/ai/utils/advisor/BdLoggerAdvisor.java
+++ modules/ai/src/main/java/com/bytedesk/ai/utils/advisor/BdLoggerAdvisor.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-02-13 10:25:32
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-05-04 08:57:10
+ * @LastEditTime: 2025-05-04 16:18:35
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -13,45 +13,37 @@
  */
 package com.bytedesk.ai.utils.advisor;
 
-import org.springframework.ai.chat.client.advisor.api.CallAdvisor;
-import org.springframework.ai.chat.client.advisor.api.CallAroundAdvisorChain;
-import org.springframework.ai.chat.client.advisor.api.StreamAdvisor;
-import org.springframework.ai.chat.client.advisor.api.StreamAroundAdvisorChain;
-import org.springframework.ai.chat.client.ChatClientRequest;
-import org.springframework.ai.chat.client.ChatClientResponse;
-import lombok.extern.slf4j.Slf4j;
-import reactor.core.publisher.Flux;
 
 /**
  * Advisor for logging the request and response of the chat client.
  * https://docs.spring.io/spring-ai/reference/api/advisors.html#_logging_advisor
  */
-@Slf4j
-public class BdLoggerAdvisor implements CallAdvisor, StreamAdvisor {
+// @Slf4j
+// public class BdLoggerAdvisor implements CallAdvisor, StreamAdvisor {
 
-	@Override
-	public String getName() { 
-		return this.getClass().getSimpleName();
-	}
+// 	@Override
+// 	public String getName() { 
+// 		return this.getClass().getSimpleName();
+// 	}
 
-	@Override
-	public int getOrder() { 
-		return 0;
-	}
+// 	@Override
+// 	public int getOrder() { 
+// 		return 0;
+// 	}
 
-	@Override
-	public Flux<ChatClientResponse> adviseStream(ChatClientRequest chatClientRequest, StreamAroundAdvisorChain chain) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'adviseStream'");
-	}
+// 	@Override
+// 	public Flux<ChatClientResponse> adviseStream(ChatClientRequest chatClientRequest, StreamAroundAdvisorChain chain) {
+// 		// TODO Auto-generated method stub
+// 		throw new UnsupportedOperationException("Unimplemented method 'adviseStream'");
+// 	}
 
-	@Override
-	public ChatClientResponse adviseCall(ChatClientRequest chatClientRequest, CallAroundAdvisorChain chain) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'adviseCall'");
-	}
-
-	
+// 	@Override
+// 	public ChatClientResponse adviseCall(ChatClientRequest chatClientRequest, CallAroundAdvisorChain chain) {
+// 		// TODO Auto-generated method stub
+// 		throw new UnsupportedOperationException("Unimplemented method 'adviseCall'");
+// 	}
 
 	
-}
+
+	
+// }
