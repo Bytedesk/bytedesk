a810d6667a9d253b329ba9c35582c08f9a8c996f
jack ning <github@bytedesk.com>
2025-05-05 21:40:38 +0800

    up

--- starter/src/main/java/com/bytedesk/starter/controller/PageRouteController.java
+++ starter/src/main/java/com/bytedesk/starter/controller/PageRouteController.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:17:36
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-04-30 15:34:28
+ * @LastEditTime: 2025-05-05 21:31:58
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -39,10 +39,10 @@ public class PageRouteController {
 	 */
 	@GetMapping({ "/", "/home" })
 	public String home(Model model) {
-		// model.addAttribute("title", "微语首页");
-		// model.addAttribute("chatUrl", "/chat/home");
-		// return "home";
-		return "quanjing";
+		model.addAttribute("title", "微语首页");
+		model.addAttribute("chatUrl", "/chat/home");
+		return "home";
+		// return "quanjing";
 	}
 
 	/**
---
+++ starter/src/main/resources/static/notebase/assets/clone-j1fbc5Lk.js
@@ -0,0 +1,1 @@
+import{c as r}from"./graph-BSAuwU4r.js";var e=4;function a(o){return r(o,e)}export{a as c};
