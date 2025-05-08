842c4764c4dfc9595b2bc79b9a1145060e95a7a0
jack ning <github@bytedesk.com>
2025-05-08 16:40:10 +0800

    update modules/service: mod        1 files

--- modules/service/src/main/java/com/bytedesk/service/channel_app/ChannelAppPlatformEnum.java
+++ modules/service/src/main/java/com/bytedesk/service/channel_app/ChannelAppPlatformEnum.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-07-23 17:02:46
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-05-08 15:58:28
+ * @LastEditTime: 2025-05-08 16:12:52
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -14,7 +14,6 @@
 package com.bytedesk.service.channel_app;
 
 public enum ChannelAppPlatformEnum {
-    // WEB,
     ANDROID,
     IOS,
     FLUTTER,
