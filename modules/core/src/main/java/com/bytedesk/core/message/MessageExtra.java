6ba99f82607dff410b849a9b5bee26091b7ddc6b
jack ning <github@bytedesk.com>
2025-05-09 09:13:35 +0800

    update modules/core: mod        3 del        1 files

--- modules/core/src/main/java/com/bytedesk/core/message/MessageExtra.java
+++ modules/core/src/main/java/com/bytedesk/core/message/MessageExtra.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-05-08 08:58:28
+ * @LastEditTime: 2025-05-09 09:05:18
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -25,8 +25,6 @@ import lombok.experimental.SuperBuilder;
 
 /**
  * 消息扩展字段，用于存储额外的信息
- * TODO：待重构，仅需要保留orgUid字段即可，作为基类使用
- * 
  */
 @Getter
 @Setter
