d8020b75bea66e8b7e4db3cfd4944fcdb709220b
jack ning <github@bytedesk.com>
2025-05-09 14:01:32 +0800

    update

---
+++ starter/src/main/resources/static/notebase/assets/channel-yOhp9TGv.js
@@ -0,0 +1,1 @@
+import{al as o,am as n}from"./mermaid.core-FIymlv9x.js";const l=(a,r)=>o.lang.round(n.parse(a)[r]);export{l as c};
--- starter/src/main/resources/static/notebase/assets/clone-EOKkErtd.js
+++
@@ -1,1 +0,0 @@
-import{c as r}from"./graph-1GVnZ2QQ.js";var e=4;function a(o){return r(o,e)}export{a as c};
