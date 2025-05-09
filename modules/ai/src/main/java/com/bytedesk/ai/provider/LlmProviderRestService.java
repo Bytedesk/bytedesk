be43bcd413180e8ff2606916290bdf6f9594ae2c
jack ning <github@bytedesk.com>
2025-05-09 12:49:32 +0800

    update

--- modules/ai/src/main/java/com/bytedesk/ai/provider/LlmProviderRestService.java
+++ modules/ai/src/main/java/com/bytedesk/ai/provider/LlmProviderRestService.java
@@ -48,7 +48,7 @@ public class LlmProviderRestService extends BaseRestService<LlmProviderEntity, L
 
     @Override
     public Page<LlmProviderResponse> queryByOrg(LlmProviderRequest request) {
-        Pageable pageable = request.getPageableAsc();
+        Pageable pageable = request.getPageable();
         Specification<LlmProviderEntity> specification = LlmProviderSpecification.search(request);
         Page<LlmProviderEntity> page = repository.findAll(specification, pageable);
         return page.map(this::convertToResponse);
