32d0fd9dd3f8fc71aa92bca3f47c5537b1cab96e
jack ning <github@bytedesk.com>
2025-05-13 14:39:40 +0800

    update modules/kbase: mod        2 files

--- modules/ai/src/main/java/com/bytedesk/ai/springai/service/SpringAIVectorStoreService.java
+++ modules/ai/src/main/java/com/bytedesk/ai/springai/service/SpringAIVectorStoreService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-07-27 21:27:01
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-05-12 18:16:13
+ * @LastEditTime: 2025-05-13 14:35:16
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
