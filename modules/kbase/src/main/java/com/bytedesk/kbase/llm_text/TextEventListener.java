4ff04d482a318e9c86aff7c6af38495b2fa433ae
jack ning <github@bytedesk.com>
2025-05-13 17:54:45 +0800

    update modules/kbase: mod        1 files

--- modules/kbase/src/main/java/com/bytedesk/kbase/llm_text/TextEventListener.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/llm_text/TextEventListener.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-02-25 09:44:18
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-05-12 18:05:18
+ * @LastEditTime: 2025-05-13 17:54:37
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -71,5 +71,8 @@ public class TextEventListener {
         }
     }
  
+
+
+    
 }
 
