5a07acb15f9fc16205fb98cc62b96d44a1925442
jack ning <github@bytedesk.com>
2025-05-17 15:57:45 +0800

    update

---
+++ modules/kbase/src/main/java/com/bytedesk/kbase/faq/batch/FaqBatchJobConfig.java
@@ -0,0 +1,88 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2025-05-17 10:48:00
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-05-17 10:48:00
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ *  联系：270580156@qq.com
+ * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
+ */
+package com.bytedesk.kbase.faq.batch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import com.bytedesk.kbase.faq.FaqEntity;
+import com.bytedesk.kbase.faq.FaqExcel;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * FAQ批处理作业配置
+ */
+@Slf4j
+@Configuration
+public class FaqBatchJobConfig {
+
+    @Autowired
+    private JobRepository jobRepository;
+    
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+    
+    @Autowired
+    private FaqItemProcessor faqItemProcessor;
+    
+    @Autowired
+    private FaqItemWriter faqItemWriter;
+    
+    /**
+     * 配置FAQ导入作业
+     */
+    @Bean
+    public Job importFaqJob() {
+        return new JobBuilder("importFaqJob", jobRepository)
+                .start(importFaqStep())
+                .build();
+    }
+    
+    /**
+     * 配置FAQ导入步骤
+     */
+    @Bean
+    public Step importFaqStep() {
+        return new StepBuilder("importFaqStep", jobRepository)
+                .<FaqExcel, FaqEntity>chunk(100, transactionManager)
+                .reader(faqExcelReader(null))  // 读取器实例将在运行时设置
+                .processor(faqItemProcessor)
+                .writer(faqItemWriter)
+                .build();
+    }
+    
+    /**
+     * 配置FAQ Excel读取器
+     * 
+     * @param resource Excel文件资源
+     * @return 配置好的Excel读取器
+     */
+    @Bean
+    public FaqExcelReader faqExcelReader(Resource resource) {
+        FaqExcelReader reader = new FaqExcelReader();
+        if (resource != null) {
+            reader.setResource(resource);
+        }
+        return reader;
+    }
+}
