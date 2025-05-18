022ab92b6af987855e4bb6b956649b4603af672f
jack ning <github@bytedesk.com>
2025-05-18 10:51:08 +0800

    update

--- modules/kbase/src/main/java/com/bytedesk/kbase/llm_website/WebsiteEntity.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/llm_website/WebsiteEntity.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-05-11 18:14:28
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-05-18 10:49:34
+ * @LastEditTime: 2025-05-18 10:50:46
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -70,7 +70,6 @@ public class WebsiteEntity extends BaseEntity {
     private String status = ChunkStatusEnum.NEW.name();
 
     @Builder.Default
-    @Column(name = "vector_status")
     private String vectorStatus = ChunkStatusEnum.NEW.name();
 
     @Builder.Default
--- modules/kbase/src/main/java/com/bytedesk/kbase/llm_website/WebsiteResponse.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/llm_website/WebsiteResponse.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-05-11 18:26:12
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-05-18 10:27:01
+ * @LastEditTime: 2025-05-18 10:50:03
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -39,8 +39,11 @@ public class WebsiteResponse extends BaseResponse {
 
     private String description;
 
-
     private String content;
+
+    private String status;
+
+    private String vectorStatus;
 
     private List<String> tagList;
 
