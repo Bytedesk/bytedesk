71925cd5c7a9f0b623c68698f8ad9201f8344dbb
jack ning <github@bytedesk.com>
2025-05-19 18:48:29 +0800

    update

--- modules/core/src/main/java/com/bytedesk/core/push/hw/package-info.java
+++ modules/core/src/main/java/com/bytedesk/core/push/hw/package-info.java
@@ -1,17 +1,4 @@
-/*
- * @Author: jackning 270580156@qq.com
- * @Date: 2025-05-19 18:38:42
- * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-05-19 18:38:53
- * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
- *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
- *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
- *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
- *  contact: 270580156@qq.com 
- * 
- * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
- */
 @NonNullApi
-package com.bytedesk.core.favorite;
+package com.bytedesk.core.push.hw;
 
 import org.springframework.lang.NonNullApi;
