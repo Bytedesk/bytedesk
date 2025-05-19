2321465bf6f34c4bf720e5c4aa1f232a300971ea
jack ning <github@bytedesk.com>
2025-05-19 10:16:35 +0800

    update channels/wechat: add        1 mod        4 del        1 files

--- modules/core/src/main/java/com/bytedesk/core/message/IMessageSendService.java
+++ modules/core/src/main/java/com/bytedesk/core/message/IMessageSendService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-10-15 16:27:05
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-10-23 23:30:23
+ * @LastEditTime: 2025-05-19 10:14:33
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
