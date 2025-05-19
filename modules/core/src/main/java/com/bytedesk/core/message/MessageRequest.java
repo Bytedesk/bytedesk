42491d4a7c06676133428acb29bcf403789cfc7d
jack ning <github@bytedesk.com>
2025-05-19 17:02:14 +0800

    update

--- modules/core/src/main/java/com/bytedesk/core/message/MessageRequest.java
+++ modules/core/src/main/java/com/bytedesk/core/message/MessageRequest.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-02-21 10:00:32
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-05-07 15:11:40
+ * @LastEditTime: 2025-05-19 16:55:45
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -39,10 +39,6 @@ public class MessageRequest extends BaseRequest {
     private String nickname;
 
     private String type;
-
-    // 有帮助、没帮助
-    // @Builder.Default
-    // private String helpful = MessageHelpfulEnum.HELPFUL.name();
 
     // used for client query
     private String componentType;
