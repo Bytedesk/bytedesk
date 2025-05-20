f434db8bc5212d7f23558f472fb5dc9f3d2436c5
jack ning <github@bytedesk.com>
2025-05-20 09:16:35 +0800

    update

--- modules/core/src/main/java/com/bytedesk/core/ip/access/IpAccessRepository.java
+++ modules/core/src/main/java/com/bytedesk/core/ip/access/IpAccessRepository.java
@@ -15,8 +15,9 @@ package com.bytedesk.core.ip.access;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public interface IpAccessRepository extends JpaRepository<IpAccessEntity, Long> {
     
-    IpAccessEntity findFirstByIpAndEndpointAndAccessTimeAfter(String ip, String endpoint, LocalDateTime time);
+    Optional<IpAccessEntity> findFirstByIpAndEndpointAndAccessTimeAfter(String ip, String endpoint, LocalDateTime time);
 } 
