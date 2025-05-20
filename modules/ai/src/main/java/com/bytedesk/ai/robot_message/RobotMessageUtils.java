3800463ec303641e52bed10648d43ceb73a203cf
jack ning <github@bytedesk.com>
2025-05-20 15:05:04 +0800

    update

--- modules/ai/src/main/java/com/bytedesk/ai/robot_message/RobotMessageUtils.java
+++ modules/ai/src/main/java/com/bytedesk/ai/robot_message/RobotMessageUtils.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-03-11 15:46:21
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-05-17 14:40:59
+ * @LastEditTime: 2025-05-20 15:02:39
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -24,41 +24,43 @@ import com.bytedesk.core.uid.UidUtils;
 
 public class RobotMessageUtils {
 
-        public static MessageProtobuf createRobotMessage(ThreadProtobuf threadProtobuf,
-                        RobotProtobuf robot, MessageProtobuf messageProtobuf) {
-                
+        public static MessageProtobuf createRobotMessage(ThreadProtobuf thread, RobotProtobuf robot,
+                        MessageProtobuf messageProtobuf) {
+
                 return MessageProtobuf.builder()
-                                .uid(UidUtils.getInstance().getUid())
-                                .status(MessageStatusEnum.SUCCESS)
-                                .thread(threadProtobuf)
-                                .user(robot.toUserProtobuf())
-                                .client(ClientEnum.SYSTEM)
-                                .extra(messageProtobuf.getExtra())
-                                .createdAt(LocalDateTime.now())
-                                .build();
+                        .uid(UidUtils.getInstance().getUid())
+                        .status(MessageStatusEnum.SUCCESS)
+                        .thread(thread)
+                        .user(robot.toUserProtobuf())
+                        .client(ClientEnum.SYSTEM)
+                        .extra(messageProtobuf.getExtra())
+                        .createdAt(LocalDateTime.now())
+                        .build();
         }
 
-        // public static MessageProtobuf createRobotMessage(ThreadEntity thread, ThreadProtobuf threadProtobuf,
-        //                 RobotEntity robot,
-        //                 MessageProtobuf messageProtobuf) {
-        //         MessageExtra extraObject = JSONObject.parseObject(messageProtobuf.getExtra(), MessageExtra.class);
-        //         UserProtobuf user = UserProtobuf.builder()
-        //                         .nickname(robot.getNickname())
-        //                         .avatar(robot.getAvatar())
-        //                         .type(UserTypeEnum.ROBOT.name())
-        //                         .build();
-        //         user.setUid(robot.getUid());
-        //         String messageUid = UidUtils.getInstance().getUid();
-        //         MessageProtobuf message = MessageProtobuf.builder()
-        //                         .uid(messageUid)
-        //                         .status(MessageStatusEnum.SUCCESS)
-        //                         .thread(threadProtobuf)
-        //                         .user(user)
-        //                         .client(ClientEnum.ROBOT)
-        //                         .extra(JSONObject.toJSONString(extraObject))
-        //                         .createdAt(LocalDateTime.now())
-        //                         .build();
-        //         return message;
+        // public static MessageProtobuf createRobotMessage(ThreadEntity thread,
+        // ThreadProtobuf threadProtobuf,
+        // RobotEntity robot,
+        // MessageProtobuf messageProtobuf) {
+        // MessageExtra extraObject = JSONObject.parseObject(messageProtobuf.getExtra(),
+        // MessageExtra.class);
+        // UserProtobuf user = UserProtobuf.builder()
+        // .nickname(robot.getNickname())
+        // .avatar(robot.getAvatar())
+        // .type(UserTypeEnum.ROBOT.name())
+        // .build();
+        // user.setUid(robot.getUid());
+        // String messageUid = UidUtils.getInstance().getUid();
+        // MessageProtobuf message = MessageProtobuf.builder()
+        // .uid(messageUid)
+        // .status(MessageStatusEnum.SUCCESS)
+        // .thread(threadProtobuf)
+        // .user(user)
+        // .client(ClientEnum.ROBOT)
+        // .extra(JSONObject.toJSONString(extraObject))
+        // .createdAt(LocalDateTime.now())
+        // .build();
+        // return message;
         // }
 
 }
