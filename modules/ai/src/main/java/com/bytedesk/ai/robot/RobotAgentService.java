a4f7d1d515d3a0ab57ed7adbdf1354e1cea8ac07
jack ning <github@bytedesk.com>
2025-05-21 14:29:24 +0800

    update modules/ai: mod        4 files

--- modules/ai/src/main/java/com/bytedesk/ai/robot/RobotAgentService.java
+++ modules/ai/src/main/java/com/bytedesk/ai/robot/RobotAgentService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-05-21 14:23:55
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-05-21 14:23:59
+ * @LastEditTime: 2025-05-21 14:27:19
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -15,9 +15,17 @@ package com.bytedesk.ai.robot;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * 智能工单填写、智能小结、智能质检
+ * @Author: jackning
+ */
 @Service
 public class RobotAgentService {
 
-    
+    public RobotRequest autoFillTicket(RobotRequest request) {
+        return request;
+    }
+
+
     
 }
