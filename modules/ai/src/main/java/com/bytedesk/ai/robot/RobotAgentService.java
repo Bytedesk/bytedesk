61d42fd17c6cc0d913f60436feffcebe3fb99eac
jack ning <github@bytedesk.com>
2025-05-21 17:08:14 +0800

    update modules/ticket: mod        2 files

--- modules/ai/src/main/java/com/bytedesk/ai/robot/RobotAgentService.java
+++ modules/ai/src/main/java/com/bytedesk/ai/robot/RobotAgentService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-05-21 14:23:55
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-05-21 14:27:19
+ * @LastEditTime: 2025-05-21 17:03:42
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -15,12 +15,17 @@ package com.bytedesk.ai.robot;
 
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
 /**
  * 智能工单填写、智能小结、智能质检
  * @Author: jackning
  */
 @Service
+@RequiredArgsConstructor
 public class RobotAgentService {
+
+    private final RobotRestService robotRestService;
 
     public RobotRequest autoFillTicket(RobotRequest request) {
         return request;
--- modules/ticket/src/main/java/com/bytedesk/ticket/ticket/TicketRestController.java
+++ modules/ticket/src/main/java/com/bytedesk/ticket/ticket/TicketRestController.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-01-16 14:56:28
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-05-21 14:28:29
+ * @LastEditTime: 2025-05-21 17:02:23
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -155,7 +155,6 @@ public class TicketRestController extends BaseRestController<TicketRequest> {
 
         return ResponseEntity.ok(JsonResult.success());
     }
-
 
     /**
      * 认领工单
