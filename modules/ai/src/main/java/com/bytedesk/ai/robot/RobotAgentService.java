8c61071ec59e5bc443f3ae1526068fd92fc257b9
jack ning <github@bytedesk.com>
2025-05-21 14:24:13 +0800

    update modules/ai: add        1 mod        4 files

---
+++ modules/ai/src/main/java/com/bytedesk/ai/robot/RobotAgentService.java
@@ -0,0 +1,23 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2025-05-21 14:23:55
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-05-21 14:23:59
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ * 
+ * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
+ */
+package com.bytedesk.ai.robot;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RobotAgentService {
+
+    
+    
+}
