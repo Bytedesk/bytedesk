16b16f4161827a4eeb3c7e9604f096686975b21d
jack ning <github@bytedesk.com>
2025-05-22 10:31:18 +0800

    update starter/src: mod        6 files

--- modules/core/src/main/java/com/bytedesk/core/config/properties/BytedeskProperties.java
+++ modules/core/src/main/java/com/bytedesk/core/config/properties/BytedeskProperties.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-30 09:14:39
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-05-07 15:25:32
+ * @LastEditTime: 2025-05-22 10:23:17
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -120,7 +120,7 @@ public class BytedeskProperties {
 
     private Boolean debug;
 
-    private String edition = Edition.COMMUNITY.name().toLowerCase();
+    // private String edition = Edition.COMMUNITY.name().toLowerCase();
 
     private String version;
 
