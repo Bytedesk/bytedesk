5bb47714ec50b4ae11fe35540dd92f6352ca7a20
jack ning <github@bytedesk.com>
2025-05-26 13:35:19 +0800

    update modules/core: mod        2 files

--- modules/core/src/main/java/com/bytedesk/core/message/MessageRequest.java
+++ modules/core/src/main/java/com/bytedesk/core/message/MessageRequest.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-02-21 10:00:32
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-05-26 12:59:57
+ * @LastEditTime: 2025-05-26 13:12:57
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -43,4 +43,7 @@ public class MessageRequest extends BaseRequest {
 
     // used for client query
     private String componentType;
+
+    // 搜索关键字
+    private String searchText;
 }
