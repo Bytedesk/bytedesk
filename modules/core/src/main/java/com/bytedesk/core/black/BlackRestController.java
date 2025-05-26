af26ce51142df68b44d71be175cdcc64fd03c195
jack ning <github@bytedesk.com>
2025-05-26 14:35:26 +0800

    update modules/core: mod        4 files

--- modules/core/src/main/java/com/bytedesk/core/black/BlackRestController.java
+++ modules/core/src/main/java/com/bytedesk/core/black/BlackRestController.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-06-27 12:20:45
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-05-06 10:21:37
+ * @LastEditTime: 2025-05-26 14:05:37
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -21,16 +21,32 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bytedesk.core.base.BaseRestController;
 import com.bytedesk.core.utils.JsonResult;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 
+/**
+ * 黑名单管理接口
+ * 
+ * @author Jackning
+ * @since 2024-06-27
+ */
 @RestController
 @RequestMapping("/api/v1/black")
 @AllArgsConstructor
+@Tag(name = "黑名单管理", description = "黑名单增删改查操作")
 public class BlackRestController extends BaseRestController<BlackRequest> {
 
     private final BlackRestService blackRestService;
 
+    /**
+     * 根据组织查询黑名单
+     * 
+     * @param request 查询请求
+     * @return 分页黑名单列表
+     */
+    @Operation(summary = "根据组织查询黑名单", description = "返回当前组织的黑名单列表")
     @Override
     public ResponseEntity<?> queryByOrg(BlackRequest request) {
         
@@ -39,6 +55,13 @@ public class BlackRestController extends BaseRestController<BlackRequest> {
         return ResponseEntity.ok(JsonResult.success(page));
     }
 
+    /**
+     * 根据用户查询黑名单
+     * 
+     * @param request 查询请求
+     * @return 分页黑名单列表
+     */
+    @Operation(summary = "根据用户查询黑名单", description = "返回当前用户的黑名单列表")
     @Override
     public ResponseEntity<?> queryByUser(BlackRequest request) {
         
@@ -47,12 +70,26 @@ public class BlackRestController extends BaseRestController<BlackRequest> {
         return ResponseEntity.ok(JsonResult.success(page));
     }
 
+    /**
+     * 根据UID查询黑名单
+     * 
+     * @param request 查询请求
+     * @return 黑名单详情
+     */
+    @Operation(summary = "根据UID查询黑名单", description = "根据唯一标识符查询黑名单")
     @Override
     public ResponseEntity<?> queryByUid(BlackRequest request) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'queryByUid'");
     }
 
+    /**
+     * 创建黑名单
+     * 
+     * @param request 创建请求
+     * @return 创建的黑名单
+     */
+    @Operation(summary = "创建黑名单", description = "创建新的黑名单记录")
     @Override
     public ResponseEntity<?> create(BlackRequest request) {
 
@@ -61,6 +98,13 @@ public class BlackRestController extends BaseRestController<BlackRequest> {
         return ResponseEntity.ok(JsonResult.success(response));
     }
 
+    /**
+     * 更新黑名单
+     * 
+     * @param request 更新请求
+     * @return 更新后的黑名单
+     */
+    @Operation(summary = "更新黑名单", description = "更新已存在的黑名单记录")
     @Override
     public ResponseEntity<?> update(BlackRequest request) {
 
@@ -69,6 +113,13 @@ public class BlackRestController extends BaseRestController<BlackRequest> {
         return ResponseEntity.ok(JsonResult.success(response));
     }
 
+    /**
+     * 删除黑名单
+     * 
+     * @param request 删除请求
+     * @return 删除结果
+     */
+    @Operation(summary = "删除黑名单", description = "删除指定的黑名单记录")
     @Override
     public ResponseEntity<?> delete(BlackRequest request) {
 
@@ -77,6 +128,14 @@ public class BlackRestController extends BaseRestController<BlackRequest> {
         return ResponseEntity.ok(JsonResult.success());
     }
 
+    /**
+     * 导出黑名单列表
+     * 
+     * @param request 导出请求
+     * @param response HTTP响应
+     * @return 导出结果
+     */
+    @Operation(summary = "导出黑名单", description = "导出黑名单数据到Excel")
     @Override
     public Object export(BlackRequest request, HttpServletResponse response) {
         return exportTemplate(
--- modules/core/src/main/java/com/bytedesk/core/thread/ThreadRestController.java
+++ modules/core/src/main/java/com/bytedesk/core/thread/ThreadRestController.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-05-09 12:00:16
+ * @LastEditTime: 2025-05-26 14:24:58
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -28,16 +28,32 @@ import com.bytedesk.core.annotation.ActionAnnotation;
 import com.bytedesk.core.base.BaseRestController;
 import com.bytedesk.core.utils.JsonResult;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 
+/**
+ * 会话管理接口
+ * 
+ * @author Jackning
+ * @since 2024-01-29
+ */
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/v1/thread")
+@Tag(name = "会话管理", description = "会话管理相关接口，包括查询、创建、更新、删除、置顶、标星等操作")
 public class ThreadRestController extends BaseRestController<ThreadRequest> {
 
     private final ThreadRestService threadRestService;
 
+    /**
+     * 根据组织查询会话
+     * 
+     * @param request 查询请求
+     * @return 分页会话列表
+     */
+    @Operation(summary = "根据组织查询会话", description = "返回当前组织的会话列表")
     @Override
     public ResponseEntity<?> queryByOrg(ThreadRequest request) {
 
@@ -46,6 +62,13 @@ public class ThreadRestController extends BaseRestController<ThreadRequest> {
         return ResponseEntity.ok(JsonResult.success(threadPage));
     }
 
+    /**
+     * 根据用户查询会话
+     * 
+     * @param request 查询请求
+     * @return 分页会话列表
+     */
+    @Operation(summary = "根据用户查询会话", description = "返回当前用户的会话列表") 
     @Override
     public ResponseEntity<?> queryByUser(ThreadRequest request) {
 
@@ -54,7 +77,13 @@ public class ThreadRestController extends BaseRestController<ThreadRequest> {
         return ResponseEntity.ok(JsonResult.success(threadPage));
     }
 
-    // query invite threads
+    /**
+     * 查询邀请会话
+     * 
+     * @param request 查询请求
+     * @return 分页邀请会话列表
+     */
+    @Operation(summary = "查询邀请会话", description = "查询邀请相关的会话")
     @GetMapping("/query/invite")
     public ResponseEntity<?> queryByThreadInvite(ThreadRequest request) {
 
@@ -63,6 +92,13 @@ public class ThreadRestController extends BaseRestController<ThreadRequest> {
         return ResponseEntity.ok(JsonResult.success(threadPage));
     }
 
+    /**
+     * 根据主题查询会话
+     * 
+     * @param request 查询请求
+     * @return 会话信息
+     */
+    @Operation(summary = "根据主题查询会话", description = "通过主题查找相关会话")
     @GetMapping("/query/topic")
     public ResponseEntity<?> queryByThreadTopic(ThreadRequest request) {
 
@@ -74,6 +110,13 @@ public class ThreadRestController extends BaseRestController<ThreadRequest> {
         return ResponseEntity.ok(JsonResult.error("not found"));
     }
 
+    /**
+     * 根据UID查询会话
+     * 
+     * @param request 查询请求
+     * @return 会话信息
+     */
+    @Operation(summary = "根据UID查询会话", description = "通过唯一标识符查询会话")
     @Override
     public ResponseEntity<?> queryByUid(ThreadRequest request) {
 
@@ -85,6 +128,13 @@ public class ThreadRestController extends BaseRestController<ThreadRequest> {
         return ResponseEntity.ok(JsonResult.error("not found"));
     }
 
+    /**
+     * 创建会话
+     * 
+     * @param request 创建会话请求
+     * @return 创建的会话
+     */
+    @Operation(summary = "创建会话", description = "创建新的会话")
     @ActionAnnotation(title = "会话", action = "新建", description = "create thread")
     @Override
     public ResponseEntity<?> create(@RequestBody ThreadRequest request) {
@@ -94,6 +144,13 @@ public class ThreadRestController extends BaseRestController<ThreadRequest> {
         return ResponseEntity.ok(JsonResult.success(thread));
     }
 
+    /**
+     * 更新会话
+     * 
+     * @param request 更新会话请求
+     * @return 更新后的会话
+     */
+    @Operation(summary = "更新会话", description = "更新已存在的会话信息")
     @ActionAnnotation(title = "会话", action = "更新", description = "update thread")
     @Override
     public ResponseEntity<?> update(@RequestBody ThreadRequest request) {
@@ -103,7 +160,13 @@ public class ThreadRestController extends BaseRestController<ThreadRequest> {
         return ResponseEntity.ok(JsonResult.success(threadResponse));
     }
 
-    // update top
+    /**
+     * 更新会话置顶状态
+     * 
+     * @param request 更新请求
+     * @return 更新后的会话
+     */
+    @Operation(summary = "更新会话置顶状态", description = "设置或取消会话置顶")
     @PostMapping("/update/top")
     public ResponseEntity<?> updateTop(@RequestBody ThreadRequest request) {
 
@@ -112,7 +175,13 @@ public class ThreadRestController extends BaseRestController<ThreadRequest> {
         return ResponseEntity.ok(JsonResult.success(thread));
     }
 
-    // update star
+    /**
+     * 更新会话标星状态
+     * 
+     * @param request 更新请求
+     * @return 更新后的会话
+     */
+    @Operation(summary = "更新会话标星状态", description = "设置或取消会话标星")
     @PostMapping("/update/star")
     public ResponseEntity<?> updateStar(@RequestBody ThreadRequest request) {
 
@@ -121,7 +190,13 @@ public class ThreadRestController extends BaseRestController<ThreadRequest> {
         return ResponseEntity.ok(JsonResult.success(threadResponse));
     }
 
-    // update mute
+    /**
+     * 更新会话静音状态
+     * 
+     * @param request 更新请求
+     * @return 更新后的会话
+     */
+    @Operation(summary = "更新会话静音状态", description = "设置或取消会话静音")
     @PostMapping("/update/mute")
     public ResponseEntity<?> updateMute(@RequestBody ThreadRequest request) {
 
@@ -130,7 +205,13 @@ public class ThreadRestController extends BaseRestController<ThreadRequest> {
         return ResponseEntity.ok(JsonResult.success(threadResponse));
     }
 
-    // update user info
+    /**
+     * 更新会话用户信息
+     * 
+     * @param request 更新请求
+     * @return 更新后的会话
+     */
+    @Operation(summary = "更新会话用户信息", description = "更新会话关联的用户信息")
     @PostMapping("/update/user")
     public ResponseEntity<?> updateUser(@RequestBody ThreadRequest request) {
 
@@ -139,7 +220,13 @@ public class ThreadRestController extends BaseRestController<ThreadRequest> {
         return ResponseEntity.ok(JsonResult.success(threadResponse));
     }
 
-    // update tagList
+    /**
+     * 更新会话标签列表
+     * 
+     * @param request 更新请求
+     * @return 更新后的会话
+     */
+    @Operation(summary = "更新会话标签列表", description = "更新会话的标签信息")
     @PostMapping("/update/tagList")
     public ResponseEntity<?> updateTagList(@RequestBody ThreadRequest request) {
         
@@ -148,6 +235,13 @@ public class ThreadRestController extends BaseRestController<ThreadRequest> {
         return ResponseEntity.ok(JsonResult.success(threadResponse));
     }
 
+    /**
+     * 更新会话未读状态
+     * 
+     * @param request 更新请求
+     * @return 更新后的会话
+     */
+    @Operation(summary = "更新会话未读状态", description = "标记会话为已读或未读")
     @PostMapping("/update/unread")
     public ResponseEntity<?> updateUnread(@RequestBody ThreadRequest request) {
 
@@ -156,7 +250,13 @@ public class ThreadRestController extends BaseRestController<ThreadRequest> {
         return ResponseEntity.ok(JsonResult.success(threadResponse));
     }
 
-    // updateThreadUnreadCount
+    /**
+     * 更新会话未读数量
+     * 
+     * @param request 更新请求
+     * @return 更新后的会话
+     */
+    @Operation(summary = "更新会话未读数量", description = "更新会话的未读消息数量")
     @PostMapping("/update/unread/count")
     public ResponseEntity<?> updateUnreadCount(@RequestBody ThreadRequest request) {
 
@@ -165,6 +265,13 @@ public class ThreadRestController extends BaseRestController<ThreadRequest> {
         return ResponseEntity.ok(JsonResult.success(threadResponse));
     }
 
+    /**
+     * 关闭会话
+     * 
+     * @param request 关闭请求
+     * @return 关闭后的会话
+     */
+    @Operation(summary = "关闭会话", description = "关闭指定会话")
     @ActionAnnotation(title = "会话", action = "close", description = "close thread")
     @PostMapping("/close")
     public ResponseEntity<?> close(@RequestBody ThreadRequest request) {
@@ -176,6 +283,13 @@ public class ThreadRestController extends BaseRestController<ThreadRequest> {
         return ResponseEntity.ok(JsonResult.success(threadResponse));
     }
     
+    /**
+     * 删除会话
+     * 
+     * @param request 删除请求
+     * @return 删除结果
+     */
+    @Operation(summary = "删除会话", description = "删除指定会话")
     @Override
     public ResponseEntity<?> delete(@RequestBody ThreadRequest request) {
         
@@ -184,8 +298,14 @@ public class ThreadRestController extends BaseRestController<ThreadRequest> {
         return ResponseEntity.ok(JsonResult.success("delete thread success"));
     }
 
-    // https://github.com/alibaba/easyexcel
-    // https://easyexcel.opensource.alibaba.com/docs/current/
+    /**
+     * 导出会话列表
+     * 
+     * @param request 导出请求
+     * @param response HTTP响应
+     * @return 导出结果
+     */
+    @Operation(summary = "导出会话列表", description = "将会话数据导出为Excel格式")
     @ActionAnnotation(title = "会话", action = "导出", description = "export thread")
     @GetMapping("/export")
     public Object export(ThreadRequest request, HttpServletResponse response) {
