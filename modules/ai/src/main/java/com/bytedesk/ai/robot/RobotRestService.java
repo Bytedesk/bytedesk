3b8081fdc46deb3381cac3621569bc604aeb76b9
jack ning <github@bytedesk.com>
2025-05-27 09:06:00 +0800

    update modules/ai: mod        2 files

--- modules/ai/src/main/java/com/bytedesk/ai/robot/RobotRestService.java
+++ modules/ai/src/main/java/com/bytedesk/ai/robot/RobotRestService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-03-22 16:44:41
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-05-22 14:13:42
+ * @LastEditTime: 2025-05-27 08:58:57
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -648,6 +648,8 @@ public class RobotRestService extends BaseRestServiceWithExcel<RobotEntity, Robo
         //
         RobotLlm llm = robot.getLlm();
         llm.setPrompt(request.getLlm().getPrompt());
+        llm.setProvider(request.getLlm().getProvider());
+        llm.setModel(request.getLlm().getModel());
         robot.setLlm(llm);
         //
         RobotEntity savedRobot = save(robot);
