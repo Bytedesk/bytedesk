6ae63d1a3d1b384c7be84c260949d22bfa8d1e7d
jack ning <github@bytedesk.com>
2025-05-28 16:36:27 +0800

    update starter/src: add     1585 mod     1105 del     1311 files

--- modules/ai/src/main/java/com/bytedesk/ai/robot/RobotEntity.java
+++ modules/ai/src/main/java/com/bytedesk/ai/robot/RobotEntity.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-03-22 16:16:26
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-05-21 14:49:00
+ * @LastEditTime: 2025-05-28 16:20:42
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
