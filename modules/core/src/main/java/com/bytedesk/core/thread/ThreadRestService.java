abf4edefeb2496bd363f6e11e28589e678aacf64
jack ning <github@bytedesk.com>
2025-06-03 14:01:13 +0800

    update modules/core: mod        1 files

--- modules/core/src/main/java/com/bytedesk/core/thread/ThreadRestService.java
+++ modules/core/src/main/java/com/bytedesk/core/thread/ThreadRestService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-06-03 13:11:51
+ * @LastEditTime: 2025-06-03 13:20:24
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -603,9 +603,9 @@ public class ThreadRestService
                 if (entity.getTagList() != null && !entity.getTagList().isEmpty()) {
                     latestEntity.setTagList(entity.getTagList());
                 }
-                if (entity.getClient() != null) {
-                    latestEntity.setClient(entity.getClient());
-                }
+                // if (entity.getClient() != null) {
+                //     latestEntity.setClient(entity.getClient());
+                // }
                 if (entity.getExtra() != null) {
                     latestEntity.setExtra(entity.getExtra());
                 }
