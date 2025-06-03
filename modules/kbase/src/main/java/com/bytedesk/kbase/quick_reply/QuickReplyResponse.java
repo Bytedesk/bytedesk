f1a2073f9fd3d5f868fb161481be85da6bbb95ff
jack ning <github@bytedesk.com>
2025-06-03 17:28:11 +0800

    update

--- modules/kbase/src/main/java/com/bytedesk/kbase/quick_reply/QuickReplyResponse.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/quick_reply/QuickReplyResponse.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-03-22 23:00:00
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-05-31 09:30:42
+ * @LastEditTime: 2025-06-03 17:15:05
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -57,5 +57,11 @@ public class QuickReplyResponse extends BaseResponse {
     private String kbUid;
 
     private String agentUid;
+
+    // elastic 索引状态 (ArticleStatusEnum: PENDING, PROCESSING, SUCCESS, ERROR)
+    private String elasticStatus;
+
+    // 向量索引状态 (ArticleStatusEnum: PENDING, PROCESSING, SUCCESS, ERROR)
+    private String vectorStatus;
     
 }
