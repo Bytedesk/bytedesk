be173be0e33ff913aa5f210c5ade98fdcbae7ac5
jack ning <github@bytedesk.com>
2025-06-04 17:13:04 +0800

    update modules/core: mod        3 files

--- modules/core/src/main/java/com/bytedesk/core/tag/TagInitializer.java
+++ modules/core/src/main/java/com/bytedesk/core/tag/TagInitializer.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-11-06 21:43:58
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-06-04 15:52:21
+ * @LastEditTime: 2025-06-04 17:09:29
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
