5cb8394d89ae67e49f75bc0c1af88fa63f0c1a43
jack ning <github@bytedesk.com>
2025-06-06 20:18:04 +0800

    update modules/core: mod        1 files

--- modules/core/src/main/java/com/bytedesk/core/webhook_received/WebhookReceivedRestController.java
+++ modules/core/src/main/java/com/bytedesk/core/webhook_received/WebhookReceivedRestController.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-05-11 18:25:36
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-06-06 09:44:18
+ * @LastEditTime: 2025-06-06 20:05:09
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -15,7 +15,6 @@ package com.bytedesk.core.webhook_received;
 
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-// import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
