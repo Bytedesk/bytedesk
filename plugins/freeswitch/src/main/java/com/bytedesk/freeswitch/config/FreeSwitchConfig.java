bab8a78210858a307daf8d3c3e68988ac80ccee6
jack ning <github@bytedesk.com>
2025-06-07 07:37:34 +0800

    update plugins/freeswitch: mod        4 del        3 files

--- plugins/freeswitch/src/main/java/com/bytedesk/freeswitch/config/FreeSwitchConfig.java
+++ plugins/freeswitch/src/main/java/com/bytedesk/freeswitch/config/FreeSwitchConfig.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-05-24 10:14:52
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-06-05 20:25:08
+ * @LastEditTime: 2025-06-07 07:13:25
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -13,14 +13,14 @@
  */
 package com.bytedesk.freeswitch.config;
 
-// import org.freeswitch.esl.client.inbound.Client;
-// import org.freeswitch.esl.client.inbound.InboundConnectionFailure;
+import org.freeswitch.esl.client.inbound.Client;
+import org.freeswitch.esl.client.inbound.InboundConnectionFailure;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-// import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-// import com.bytedesk.freeswitch.handler.FreeSwitchEventListener;
+import com.bytedesk.freeswitch.handler.FreeSwitchEventListener;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -34,38 +34,38 @@ import lombok.extern.slf4j.Slf4j;
 @ConditionalOnProperty(name = "bytedesk.freeswitch.enabled", havingValue = "true", matchIfMissing = false)
 public class FreeSwitchConfig {
         
-    // private final FreeSwitchProperties freeSwitchProperties;
-    // private final FreeSwitchEventListener freeSwitchEventListener;
+    private final FreeSwitchProperties freeSwitchProperties;
+    private final FreeSwitchEventListener freeSwitchEventListener;
 
     /**
      * 配置FreeSwitch ESL客户端
      */
-    // @Bean
-    // public Client eslClient() {
-    //     Client client = new Client();
+    @Bean
+    public Client eslClient() {
+        Client client = new Client();
         
-    //     try {
-    //         log.info("连接FreeSwitch ESL: {}:{}", 
-    //                 freeSwitchProperties.getServer(), 
-    //                 freeSwitchProperties.getEslPort());
+        try {
+            log.info("连接FreeSwitch ESL: {}:{}", 
+                    freeSwitchProperties.getServer(), 
+                    freeSwitchProperties.getEslPort());
                     
-    //         client.connect(
-    //             freeSwitchProperties.getServer(), 
-    //             freeSwitchProperties.getEslPort(), 
-    //             freeSwitchProperties.getEslPassword(),
-    //             2);
+            client.connect(
+                freeSwitchProperties.getServer(), 
+                freeSwitchProperties.getEslPort(), 
+                freeSwitchProperties.getEslPassword(),
+                2);
                 
-    //         // 注册事件监听器
-    //         client.addEventListener(freeSwitchEventListener);
+            // 注册事件监听器
+            client.addEventListener(freeSwitchEventListener);
             
-    //         // 订阅所有事件
-    //         client.setEventSubscriptions("plain", "all");
+            // 订阅所有事件
+            client.setEventSubscriptions("plain", "all");
             
-    //         log.info("FreeSwitch ESL连接成功");
-    //     } catch (InboundConnectionFailure e) {
-    //         log.error("FreeSwitch ESL连接失败: {}", e.getMessage(), e);
-    //     }
+            log.info("FreeSwitch ESL连接成功");
+        } catch (InboundConnectionFailure e) {
+            log.error("FreeSwitch ESL连接失败: {}", e.getMessage(), e);
+        }
         
-    //     return client;
-    // }
+        return client;
+    }
 }
--- plugins/freeswitch/src/main/java/com/bytedesk/freeswitch/handler/FreeSwitchEventListener.java
+++ plugins/freeswitch/src/main/java/com/bytedesk/freeswitch/handler/FreeSwitchEventListener.java
@@ -1,129 +1,129 @@
 package com.bytedesk.freeswitch.handler;
 
-// import org.freeswitch.esl.client.IEslEventListener;
-// import org.freeswitch.esl.client.transport.event.EslEvent;
-// import org.springframework.stereotype.Component;
+import org.freeswitch.esl.client.IEslEventListener;
+import org.freeswitch.esl.client.transport.event.EslEvent;
+import org.springframework.stereotype.Component;
 
-// import com.bytedesk.freeswitch.service.ICallService;
+import com.bytedesk.freeswitch.service.CallService;
 
-// import lombok.RequiredArgsConstructor;
-// import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
-// /**
-//  * FreeSwitch事件监听器
-//  */
-// @Slf4j
-// @Component
-// @RequiredArgsConstructor
-// public class FreeSwitchEventListener implements IEslEventListener {
+/**
+ * FreeSwitch事件监听器
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class FreeSwitchEventListener implements IEslEventListener {
 
-//     private final ICallService callService;
+    private final CallService callService;
     
-//     /**
-//      * 处理FreeSwitch事件
-//      */
-//     @Override
-//     public void eventReceived(EslEvent eslEvent) {
-//             String eventName = eslEvent.getEventName();
-//         // String eventSubclass = eslEvent.getEventSubclass();
+    /**
+     * 处理FreeSwitch事件
+     */
+    @Override
+    public void eventReceived(EslEvent eslEvent) {
+            String eventName = eslEvent.getEventName();
+        // String eventSubclass = eslEvent.getEventSubclass();
         
-//         log.debug("收到FreeSwitch事件: {} / {}", eventName);
+        log.debug("收到FreeSwitch事件: {} / {}", eventName);
         
-//         // 处理不同类型的事件
-//         switch(eventName) {
-//             case "CHANNEL_CREATE":
-//                 handleChannelCreate(eslEvent);
-//                 break;
+        // 处理不同类型的事件
+        switch(eventName) {
+            case "CHANNEL_CREATE":
+                handleChannelCreate(eslEvent);
+                break;
                 
-//             case "CHANNEL_ANSWER":
-//                 handleChannelAnswer(eslEvent);
-//                 break;
+            case "CHANNEL_ANSWER":
+                handleChannelAnswer(eslEvent);
+                break;
                 
-//             case "CHANNEL_HANGUP":
-//                 handleChannelHangup(eslEvent);
-//                 break;
+            case "CHANNEL_HANGUP":
+                handleChannelHangup(eslEvent);
+                break;
                 
-//             case "DTMF":
-//                 handleDtmf(eslEvent);
-//                 break;
+            case "DTMF":
+                handleDtmf(eslEvent);
+                break;
                 
-//             case "CUSTOM":
-//                 handleCustomEvent(eslEvent);
-//                 break;
+            case "CUSTOM":
+                handleCustomEvent(eslEvent);
+                break;
                 
-//             default:
-//                 // 其他事件暂不处理
-//                 break;
-//         }
-//     }
+            default:
+                // 其他事件暂不处理
+                break;
+        }
+    }
 
-//     @Override
-//     public void backgroundJobResultReceived(EslEvent event) {
-//         // TODO Auto-generated method stub
-//         throw new UnsupportedOperationException("Unimplemented method 'backgroundJobResultReceived'");
-//     }
+    @Override
+    public void backgroundJobResultReceived(EslEvent event) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'backgroundJobResultReceived'");
+    }
 
-//     /**
-//      * 处理通道创建事件
-//      */
-//     private void handleChannelCreate(EslEvent eslEvent) {
-//         String callerId = eslEvent.getEventHeaders().get("Caller-Caller-ID-Number");
-//         String destination = eslEvent.getEventHeaders().get("Caller-Destination-Number");
-//         String uuid = eslEvent.getEventHeaders().get("Unique-ID");
+    /**
+     * 处理通道创建事件
+     */
+    private void handleChannelCreate(EslEvent eslEvent) {
+        String callerId = eslEvent.getEventHeaders().get("Caller-Caller-ID-Number");
+        String destination = eslEvent.getEventHeaders().get("Caller-Destination-Number");
+        String uuid = eslEvent.getEventHeaders().get("Unique-ID");
         
-//         log.info("通道创建: 主叫 {} 被叫 {} UUID {}", callerId, destination, uuid);
+        log.info("通道创建: 主叫 {} 被叫 {} UUID {}", callerId, destination, uuid);
         
-//         // 调用通话服务处理
-//         callService.handleCallStart(callerId, destination, uuid);
-//     }
+        // 调用通话服务处理
+        callService.handleCallStart(callerId, destination, uuid);
+    }
     
-//     /**
-//      * 处理通道应答事件
-//      */
-//     private void handleChannelAnswer(EslEvent eslEvent) {
-//         String uuid = eslEvent.getEventHeaders().get("Unique-ID");
+    /**
+     * 处理通道应答事件
+     */
+    private void handleChannelAnswer(EslEvent eslEvent) {
+        String uuid = eslEvent.getEventHeaders().get("Unique-ID");
         
-//         log.info("通道应答: UUID {}", uuid);
+        log.info("通道应答: UUID {}", uuid);
         
-//         // 调用通话服务处理
-//         callService.handleCallAnswered(uuid);
-//     }
+        // 调用通话服务处理
+        callService.handleCallAnswered(uuid);
+    }
     
-//     /**
-//      * 处理通道挂断事件
-//      */
-//     private void handleChannelHangup(EslEvent eslEvent) {
-//         String uuid = eslEvent.getEventHeaders().get("Unique-ID");
-//         String hangupCause = eslEvent.getEventHeaders().get("Hangup-Cause");
+    /**
+     * 处理通道挂断事件
+     */
+    private void handleChannelHangup(EslEvent eslEvent) {
+        String uuid = eslEvent.getEventHeaders().get("Unique-ID");
+        String hangupCause = eslEvent.getEventHeaders().get("Hangup-Cause");
         
-//         log.info("通道挂断: UUID {} 原因 {}", uuid, hangupCause);
+        log.info("通道挂断: UUID {} 原因 {}", uuid, hangupCause);
         
-//         // 调用通话服务处理
-//         callService.handleCallEnd(uuid, hangupCause);
-//     }
+        // 调用通话服务处理
+        callService.handleCallEnd(uuid, hangupCause);
+    }
     
-//     /**
-//      * 处理DTMF按键事件
-//      */
-//     private void handleDtmf(EslEvent eslEvent) {
-//         String uuid = eslEvent.getEventHeaders().get("Unique-ID");
-//         String digit = eslEvent.getEventHeaders().get("DTMF-Digit");
+    /**
+     * 处理DTMF按键事件
+     */
+    private void handleDtmf(EslEvent eslEvent) {
+        String uuid = eslEvent.getEventHeaders().get("Unique-ID");
+        String digit = eslEvent.getEventHeaders().get("DTMF-Digit");
         
-//         log.info("DTMF按键: UUID {} 键值 {}", uuid, digit);
+        log.info("DTMF按键: UUID {} 键值 {}", uuid, digit);
         
-//         // 调用通话服务处理
-//         callService.handleDtmf(uuid, digit);
-//     }
+        // 调用通话服务处理
+        callService.handleDtmf(uuid, digit);
+    }
     
-//     /**
-//      * 处理自定义事件
-//      */
-//     private void handleCustomEvent(EslEvent eslEvent) {
-//         // String eventSubclass = eslEvent.getEventSubclass();
+    /**
+     * 处理自定义事件
+     */
+    private void handleCustomEvent(EslEvent eslEvent) {
+        // String eventSubclass = eslEvent.getEventSubclass();
         
-//         // if ("bytedesk::custom".equals(eventSubclass)) {
-//         //     // 处理自定义事件
-//         //     log.info("自定义事件: {}", eslEvent.getEventHeaders());
-//         // }
-//     }
-// }
+        // if ("bytedesk::custom".equals(eventSubclass)) {
+        //     // 处理自定义事件
+        //     log.info("自定义事件: {}", eslEvent.getEventHeaders());
+        // }
+    }
+}
