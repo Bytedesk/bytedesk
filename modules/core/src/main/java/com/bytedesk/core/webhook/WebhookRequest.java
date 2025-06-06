32252013818ad6834ea40077808c87ca213f1aca
jack ning <github@bytedesk.com>
2025-06-06 17:25:02 +0800

    update

--- modules/core/src/main/java/com/bytedesk/core/webhook/WebhookRequest.java
+++ modules/core/src/main/java/com/bytedesk/core/webhook/WebhookRequest.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-05-11 18:26:04
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-06-04 15:35:51
+ * @LastEditTime: 2025-06-06 17:22:26
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -39,9 +39,7 @@ public class WebhookRequest extends BaseRequest {
     // @Builder.Default
     // private String type = WebhookTypeEnum.CUSTOMER.name();
 
-    @Builder.Default
-    private String color = "red";
+    private String url;
 
-    @Builder.Default
-    private Integer order = 0;
+    private String secret;
 }
--- modules/core/src/main/java/com/bytedesk/core/webhook/WebhookResponse.java
+++ modules/core/src/main/java/com/bytedesk/core/webhook/WebhookResponse.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-05-11 18:26:12
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-06-04 15:36:28
+ * @LastEditTime: 2025-06-06 17:22:39
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -37,8 +37,8 @@ public class WebhookResponse extends BaseResponse {
 
     private String type;
 
-    private String color;
+    private String url;
 
-    private Integer order;
+    private String secret;
 
 }
