9e7a036e40839b09df2f20b8ff5a600ac0532325
jack ning <github@bytedesk.com>
2025-06-07 10:21:16 +0800

    update starter/src: mod        2 files

--- modules/kbase/src/main/java/com/bytedesk/kbase/kbase/KbaseConsts.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/kbase/KbaseConsts.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-07-24 11:10:03
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-01 12:35:45
+ * @LastEditTime: 2025-06-07 10:14:12
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -13,38 +13,63 @@
  */
 package com.bytedesk.kbase.kbase;
 
-import com.bytedesk.core.constant.I18Consts;
-
 public class KbaseConsts {
 
     private KbaseConsts() {
     }
 
-    public static final String KB_NAME = I18Consts.I18N_PREFIX + "kb.name";
+    // public static final String KB_NAME = I18Consts.I18N_PREFIX + "kb.name";
 
-    public static final String KB_PLATFORM_NAME = I18Consts.I18N_PREFIX + "kb.platform.name";
+    // public static final String KB_PLATFORM_NAME = I18Consts.I18N_PREFIX + "kb.platform.name";
 
-    public static final String KB_HELPCENTER_NAME = I18Consts.I18N_PREFIX + "kb.helpcenter.name";
+    // public static final String KB_HELPCENTER_NAME = I18Consts.I18N_PREFIX + "kb.helpcenter.name";
 
-    public static final String KB_NOTEBASE_NAME = I18Consts.I18N_PREFIX + "kb.notebase.name";
+    // public static final String KB_NOTEBASE_NAME = I18Consts.I18N_PREFIX + "kb.notebase.name";
 
-    public static final String HEADLINE = I18Consts.I18N_PREFIX + "kb.helpcenter.headline";
+    // public static final String HEADLINE = I18Consts.I18N_PREFIX + "kb.helpcenter.headline";
 
-    public static final String SUB_HEADLINE = I18Consts.I18N_PREFIX + "kb.helpcenter.subHeadline";
+    // public static final String SUB_HEADLINE = I18Consts.I18N_PREFIX + "kb.helpcenter.subHeadline";
 
-    public static final String KB_LLM_NAME = I18Consts.I18N_PREFIX + "kb.llm.name";
+    // public static final String KB_LLM_NAME = I18Consts.I18N_PREFIX + "kb.llm.name";
 
-    public static final String KB_KEYWORD_NAME = I18Consts.I18N_PREFIX + "kb.keyword.name";
+    // public static final String KB_KEYWORD_NAME = I18Consts.I18N_PREFIX + "kb.keyword.name";
 
-    public static final String KB_FAQ_NAME = I18Consts.I18N_PREFIX + "kb.faq.name";
+    // public static final String KB_FAQ_NAME = I18Consts.I18N_PREFIX + "kb.faq.name";
 
-    public static final String KB_AUTOREPLY_NAME = I18Consts.I18N_PREFIX + "kb.autoreply.name";
+    // public static final String KB_AUTOREPLY_NAME = I18Consts.I18N_PREFIX + "kb.autoreply.name";
 
-    public static final String KB_QUICKREPLY_NAME = I18Consts.I18N_PREFIX + "kb.quickreply.name";
+    // public static final String KB_QUICKREPLY_NAME = I18Consts.I18N_PREFIX + "kb.quickreply.name";
 
-    public static final String KB_TABOO_NAME = I18Consts.I18N_PREFIX + "kb.taboo.name";
+    // public static final String KB_TABOO_NAME = I18Consts.I18N_PREFIX + "kb.taboo.name";
 
-    public static final String KB_DESCRIPTION = I18Consts.I18N_PREFIX + "kb.description";
+    // public static final String KB_DESCRIPTION = I18Consts.I18N_PREFIX + "kb.description";
+
+    public static final String KB_NAME = "知识库";
+
+    public static final String KB_PLATFORM_NAME = "知识库平台";
+
+    public static final String KB_HELPCENTER_NAME = "帮助中心";
+
+    public static final String KB_NOTEBASE_NAME = "笔记库";
+
+    public static final String HEADLINE = "帮助中心标题";
+
+    public static final String SUB_HEADLINE = "帮助中心副标题";
+
+    public static final String KB_LLM_NAME = "大语言模型";
+
+    public static final String KB_KEYWORD_NAME = "关键词";
+
+    public static final String KB_FAQ_NAME = "常见问题";
+
+    public static final String KB_AUTOREPLY_NAME = "自动回复";
+
+    public static final String KB_QUICKREPLY_NAME = "快捷回复";
+
+    public static final String KB_TABOO_NAME = "违禁词";
+
+    public static final String KB_DESCRIPTION = "描述";
+
 
     // public static final String KB_EMBEDDING = "M3E_BASE";
 
