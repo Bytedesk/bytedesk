7818d6d62ef286fa7e7b3dbd216e3dc437a64137
jack ning <github@bytedesk.com>
2025-06-12 09:42:02 +0800

    update modules/kbase: mod        3 files

--- modules/kbase/src/main/java/com/bytedesk/kbase/quick_reply/QuickReplyRequest.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/quick_reply/QuickReplyRequest.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-03-22 22:59:48
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-06-04 18:42:19
+ * @LastEditTime: 2025-06-12 09:18:06
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -73,4 +73,7 @@ public class QuickReplyRequest extends BaseRequest {
 
     // 
     private String searchText;
+
+    // used for client query
+    private String componentType;
 }
