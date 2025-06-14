51dda217e6ebf39a3f95bef52b4bdb2b45a5fee9
jack ning <github@bytedesk.com>
2025-06-14 13:16:16 +0800

    update modules/core: mod        3 files

--- modules/core/src/main/java/com/bytedesk/core/group/GroupRestService.java
+++ modules/core/src/main/java/com/bytedesk/core/group/GroupRestService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:20:17
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-06-13 14:02:09
+ * @LastEditTime: 2025-06-14 13:12:27
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
--- modules/core/src/main/java/com/bytedesk/core/group/GroupSpecification.java
+++ modules/core/src/main/java/com/bytedesk/core/group/GroupSpecification.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-06-28 10:46:47
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-03 23:50:34
+ * @LastEditTime: 2025-06-14 13:12:18
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -34,6 +34,11 @@ public class GroupSpecification extends BaseSpecification {
             // 
             if (StringUtils.hasText(request.getName())) {
                 predicates.add(criteriaBuilder.like(root.get("name"), "%" + request.getName() + "%"));
+            }
+            //
+            // 如果userUid不为空，则查询该用户所在的群组
+            if (StringUtils.hasText(request.getUserUid())) {
+                predicates.add(criteriaBuilder.equal(root.join("members").get("user").get("uid"), request.getUserUid()));
             }
             //
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
