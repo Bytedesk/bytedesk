64f0384b26a9b3d52e59d6904c4d896ddf0aa148
jack ning <github@bytedesk.com>
2025-06-16 18:34:31 +0800

    update

--- modules/core/src/main/java/com/bytedesk/core/enums/ClientEnum.java
+++ modules/core/src/main/java/com/bytedesk/core/enums/ClientEnum.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-05-25 13:07:20
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-06-16 11:11:56
+ * @LastEditTime: 2025-06-16 17:26:27
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -24,6 +24,7 @@ public enum ClientEnum {
     // 
     IOS,
     ANDROID,
+    // 
     ELECTRON,
     LINUX,
     MACOS,
--- modules/core/src/main/java/com/bytedesk/core/message/MessageSendServiceImpl.java
+++ modules/core/src/main/java/com/bytedesk/core/message/MessageSendServiceImpl.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-10-15 16:27:35
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-03-27 20:47:43
+ * @LastEditTime: 2025-06-16 16:58:19
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -16,10 +16,6 @@ package com.bytedesk.core.message;
 import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson2.JSON;
-import com.bytedesk.core.annotation.BlackIpFilter;
-import com.bytedesk.core.annotation.BlackUserFilter;
-import com.bytedesk.core.annotation.TabooJsonFilter;
-import com.bytedesk.core.annotation.TabooProtobufFilter;
 import com.bytedesk.core.config.BytedeskEventPublisher;
 import com.bytedesk.core.utils.ApplicationContextHolder;
 
@@ -29,18 +25,12 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class MessageSendServiceImpl implements IMessageSendService {
 
-    @BlackIpFilter(title = "black", action = "sendJsonMessage")
-    @BlackUserFilter(title = "black", action = "sendJsonMessage")
-    @TabooJsonFilter(title = "敏感词", action = "sendJsonMessage")
     @Override
     public void sendJsonMessage(String json) {
         // log.debug("sendJsonMessage: {}", json);
         publishMessageJsonEvent(json);
     }
 
-    @BlackIpFilter(title = "black", action = "sendProtobufMessage")
-    @BlackUserFilter(title = "black", action = "sendProtobufMessage")
-    @TabooProtobufFilter(title = "敏感词", action = "sendProtobufMessage")
     @Override
     public void sendProtobufMessage(MessageProtobuf messageProtobuf) {
         String json = JSON.toJSONString(messageProtobuf);
