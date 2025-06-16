dcedafc59a3e1f3e3f9ffe53bd9a9527009acd22
jack ning <github@bytedesk.com>
2025-06-16 14:04:20 +0800

    update

--- modules/service/src/main/java/com/bytedesk/service/visitor/VisitorRestService.java
+++ modules/service/src/main/java/com/bytedesk/service/visitor/VisitorRestService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-06-12 16:23:13
+ * @LastEditTime: 2025-06-16 13:50:26
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -80,7 +80,7 @@ public class VisitorRestService extends BaseRestServiceWithExcel<VisitorEntity, 
 
     public VisitorResponse create(VisitorRequest request) {
         //
-        String visitorUid =  request.getVisitorUid();
+        String visitorUid = request.getVisitorUid();
         if (!StringUtils.hasText(visitorUid)) {
             visitorUid = request.getUid();
         }
