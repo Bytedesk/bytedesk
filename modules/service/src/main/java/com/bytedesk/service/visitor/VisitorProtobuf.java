74a2ba30d96bb2bd5b5208d99632d772b2176b48
jack ning <github@bytedesk.com>
2025-06-21 14:41:06 +0800

    update

---
+++ modules/service/src/main/java/com/bytedesk/service/visitor/VisitorProtobuf.java
@@ -0,0 +1,80 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2024-01-29 16:21:24
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-06-21 14:34:42
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ *  联系：270580156@qq.com
+ * Copyright (c) 2024 by bytedesk.com, All Rights Reserved. 
+ */
+package com.bytedesk.service.visitor;
+
+import java.io.Serializable;
+
+import com.alibaba.fastjson2.JSON;
+import com.bytedesk.core.constant.AvatarConsts;
+import com.bytedesk.core.constant.BytedeskConsts;
+import com.bytedesk.core.constant.I18Consts;
+import com.bytedesk.core.rbac.user.UserTypeEnum;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * 所有字段跟user.proto中字段一一对应
+ */
+@Data
+@Builder
+@Accessors(chain = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class VisitorProtobuf implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+	private String uid;
+
+    private String nickname;
+
+    private String avatar;
+
+    // ROBOT/AGENT/SYSTEM/USER/VISITOR/WORKGROUP
+    @Builder.Default
+    private String type = UserTypeEnum.USER.name();
+
+    private String extra;
+
+    public static VisitorProtobuf fromJson(String user) {
+        return JSON.parseObject(user, VisitorProtobuf.class);
+    }
+
+    public String toJson() {
+        return JSON.toJSONString(this);
+    }
+
+    public static VisitorProtobuf getSystemUser() {
+        return VisitorProtobuf.builder()
+                .uid(BytedeskConsts.DEFAULT_SYSTEM_UID)
+                .nickname(I18Consts.I18N_SYSTEM_NOTIFICATION_NAME)
+                .avatar(AvatarConsts.getDefaultSystemNotificationAvatarUrl())
+                .type(UserTypeEnum.SYSTEM.name())
+                .build();
+    }
+
+    public static VisitorProtobuf getFileAssistantUser() {
+        return VisitorProtobuf.builder()
+                .uid(BytedeskConsts.DEFAULT_FILE_ASSISTANT_UID)
+                .nickname(I18Consts.I18N_FILE_ASSISTANT_NAME)
+                .avatar(AvatarConsts.getDefaultFileAssistantAvatarUrl())
+                .type(UserTypeEnum.SYSTEM.name())
+                .build();
+    }
+
+}
--- modules/service/src/main/java/com/bytedesk/service/visitor/VisitorRestControllerVisitor.java
+++ modules/service/src/main/java/com/bytedesk/service/visitor/VisitorRestControllerVisitor.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-06-21 13:13:18
+ * @LastEditTime: 2025-06-21 14:32:29
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -95,10 +95,9 @@ public class VisitorRestControllerVisitor {
         }
         // 
         VisitorResponse visitor = visitorRestService.create(visitorRequest);
-        // 
-        UserProtobuf user = ServiceConvertUtils.convertToVisitorProtobuf(visitor);
+        // UserProtobuf user = ServiceConvertUtils.convertToVisitorProtobuf(visitor);
         //
-        return ResponseEntity.ok(JsonResult.success(user));
+        return ResponseEntity.ok(JsonResult.success(visitor));
     }
 
     // @VisitorAnnotation(title = "visitor", action = "requestThread", description = "request thread")
