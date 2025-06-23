70f400980463d6a27c8a41ef204218dd1ce8bcaf
jack ning <github@bytedesk.com>
2025-06-23 15:08:41 +0800

    update modules/service: mod        2 files

--- modules/service/src/main/java/com/bytedesk/service/message_unread/MessageUnreadRestService.java
+++ modules/service/src/main/java/com/bytedesk/service/message_unread/MessageUnreadRestService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-06-28 17:19:02
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-06-23 12:15:22
+ * @LastEditTime: 2025-06-23 15:05:56
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
