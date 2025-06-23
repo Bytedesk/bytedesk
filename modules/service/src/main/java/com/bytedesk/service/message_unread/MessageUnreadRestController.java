d8b82b254e09a63712cee02370fc7c71de035119
jack ning <github@bytedesk.com>
2025-06-23 13:48:16 +0800

    update modules/service: mod        1 files

--- modules/service/src/main/java/com/bytedesk/service/message_unread/MessageUnreadRestController.java
+++ modules/service/src/main/java/com/bytedesk/service/message_unread/MessageUnreadRestController.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-07-01 12:38:42
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-06-23 10:41:29
+ * @LastEditTime: 2025-06-23 13:38:13
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -13,16 +13,17 @@
  */
 package com.bytedesk.service.message_unread;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bytedesk.core.base.BaseRestController;
-import com.bytedesk.core.message.MessageResponse;
 import com.bytedesk.core.utils.JsonResult;
 
 import jakarta.servlet.http.HttpServletResponse;
@@ -89,6 +90,26 @@ public class MessageUnreadRestController extends BaseRestController<MessageUnrea
         // messageUnreadService.export(request, response);
 
         return JsonResult.success("export unread messages success");
+    }
+
+    // 获取未读消息数
+    @GetMapping("/count")
+    public ResponseEntity<?> getMessageUnreadCount(MessageUnreadRequest request) {
+
+        long count = messageUnreadService.getUnreadCount(request);
+
+        return ResponseEntity.ok(JsonResult.success("get unread messages count success", count));
+    }
+
+    // 清空当前用户所有未读消息
+    @PostMapping("/clear")
+    public ResponseEntity<?> clearMessageUnread(@RequestBody MessageUnreadRequest request) {
+        // 
+        messageUnreadService.clearUnreadMessages(request);
+        // 看下是否清空了
+        long count = messageUnreadService.getUnreadCount(request);
+
+        return ResponseEntity.ok(JsonResult.success("clear unread messages count success", count));
     }
     
 
