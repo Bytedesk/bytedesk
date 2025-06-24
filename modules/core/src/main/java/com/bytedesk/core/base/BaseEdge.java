23750e83e5c3fe397bc471474620747f2a11eacc
jack ning <github@bytedesk.com>
2025-06-24 16:48:57 +0800

    update modules/core: add        1 files

---
+++ modules/core/src/main/java/com/bytedesk/core/base/BaseEdge.java
@@ -0,0 +1,39 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2025-06-19 16:16:28
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-06-24 16:46:01
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ * 
+ * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
+ */
+package com.bytedesk.core.base;
+
+import java.io.Serializable;
+
+import com.alibaba.fastjson2.JSON;
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@MappedSuperclass
+public abstract class BaseEdge implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public String toJson() {
+        return JSON.toJSONString(this);
+    }
+    
+}
