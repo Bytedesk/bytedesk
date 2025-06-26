a9c5a5077ede175bea88bb1beb4084bb3346f84c
jack ning <github@bytedesk.com>
2025-06-26 21:09:29 +0800

    update

--- modules/core/src/test/java/com/bytedesk/core/upload/watermark/WatermarkServiceTest.java
+++ modules/core/src/test/java/com/bytedesk/core/upload/watermark/WatermarkServiceTest.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-01-27 10:00:00
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-01-27 10:00:00
+ * @LastEditTime: 2025-06-26 21:00:43
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -18,7 +18,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
