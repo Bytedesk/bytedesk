84474653a5e36ab54d7a18de3636da00db90412a
jack ning <github@bytedesk.com>
2025-06-26 18:00:08 +0800

    update

--- modules/kbase/src/main/java/com/bytedesk/kbase/article_archive/ArticleArchiveRestController.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/article_archive/ArticleArchiveRestController.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-03-22 22:59:07
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-06-25 09:25:22
+ * @LastEditTime: 2025-06-26 17:59:25
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -15,7 +15,6 @@ package com.bytedesk.kbase.article_archive;
 
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
