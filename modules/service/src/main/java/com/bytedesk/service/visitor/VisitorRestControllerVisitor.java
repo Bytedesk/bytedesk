fd8b09dc99a30b5154b25230f03cfc4e25e4d823
jack ning <github@bytedesk.com>
2025-07-04 09:42:40 +0800

    update modules/service: mod        1 files

--- modules/service/src/main/java/com/bytedesk/service/visitor/VisitorRestControllerVisitor.java
+++ modules/service/src/main/java/com/bytedesk/service/visitor/VisitorRestControllerVisitor.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-07-04 09:02:49
+ * @LastEditTime: 2025-07-04 09:31:19
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -91,7 +91,6 @@ public class VisitorRestControllerVisitor {
         }
         // 
         VisitorResponse visitor = visitorRestService.create(visitorRequest);
-        // VisitorProtobuf visitorProtobuf = ServiceConvertUtils.convertToVisitorProtobuf(visitor);
         //
         return ResponseEntity.ok(JsonResult.success(visitor));
     }
