947afa4af81198bb1ff68133e481cb9edb37b147
jack ning <github@bytedesk.com>
2025-07-04 09:12:00 +0800

    update modules/service: mod        4 files

--- modules/service/src/main/java/com/bytedesk/service/visitor/VisitorRestControllerVisitor.java
+++ modules/service/src/main/java/com/bytedesk/service/visitor/VisitorRestControllerVisitor.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-06-23 12:16:13
+ * @LastEditTime: 2025-07-04 09:02:49
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -77,7 +77,6 @@ public class VisitorRestControllerVisitor {
 
     private final ExecutorService executorService = Executors.newCachedThreadPool();
 
-    @ApiRateLimiter(value = 10.0, timeout = 1)
     @PostMapping("/init")
     public ResponseEntity<?> init(@RequestBody VisitorRequest visitorRequest, HttpServletRequest httpRequest) {
         //
