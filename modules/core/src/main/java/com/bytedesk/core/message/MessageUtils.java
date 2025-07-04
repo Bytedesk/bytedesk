0fb97cd1a2ceaadb3bf256899e9b188748c3303b
jack ning <github@bytedesk.com>
2025-07-04 10:43:46 +0800

    update

--- modules/core/src/main/java/com/bytedesk/core/message/MessageUtils.java
+++ modules/core/src/main/java/com/bytedesk/core/message/MessageUtils.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-08-31 16:23:54
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-07-04 10:25:22
+ * @LastEditTime: 2025-07-04 10:34:17
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -13,7 +13,6 @@
  */
 package com.bytedesk.core.message;
 
-import java.time.ZonedDateTime;
 import com.bytedesk.core.config.BytedeskEventPublisher;
 import com.bytedesk.core.enums.ClientEnum;
 import com.bytedesk.core.rbac.user.UserProtobuf;
