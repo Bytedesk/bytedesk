39c1a267f32220cb26d13aba3fdc1ae5753094b9
jack ning <github@bytedesk.com>
2025-07-11 16:13:43 +0800

    update channels/social: add        1 mod        8 del        4 files

--- modules/core/src/main/java/com/bytedesk/core/thread/ThreadExtra.java
+++ modules/core/src/main/java/com/bytedesk/core/thread/ThreadExtra.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-09-07 08:11:16
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-05-19 11:57:44
+ * @LastEditTime: 2025-07-11 16:07:08
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
