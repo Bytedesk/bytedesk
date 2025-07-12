57a03a5c27938be9ebb4d1fe09cfcde8d0c9e65e
jack ning <github@bytedesk.com>
2025-07-12 12:21:21 +0800

    update

--- modules/core/src/main/java/com/bytedesk/core/favorite/FavoriteRepository.java
+++ modules/core/src/main/java/com/bytedesk/core/favorite/FavoriteRepository.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-05-11 18:25:55
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-10-24 18:20:39
+ * @LastEditTime: 2025-07-12 12:10:14
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -22,5 +22,4 @@ public interface FavoriteRepository extends JpaRepository<FavoriteEntity, Long>,
 
     Optional<FavoriteEntity> findByUid(String uid);
 
-    // Boolean existsByPlatform(String platform);
 }
