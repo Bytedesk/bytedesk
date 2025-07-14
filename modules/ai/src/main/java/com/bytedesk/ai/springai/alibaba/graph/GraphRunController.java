2a3c4babf711e6e04816a82efef801952b0ba710
jack ning <github@bytedesk.com>
2025-07-14 17:40:55 +0800

    update modules/ai: add        5 files

---
+++ modules/ai/src/main/java/com/bytedesk/ai/springai/alibaba/graph/GraphRunController.java
@@ -0,0 +1,61 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2025-07-14 17:38:38
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-07-14 17:40:30
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ * 
+ * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
+ */
+package com.bytedesk.ai.springai.alibaba.graph;
+
+import com.alibaba.cloud.ai.graph.CompiledGraph;
+import com.alibaba.cloud.ai.graph.NodeOutput;
+import com.alibaba.cloud.ai.graph.OverAllState;
+import com.alibaba.cloud.ai.graph.async.AsyncGenerator;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@RestController("/alibaba/graph/run")
+public class GraphRunController {
+
+    private CompiledGraph graph;
+
+    public GraphRunController(@Qualifier("buildGraph") CompiledGraph graph) {
+        this.graph = graph;
+    }
+
+    // http://127.0.0.1:9003/alibaba/graph/run/stream
+    @PostMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<NodeOutput> stream(@RequestBody Map<String, Object> inputs) throws Exception {
+        AsyncGenerator<NodeOutput> nodeOutputs = graph.stream(inputs);
+        return Flux.fromStream(nodeOutputs.stream());
+    }
+
+    // http://127.0.0.1:9003/alibaba/graph/run/invoke
+    @PostMapping(value = "/invoke")
+    public OverAllState invoke(@RequestBody Map<String, Object> inputs) throws Exception {
+        return graph.invoke(inputs).orElse(null);
+    }
+
+    // http://127.0.0.1:9003/alibaba/graph/run/start
+    @PostMapping(value = "/start")
+    public Map<String, Object> startInvoke(@RequestBody Map<String, Object> inputs) throws Exception {
+        Map<String, Object> startInputs = new HashMap<>();
+        startInputs.put("review", inputs.get("review")); // null
+        return graph.invoke(startInputs).get().data();
+
+    }
+
+}
---
+++ modules/ai/src/main/java/com/bytedesk/ai/springai/alibaba/mcp/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.bytedesk.ai.springai.alibaba.mcp;
+
+import org.springframework.lang.NonNullApi;
