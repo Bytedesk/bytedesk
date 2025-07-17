55d8c73c08afc328f37efecff39b081e5aa8ddb3
jack ning <github@bytedesk.com>
2025-07-17 08:53:08 +0800

    Update LlmConfigUtilsTest.java

--- modules/core/src/test/java/com/bytedesk/core/utils/LlmConfigUtilsTest.java
+++ modules/core/src/test/java/com/bytedesk/core/utils/LlmConfigUtilsTest.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-01-27 10:00:00
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-01-27 10:00:00
+ * @LastEditTime: 2025-07-17 08:49:05
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -16,7 +16,6 @@ package com.bytedesk.core.utils;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.core.env.Environment;
 import org.springframework.mock.env.MockEnvironment;
 
 import com.bytedesk.core.base.LlmProviderConfigDefault;
