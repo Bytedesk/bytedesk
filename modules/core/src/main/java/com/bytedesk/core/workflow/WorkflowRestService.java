f4ef09694f882b1fdf2f044dcfe4369292127de2
jack ning <github@bytedesk.com>
2025-07-25 12:00:55 +0800

    update modules/core: mod        1 files

--- modules/core/src/main/java/com/bytedesk/core/workflow/WorkflowRestService.java
+++ modules/core/src/main/java/com/bytedesk/core/workflow/WorkflowRestService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-05-11 18:25:45
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-07-24 13:47:49
+ * @LastEditTime: 2025-07-25 11:46:09
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -164,6 +164,5 @@ public class WorkflowRestService extends BaseRestService<WorkflowEntity, Workflo
         return modelMapper.map(entity, WorkflowResponse.class);
     }
 
-    
 
 }
