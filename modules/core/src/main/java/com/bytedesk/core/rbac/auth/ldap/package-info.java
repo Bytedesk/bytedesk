5be1ad798733f70aab10f6b6d67012949549a087
jack ning <github@bytedesk.com>
2025-07-26 18:58:53 +0800

    update

--- modules/core/src/main/java/com/bytedesk/core/rbac/auth/ldap/package-info.java
+++
@@ -1,4 +0,0 @@
-@NonNullApi
-package com.bytedesk.core.rbac.auth.ldap;
-
-import org.springframework.lang.NonNullApi;
--- modules/core/src/main/java/com/bytedesk/core/rbac/auth/oauth2/package-info.java
+++
@@ -1,4 +0,0 @@
-@NonNullApi
-package com.bytedesk.core.rbac.auth.oauth2;
-
-import org.springframework.lang.NonNullApi;
