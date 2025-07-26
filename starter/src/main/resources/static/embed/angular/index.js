4403e804a8ac9639b36894b2cf803a73fc5f6696
jack ning <github@bytedesk.com>
2025-07-26 10:30:34 +0800

    update starter/src: mod        1 files

--- starter/src/main/resources/static/embed/angular/index.js
+++ starter/src/main/resources/static/embed/angular/index.js
@@ -1,30 +1,30 @@
-var M = Object.create;
-var C = Object.defineProperty;
-var N = Object.getOwnPropertyDescriptor;
-var E = (e, t) => (t = Symbol[e]) ? t : Symbol.for("Symbol." + e), b = (e) => {
+var N = Object.create;
+var h = Object.defineProperty;
+var P = Object.getOwnPropertyDescriptor;
+var E = (e, t) => (t = Symbol[e]) ? t : Symbol.for("Symbol." + e), p = (e) => {
   throw TypeError(e);
 };
-var F = (e, t, n) => t in e ? C(e, t, { enumerable: !0, configurable: !0, writable: !0, value: n }) : e[t] = n;
-var j = (e, t) => C(e, "name", { value: t, configurable: !0 });
-var G = (e) => [, , , M((e == null ? void 0 : e[E("metadata")]) ?? null)], H = ["class", "method", "getter", "setter", "accessor", "field", "value", "get", "set"], f = (e) => e !== void 0 && typeof e != "function" ? b("Function expected") : e, P = (e, t, n, y, s) => ({ kind: H[e], name: t, metadata: y, addInitializer: (l) => n._ ? b("Already initialized") : s.push(f(l || null)) }), I = (e, t) => F(t, E("metadata"), e[3]), O = (e, t, n, y) => {
-  for (var s = 0, l = e[t >> 1], u = l && l.length; s < u; s++) t & 1 ? l[s].call(n) : y = l[s].call(n, y);
-  return y;
-}, J = (e, t, n, y, s, l) => {
-  var u, d, D, c, w, o = t & 7, B = !!(t & 8), r = !!(t & 16), h = o > 3 ? e.length + 1 : o ? B ? 1 : 2 : 0, T = H[o + 5], W = o > 3 && (e[h - 1] = []), L = e[h] || (e[h] = []), g = o && (!r && !B && (s = s.prototype), o < 5 && (o > 3 || !r) && N(o < 4 ? s : { get [n]() {
-    return q(this, l);
-  }, set [n](i) {
-    return z(this, l, i);
+var F = (e, t, n) => t in e ? h(e, t, { enumerable: !0, configurable: !0, writable: !0, value: n }) : e[t] = n;
+var j = (e, t) => h(e, "name", { value: t, configurable: !0 });
+var G = (e) => [, , , N((e == null ? void 0 : e[E("metadata")]) ?? null)], H = ["class", "method", "getter", "setter", "accessor", "field", "value", "get", "set"], m = (e) => e !== void 0 && typeof e != "function" ? p("Function expected") : e, Q = (e, t, n, r, l) => ({ kind: H[e], name: t, metadata: r, addInitializer: (s) => n._ ? p("Already initialized") : l.push(m(s || null)) }), O = (e, t) => F(t, E("metadata"), e[3]), v = (e, t, n, r) => {
+  for (var l = 0, s = e[t >> 1], a = s && s.length; l < a; l++) t & 1 ? s[l].call(n) : r = s[l].call(n, r);
+  return r;
+}, J = (e, t, n, r, l, s) => {
+  var a, i, D, k, B, o = t & 7, b = !!(t & 8), y = !!(t & 16), A = o > 3 ? e.length + 1 : o ? b ? 1 : 2 : 0, T = H[o + 5], W = o > 3 && (e[A - 1] = []), M = e[A] || (e[A] = []), g = o && (!y && !b && (l = l.prototype), o < 5 && (o > 3 || !y) && P(o < 4 ? l : { get [n]() {
+    return q(this, s);
+  }, set [n](d) {
+    return z(this, s, d);
   } }, n));
-  o ? r && o < 4 && j(l, (o > 2 ? "set " : o > 1 ? "get " : "") + n) : j(s, n);
-  for (var A = y.length - 1; A >= 0; A--)
-    c = P(o, n, D = {}, e[3], L), o && (c.static = B, c.private = r, w = c.access = { has: r ? (i) => Q(s, i) : (i) => n in i }, o ^ 3 && (w.get = r ? (i) => (o ^ 1 ? q : S)(i, s, o ^ 4 ? l : g.get) : (i) => i[n]), o > 2 && (w.set = r ? (i, R) => z(i, s, R, o ^ 4 ? l : g.set) : (i, R) => i[n] = R)), d = (0, y[A])(o ? o < 4 ? r ? l : g[T] : o > 4 ? void 0 : { get: g.get, set: g.set } : s, c), D._ = 1, o ^ 4 || d === void 0 ? f(d) && (o > 4 ? W.unshift(d) : o ? r ? l = d : g[T] = d : s = d) : typeof d != "object" || d === null ? b("Object expected") : (f(u = d.get) && (g.get = u), f(u = d.set) && (g.set = u), f(u = d.init) && W.unshift(u));
-  return o || I(e, s), g && C(s, n, g), r ? o ^ 4 ? l : g : s;
-}, v = (e, t, n) => F(e, typeof t != "symbol" ? t + "" : t, n), x = (e, t, n) => t.has(e) || b("Cannot " + n), Q = (e, t) => Object(t) !== t ? b('Cannot use the "in" operator on this value') : e.has(t), q = (e, t, n) => (x(e, t, "read from private field"), n ? n.call(e) : t.get(e));
-var z = (e, t, n, y) => (x(e, t, "write to private field"), y ? y.call(e, n) : t.set(e, n), n), S = (e, t, n) => (x(e, t, "access private method"), n);
+  o ? y && o < 4 && j(s, (o > 2 ? "set " : o > 1 ? "get " : "") + n) : j(l, n);
+  for (var C = r.length - 1; C >= 0; C--)
+    k = Q(o, n, D = {}, e[3], M), o && (k.static = b, k.private = y, B = k.access = { has: y ? (d) => R(l, d) : (d) => n in d }, o ^ 3 && (B.get = y ? (d) => (o ^ 1 ? q : S)(d, l, o ^ 4 ? s : g.get) : (d) => d[n]), o > 2 && (B.set = y ? (d, I) => z(d, l, I, o ^ 4 ? s : g.set) : (d, I) => d[n] = I)), i = (0, r[C])(o ? o < 4 ? y ? s : g[T] : o > 4 ? void 0 : { get: g.get, set: g.set } : l, k), D._ = 1, o ^ 4 || i === void 0 ? m(i) && (o > 4 ? W.unshift(i) : o ? y ? s = i : g[T] = i : l = i) : typeof i != "object" || i === null ? p("Object expected") : (m(a = i.get) && (g.get = a), m(a = i.set) && (g.set = a), m(a = i.init) && W.unshift(a));
+  return o || O(e, l), g && h(l, n, g), y ? o ^ 4 ? s : g : l;
+}, K = (e, t, n) => F(e, typeof t != "symbol" ? t + "" : t, n), x = (e, t, n) => t.has(e) || p("Cannot " + n), R = (e, t) => Object(t) !== t ? p('Cannot use the "in" operator on this value') : e.has(t), q = (e, t, n) => (x(e, t, "read from private field"), n ? n.call(e) : t.get(e));
+var z = (e, t, n, r) => (x(e, t, "write to private field"), r ? r.call(e, n) : t.set(e, n), n), S = (e, t, n) => (x(e, t, "access private method"), n);
 import { Component as U, Input as V } from "@angular/core";
 import X from "../core/BytedeskWeb/index.js";
-let k = null, a = 0;
-var K, m, p;
+let u = null, c = 0;
+var L, f, w;
 const $ = U({
   selector: "bytedesk-angular",
   template: "",
@@ -33,24 +33,23 @@ const $ = U({
       display: none;
     }
   `]
-})((K = [V()], p = class {
+})((L = [V()], w = class {
   constructor() {
-    v(this, "config", O(m, 8, this)), O(m, 11, this);
-    v(this, "bytedeskRef", null);
+    K(this, "config", v(f, 8, this)), v(f, 11, this);
   }
   ngOnInit() {
-    if (a++, k) {
-      console.log("BytedeskAngular: 使用现有全局实例，当前活跃组件数:", a), this.bytedeskRef = k, window.bytedesk = k;
+    if (c++, u) {
+      console.log("BytedeskAngular: 使用现有全局实例，当前活跃组件数:", c), window.bytedesk = u;
       return;
     }
-    console.log("BytedeskAngular: 创建新的全局实例"), k = new X(this.config), this.config, this.bytedeskRef = k, k.init(), window.bytedesk = k;
+    console.log("BytedeskAngular: 创建新的全局实例"), u = new X(this.config), u.init(), window.bytedesk = u;
   }
   ngOnDestroy() {
-    a--, console.log("BytedeskAngular: 组件卸载，当前活跃组件数:", a), this.bytedeskRef = null, a <= 0 && (console.log("BytedeskAngular: 没有活跃组件，清理全局实例"), setTimeout(() => {
-      k && a <= 0 && (k.destroy(), k = null, delete window.bytedesk, a = 0);
+    c--, console.log("BytedeskAngular: 组件卸载，当前活跃组件数:", c), c <= 0 && (console.log("BytedeskAngular: 没有活跃组件，清理全局实例"), setTimeout(() => {
+      u && c <= 0 && (u.destroy(), u = null, delete window.bytedesk, c = 0);
     }, 100));
   }
-}, m = G(null), J(m, 5, "config", K, p), I(m, p), p));
+}, f = G(null), J(f, 5, "config", L, w), O(f, w), w));
 export {
   $ as BytedeskAngular
 };
