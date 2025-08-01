b6693385f4cb138f9eeff93c88adb92ec123369b
jack ning <github@bytedesk.com>
2025-08-01 15:27:25 +0800

    update

--- modules/ai/src/main/java/com/bytedesk/ai/robot/RobotSpecification.java
+++ modules/ai/src/main/java/com/bytedesk/ai/robot/RobotSpecification.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-06-12 09:07:53
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-07-21 19:49:22
+ * @LastEditTime: 2025-08-01 15:24:03
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -20,7 +20,6 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.util.StringUtils;
 
 import com.bytedesk.core.base.BaseSpecification;
-// import com.bytedesk.core.enums.LevelEnum;
 
 import jakarta.persistence.criteria.Predicate;
 import lombok.extern.slf4j.Slf4j;
