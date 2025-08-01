77ef1d233e7cb35573058ae6928351ac76aee5ef
jack ning <github@bytedesk.com>
2025-08-02 07:05:58 +0800

    update

--- modules/service/src/main/java/com/bytedesk/service/visitor/VisitorRestService.java
+++ modules/service/src/main/java/com/bytedesk/service/visitor/VisitorRestService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-29 16:21:24
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-08-01 15:39:50
+ * @LastEditTime: 2025-08-02 07:02:53
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
