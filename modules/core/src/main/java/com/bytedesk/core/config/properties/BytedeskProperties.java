e8b2c7a6805bd359b1e0bec93ac9fee00f0e32ce
jack ning <github@bytedesk.com>
2025-08-03 17:15:29 +0800

    update

--- modules/core/src/main/java/com/bytedesk/core/config/properties/BytedeskProperties.java
+++ modules/core/src/main/java/com/bytedesk/core/config/properties/BytedeskProperties.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-30 09:14:39
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-08-03 17:11:55
+ * @LastEditTime: 2025-08-03 17:12:57
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -238,10 +238,6 @@ public class BytedeskProperties {
         private List<String> mobileWhitelist = new ArrayList<>();
         private List<String> emailWhitelist = new ArrayList<>();
         private String validateCode;
-        // 
-        private Boolean allowRegister;
-        private Boolean forceValidateMobile;
-        private Boolean forceValidateEmail;
     }
 
     @Getter
