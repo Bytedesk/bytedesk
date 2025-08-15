84abe39d384b4c8f3f62a2349ba8c3d56aea3234
jack ning <github@bytedesk.com>
2025-08-15 22:19:57 +0800

    update

--- modules/core/src/main/java/com/bytedesk/core/redis/RedisConsts.java
+++ modules/core/src/main/java/com/bytedesk/core/redis/RedisConsts.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-10-23 15:20:38
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-07-10 16:33:33
+ * @LastEditTime: 2025-08-15 22:10:29
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
