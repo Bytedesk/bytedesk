5e7e467d3c2623d77178067d705cdf247bbe01cf
jack ning <github@bytedesk.com>
2025-08-19 10:07:44 +0800

    update

--- modules/ai/src/main/java/com/bytedesk/ai/springai/providers/deepseek/SpringAIDeepseekController.java
+++ modules/ai/src/main/java/com/bytedesk/ai/springai/providers/deepseek/SpringAIDeepseekController.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-02-13 13:41:56
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-07-18 17:06:32
+ * @LastEditTime: 2025-08-19 10:04:16
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -55,7 +55,6 @@ public class SpringAIDeepseekController {
     
     private final BytedeskProperties bytedeskProperties;
     private final SpringAIDeepseekService springAIDeepseekService;
-    // private final UidUtils uidUtils;
     private final ExecutorService executorService = Executors.newCachedThreadPool();
 
     /**
