bb5c9455827e9f45663e177171f005e4aa957e1c
jack ning <github@bytedesk.com>
2025-08-24 15:10:16 +0800

    update

--- modules/ai/src/main/java/com/bytedesk/ai/springai/providers/baidu/SpringAIBaiduChatController.java
+++ modules/ai/src/main/java/com/bytedesk/ai/springai/providers/baidu/SpringAIBaiduChatController.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-02-13 13:41:56
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-08-21 13:54:00
+ * @LastEditTime: 2025-08-24 15:06:43
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -58,6 +58,7 @@ public class SpringAIBaiduChatController {
             @RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
         
         String response = springAIBaiduService.processPromptSync(message, null, "");
+        
         return ResponseEntity.ok(JsonResult.success(response));
     }
 
