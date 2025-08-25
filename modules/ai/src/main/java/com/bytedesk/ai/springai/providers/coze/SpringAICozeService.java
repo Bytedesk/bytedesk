1467e465fd7d43da737a171d9aa5dd9c21f5135d
jack ning <github@bytedesk.com>
2025-08-26 07:34:41 +0800

    update

--- modules/ai/src/main/java/com/bytedesk/ai/springai/providers/coze/SpringAICozeService.java
+++ modules/ai/src/main/java/com/bytedesk/ai/springai/providers/coze/SpringAICozeService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-08-25 00:00:00
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-08-25 15:04:27
+ * @LastEditTime: 2025-08-26 07:27:49
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -464,4 +464,5 @@ public class SpringAICozeService extends BaseSpringAIService {
             return false;
         }
     }
+
 }
