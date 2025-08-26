e8739c3214ac1a8d588d83a13453c2c16a259055
jack ning <github@bytedesk.com>
2025-08-26 09:43:11 +0800

    Update FaqRestService.java

--- modules/kbase/src/main/java/com/bytedesk/kbase/llm_faq/FaqRestService.java
+++ modules/kbase/src/main/java/com/bytedesk/kbase/llm_faq/FaqRestService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-03-22 22:59:18
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-08-20 20:58:02
+ * @LastEditTime: 2025-08-26 09:38:12
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -644,7 +644,7 @@ public class FaqRestService extends BaseRestServiceWithExport<FaqEntity, FaqRequ
                 .uid(UidUtils.getInstance().getUid())
                 .content(content)
                 .type(MessageTypeEnum.FAQ_QUESTION.name())
-                .status(MessageStatusEnum.SUCCESS.name())
+                .status(MessageStatusEnum.READ.name())
                 .channel(threadEntity.getChannel())
                 .user(user)
                 .orgUid(threadEntity.getOrgUid())
