17aab06e1a1d753b916c5cc437965f5c7ed60c89
jack ning <github@bytedesk.com>
2025-09-01 22:13:31 +0800

    update

---
+++ modules/core/src/main/java/com/bytedesk/core/message/content/node/FormNodeContent.java
@@ -0,0 +1,18 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2025-09-01 22:10:30
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-09-01 22:10:54
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ * 
+ * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
+ */
+package com.bytedesk.core.message.content.node;
+
+public class FormNodeContent {
+    
+}
