0474cd459e95e4dd2484843e654531650fa55734
jack ning <github@bytedesk.com>
2025-09-02 23:29:39 +0800

    update

---
+++ modules/ai/src/main/java/com/bytedesk/ai/springai/providers/dashscope/SpringAIDashscopeRestService.java
@@ -0,0 +1,27 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2025-09-02 23:27:38
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-09-02 23:27:42
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ * 
+ * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
+ */
+package com.bytedesk.ai.springai.providers.dashscope;
+
+/**
+ * https://bailian.console.aliyun.com/?spm=a2c4g.11186623.0.0.11c67980m5X2VR&tab=api#/api/?type=model&url=2712472
+ */
+public class SpringAIDashscopeRestService {
+
+    /**
+     * curl "https://dashscope.aliyuncs.com/api/v1/deployments/models?page_no=1&page_size=100" \
+    --header "Authorization: Bearer ${DASHSCOPE_API_KEY}" \
+    --header 'Content-Type: application/json'
+     */
+    
+}
---
+++ modules/ai/src/main/java/com/bytedesk/ai/springai/providers/siliconflow/SpringAISiliconFlowRestService.java
@@ -0,0 +1,28 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2025-09-02 23:23:58
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-09-02 23:24:02
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ * 
+ * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
+ */
+package com.bytedesk.ai.springai.providers.siliconflow;
+
+/**
+ * https://docs.siliconflow.cn/cn/api-reference/models/get-model-list
+ */
+public class SpringAISiliconFlowRestService {
+
+    /**
+     * curl --request GET \
+  --url https://api.siliconflow.cn/v1/models \
+  --header 'Authorization: Bearer <token>'
+     */
+    
+    
+}
