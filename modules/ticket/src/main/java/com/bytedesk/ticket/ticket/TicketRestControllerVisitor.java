eef5307fd57d29891ccdf202328a4df6bcb20293
jack ning <github@bytedesk.com>
2025-09-22 17:21:23 +0800

    update

--- modules/ticket/src/main/java/com/bytedesk/ticket/ticket/TicketRestControllerVisitor.java
+++ modules/ticket/src/main/java/com/bytedesk/ticket/ticket/TicketRestControllerVisitor.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-09-18 16:18:09
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-09-19 16:53:04
+ * @LastEditTime: 2025-09-22 16:52:46
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
--- modules/ticket/src/main/java/com/bytedesk/ticket/ticket/TicketRestService.java
+++ modules/ticket/src/main/java/com/bytedesk/ticket/ticket/TicketRestService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-01-16 18:50:22
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-09-19 16:55:23
+ * @LastEditTime: 2025-09-22 16:52:54
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
