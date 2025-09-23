3000c4f9c936fdd39cf43699cc07f2fd31606847
jack ning <github@bytedesk.com>
2025-09-23 15:26:34 +0800

    Update RobotRestService.java

--- modules/ai/src/main/java/com/bytedesk/ai/robot/RobotRestService.java
+++ modules/ai/src/main/java/com/bytedesk/ai/robot/RobotRestService.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-03-22 16:44:41
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-09-23 15:02:11
+ * @LastEditTime: 2025-09-23 15:21:46
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -290,7 +290,6 @@ public class RobotRestService extends BaseRestServiceWithExport<RobotEntity, Rob
         robot.setAvatar(request.getAvatar());
         robot.setDescription(request.getDescription());
         // robot.setPublished(request.getPublished());
-        // robot.setDefaultReply(request.getDefaultReply());
         robot.setKbEnabled(request.getKbEnabled());
         robot.setKbUid(request.getKbUid());
         
