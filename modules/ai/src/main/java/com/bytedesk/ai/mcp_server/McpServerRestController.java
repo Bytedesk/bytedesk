ceb9c33c0a489fd7270957d6640338e58e46017f
jack ning <github@bytedesk.com>
2025-09-26 14:50:27 +0800

    update

--- modules/ai/src/main/java/com/bytedesk/ai/mcp_server/McpServerRestController.java
+++ modules/ai/src/main/java/com/bytedesk/ai/mcp_server/McpServerRestController.java
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-05-11 18:25:36
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-09-26 13:36:46
+ * @LastEditTime: 2025-09-26 14:35:19
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -13,18 +13,10 @@
  */
 package com.bytedesk.ai.mcp_server;
 
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.context.annotation.Description;
 
