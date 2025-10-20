a38e8c42aacb01fdad9cbf0ff1f77fe705e8e3a2
jack ning <github@bytedesk.com>
2025-10-20 17:40:47 +0800

    update

--- starter/src/main/resources/mysql-init/01-create-freeswitch.sql
+++ starter/src/main/resources/mysql-init/01-create-freeswitch.sql
@@ -1,4 +1,4 @@
--- 创建 freeswitch 数据库（如不存在）
-CREATE DATABASE IF NOT EXISTS `freeswitch`
+-- 创建 bytedesk_freeswitch 数据库（如不存在）
+CREATE DATABASE IF NOT EXISTS `bytedesk_freeswitch`
   CHARACTER SET utf8mb4
   COLLATE utf8mb4_unicode_ci;
