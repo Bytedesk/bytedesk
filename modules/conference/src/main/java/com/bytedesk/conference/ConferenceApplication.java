d90ae32079934eb571cce80b1c57ffc61dec9ee3
jack ning <github@bytedesk.com>
2025-10-21 14:18:24 +0800

    Update ConferenceApplication.java

--- modules/conference/src/main/java/com/bytedesk/conference/ConferenceApplication.java
+++ modules/conference/src/main/java/com/bytedesk/conference/ConferenceApplication.java
@@ -1,6 +1,6 @@
 package com.bytedesk.conference;
 
-import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
