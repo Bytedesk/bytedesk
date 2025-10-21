54b4e38774215ef0a35ef002b04d1e6d747618c2
jack ning <github@bytedesk.com>
2025-10-21 15:57:51 +0800

    update

--- deploy/server/agent/agent/assets/js/index-BtFLy8xS.js
+++
@@ -1,1 +0,0 @@
-import i from"./index-CWL6w_BV.js";import"./index-D_XoHqKW.js";function e(t){return i({url:"/visitor/api/feedback/submit",method:"post",data:t})}export{e as submitFeedback};
---
+++ deploy/server/agent/agent/assets/js/index-C-EJI_cj.js
@@ -0,0 +1,1 @@
+import{T as t}from"./index-CHOiYuc8.js";import o from"./index-EFwrVLcY.js";async function a(i){return o("/visitor/api/v1/init",{method:"POST",data:{...i,client:t}})}async function e(i){return o("/visitor/api/v1/browse",{method:"POST",data:{...i,client:t}})}export{e as browse,a as initVisitor};
