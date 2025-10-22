6bbeb621c80346e37029914340b6744b16a954a8
jack ning <github@bytedesk.com>
2025-10-22 14:08:18 +0800

    update

--- starter/src/main/resources/mysql-init/01-create-db.sql
+++ starter/src/main/resources/mysql-init/01-create-db.sql
@@ -1,3 +1,8 @@
+-- 创建 bytedesk 数据库（如不存在）
+CREATE DATABASE IF NOT EXISTS `bytedesk`
+  CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
+
 -- 创建 bytedesk_freeswitch 数据库（如不存在）
 CREATE DATABASE IF NOT EXISTS `bytedesk_freeswitch`
   CHARACTER SET utf8mb4
