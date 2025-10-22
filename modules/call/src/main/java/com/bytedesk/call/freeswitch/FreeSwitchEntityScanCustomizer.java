1bc8170628e18db0b42c7a67bd14570981cbd65f
jack ning <github@bytedesk.com>
2025-10-22 17:06:51 +0800

    update

--- modules/call/src/main/java/com/bytedesk/call/freeswitch/FreeSwitchEntityScanCustomizer.java
+++ modules/call/src/main/java/com/bytedesk/call/freeswitch/FreeSwitchEntityScanCustomizer.java
@@ -18,7 +18,6 @@ import org.springframework.beans.factory.config.TypedStringValue;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.ClassUtils;
 import org.springframework.util.StringUtils;
 
 @Configuration
