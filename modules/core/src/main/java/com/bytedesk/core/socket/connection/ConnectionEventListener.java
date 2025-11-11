32fe897766fe8c167e99ef0abbc21b0801e9ee77
jack ning <github@bytedesk.com>
2025-11-11 13:58:18 +0800

    update

--- modules/core/src/main/java/com/bytedesk/core/socket/connection/ConnectionEventListener.java
+++ modules/core/src/main/java/com/bytedesk/core/socket/connection/ConnectionEventListener.java
@@ -34,12 +34,12 @@ public class ConnectionEventListener {
     public void onMqttConnectedEvent(MqttConnectedEvent event) {
         String clientId = event.getClientId();
         // 用户clientId格式: uid/client/deviceUid
-        final String uid = clientId.split("/")[0];
-        log.info("user onMqttConnectedEvent uid {}, clientId {}", uid, clientId);
+        final String userUid = clientId.split("/")[0];
+        final String deviceUid = clientId.contains("/") && clientId.split("/").length > 2 ? clientId.split("/")[2] : null;
+        log.info("user onMqttConnectedEvent uid {}, clientId {}", userUid, clientId);
         // 标记连接（使用 ConnectionEntity 支持多端在线）
         // 无法从事件中获取更多上下文，使用协议 MQTT，其它信息置空/默认
-        connectionRestService.markConnected(uid, null, clientId,
-                clientId.contains("/") && clientId.split("/").length > 2 ? clientId.split("/")[2] : null,
+        connectionRestService.markConnected(userUid, null, clientId, deviceUid,
                 ConnectionProtocalEnum.MQTT.name(), null, null, null, 90);
     }
 
@@ -47,8 +47,8 @@ public class ConnectionEventListener {
     public void onMqttDisconnectedEvent(MqttDisconnectedEvent event) {
         String clientId = event.getClientId();
         // 用户clientId格式: uid/client/deviceUid
-        final String uid = clientId.split("/")[0];
-        log.info("user onMqttDisconnectedEvent uid {}, clientId {}", uid, clientId);
+        final String userUid = clientId.split("/")[0];
+        log.info("user onMqttDisconnectedEvent uid {}, clientId {}", userUid, clientId);
         // 先标记该 client 断开
         connectionRestService.markDisconnected(clientId);
     }
