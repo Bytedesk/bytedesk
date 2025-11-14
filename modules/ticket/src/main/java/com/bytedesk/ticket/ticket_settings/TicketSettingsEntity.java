2277efefe8476947ddddc70f5a14d5f40f70f2fd
jack ning <github@bytedesk.com>
2025-11-14 16:24:00 +0800

    update

--- modules/ticket/src/main/java/com/bytedesk/ticket/ticket_settings/TicketSettingsEntity.java
+++ modules/ticket/src/main/java/com/bytedesk/ticket/ticket_settings/TicketSettingsEntity.java
@@ -84,8 +84,6 @@ public class TicketSettingsEntity extends BaseEntity {
 
 
 
-
-
     // ====== 发布版本 ======
     @ManyToOne(fetch = FetchType.LAZY, optional = true, cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE })
     private TicketBasicSettingsEntity basicSettings;
