bf1f8ac7ac08ac82f5a753d1ad8e45dd6404afb1
jack ning <github@bytedesk.com>
2025-12-10 16:21:18 +0800

    update

--- demos/src/main/java/com/bytedesk/demos/consumer/package-info.java
+++
@@ -1,5 +0,0 @@
-
-@NonNullApi
-package com.bytedesk.demos.consumer;
-
-import org.springframework.lang.NonNullApi;
--- demos/src/main/java/com/bytedesk/demos/goods/package-info.java
+++
@@ -1,5 +0,0 @@
-
-@NonNullApi
-package com.bytedesk.demos.goods;
-
-import org.springframework.lang.NonNullApi;
